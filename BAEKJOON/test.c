7fde8c8144240e06c3564f667900792e84aa840e
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-01-27 13:10:19 +0900

    11650 struct

--- BAEKJOON/test.c
+++ BAEKJOON/test.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct point{
+    int x;
+    int y;
+};
+
 int main(){
-    int arr[] = {1,2};
 
-    printf("%d", sizeof(arr));
+    int N, i;
+    scanf("%d", &N);
 
+    struct point* pnt = malloc(sizeof(struct point)*N);
+
+    for(i=0; i<N; i++){
+        scanf("%d %d", &pnt[i].x, &pnt[i].y);
+    }
+
+    
 
     return 0;
 }
