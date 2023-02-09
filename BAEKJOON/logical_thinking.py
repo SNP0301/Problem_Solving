d9c5787a5778836a0a46dcfda9ff91bcb58b452e
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-09 15:42:42 +0900

    daily done

--- BAEKJOON/logical_thinking.py
+++ BAEKJOON/logical_thinking.py
@@ -1,6 +1,12 @@
-print('         ,r\'\"7')
-print('r`-_   ,\'  ,/')
-print(' \\. \". L_r\'')
-print('   `~\\/')
-print('      |')
-print('      |')
+s = int(input())
+
+if(s>=90):
+    print('A')
+elif(s>=80):
+    print('B')
+elif(s>=70):
+    print('C')
+elif(s>=60):
+    print('D')
+else:
+    print('F')
