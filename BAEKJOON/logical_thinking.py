1e5730cad6c61292d9011ee58118344d11988ab7
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-10 09:47:22 +0900

    2480

--- BAEKJOON/logical_thinking.py
+++ BAEKJOON/logical_thinking.py
@@ -1,9 +1,13 @@
-A, B = map(int,input().split())
-C = int(input())
+A, B, C = map(int,input().split())
 
-total = (A*60)+B+C
-
-H = (total//60)%24
-M = total%60
-
-print(H, M)
+if((A==B)and(B==C)):
+    print((A*1000)+10000)
+elif((A==B)or(B==C)or(C==A)):
+    if(A==B):
+        print((A*100)+1000)
+    elif(B==C):
+        print((B*100)+1000)
+    else:
+        print((A*100)+1000)
+else:
+    print(max(A,B,C)*100)
