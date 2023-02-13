c54191b94d9d33ffe76f9383c1ff62b7421e6d7a
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-13 10:06:43 +0900

    11021

--- BAEKJOON/logical_thinking.py
+++ BAEKJOON/logical_thinking.py
@@ -1,8 +1,5 @@
-import sys
-
 t = int(input())
-for i in range(1, t+1):
-    a,b = map(int,sys.stdin.readline().rstrip().split())
-    print(a+b)
 
-print(a)
+for i in range(1, t+1):
+    a,b = map(int,input().split())
+    print("Cased #%d: %d"%(i, a+b))
