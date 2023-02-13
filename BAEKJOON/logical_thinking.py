4753a13bcf61336c86105a12a4542edbded243a2
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-13 10:50:35 +0900

    1110

--- BAEKJOON/logical_thinking.py
+++ BAEKJOON/logical_thinking.py
@@ -1,7 +1,11 @@
-import sys
+n = int(input())
+new_n = (n%10)*10 + ((n//10)+(n%10))%10
+cnt = 0
+
 while True:
-    try:
-        a, b = map(int,input().split())
-        print(a+b)
-    except:
+    if (n!=new_n):
+        cnt += 1
+        new_n = (new_n%10)*10 + ((new_n//10)+(new_n%10))%10
+    else:
+        print(cnt+1)
         break
