a4f8aef6675605ffe52a9f12daae6bf7a42b0504
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-13 10:22:54 +0900

    10952

--- BAEKJOON/logical_thinking.py
+++ BAEKJOON/logical_thinking.py
@@ -1,4 +1,6 @@
-x = int(input())
-
-for i in range(1,x+1):
-    print(' '*(x-i)+'*'*i)
+while(1):
+    a, b = map(int,input().split())
+    if((a==0)and(b==0)):
+        break
+    else:
+        print(a+b)
