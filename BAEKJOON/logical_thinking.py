7d99d0cb8700cd082f987653fed725a723b4f8ef
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-15 10:09:20 +0900

    2562

--- BAEKJOON/logical_thinking.py
+++ BAEKJOON/logical_thinking.py
@@ -1,6 +1,11 @@
-list = []
-for i in range(9):
-    list.append(int(input()))
-    
-print(max(list))
-print(list.index(max(list))+1)
+n = int(input())
+list = list(map(int,input().split()))
+min = 9999999
+max = -9999999
+for i in range(0,n):
+    if (list[i]>=max):
+        max = list[i]
+    if (list[i]<=min):
+        min = list[i]
+
+print('%d %d'%(min, max))
