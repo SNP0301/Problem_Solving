57b49ae07878af43aa982eedc52eea47263d2051
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-16 09:42:28 +0900

    1546

--- BAEKJOON/logical_thinking.py
+++ BAEKJOON/logical_thinking.py
@@ -1,18 +1,14 @@
-cnt = 0
+avg = 0
+n = int(input())
 
-input_list = []
-for i in range(10):
-    input_list.append(int(input()))
+score = list(map(float,input().split()))
 
-div_check_list = []
-for i in range(42):
-    div_check_list.append(0)
+M = max(score)
 
-for i in range(10):
-    div_check_list[input_list[i]%42] = 1
+for i in range(n):
+    score[i] = score[i]/M*100
 
-for i in range(42):
-    if(div_check_list[i]==1):
-        cnt += 1
+for i in range(n):
+    avg += score[i]
 
-print(cnt)
+print(avg/n)
