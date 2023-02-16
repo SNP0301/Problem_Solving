0b8f2e7c61d675c03ca25ee90d6b59d117d1431d
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-16 09:18:34 +0900

    3052

--- BAEKJOON/logical_thinking.py
+++ BAEKJOON/logical_thinking.py
@@ -1,18 +1,18 @@
-check_list = []
-for i in range(0,31):
-    check_list.append(0)
-    
-##name_list = []
-for i in range(0,28):
-    ##name_list.append(int(input()))
-    new_number = int(input())
-    check_list[new_number]=1
+cnt = 0
 
+input_list = []
+for i in range(10):
+    input_list.append(int(input()))
 
-no_submit = []
-for i in range(1,31):
-    if(check_list[i]==0):
-        no_submit.append(i)
+div_check_list = []
+for i in range(42):
+    div_check_list.append(0)
 
-print(min(no_submit))
-print(max(no_submit))
+for i in range(10):
+    div_check_list[input_list[i]%42] = 1
+
+for i in range(42):
+    if(div_check_list[i]==1):
+        cnt += 1
+
+print(cnt)
