b7b134c36adb0a85123caef9592f15de6284448a
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-17 10:15:01 +0900

    15596

--- BAEKJOON/logical_thinking.py
+++ BAEKJOON/logical_thinking.py
@@ -1,22 +1,4 @@
-def Kaprekar(n:int):
-    value = n
-    while(n>0):
-        value += n%10
-        n = n//10
-        
-    return value
-
-arr = []
-
-for i in range(10001):
-    arr.append(0)
-
-for i in range(1,10001):
-    value = i
-    while(Kaprekar(value)<=10000):
-        arr[Kaprekar(value)] = 1
-        value = Kaprekar(value)
-
-for i in range(1,10001):
-    if(arr[i]==0):
-        print(i)
+def solve(a):
+    ans = 0
+    ans = sum(a)
+    return ans
