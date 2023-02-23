7db74be452174126788142b4bcf83f7ca895db5a
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-23 17:05:37 +0900

    10810

--- BAEKJOON/ps.py
+++ BAEKJOON/ps.py
@@ -1,6 +1,16 @@
-N = int(input())
+N, M = map(int,input().split())
+arr= []
 
-for i in range(N//4):
-    print("long ",end="")
+for i in range(N+1):
+    arr.append(0)
 
-print("int")
+
+for i in range(M):
+    start, end, num = map(int,input().split())
+    for j in range(start,end+1):
+        arr[j]=num
+
+for i in range(1,N+1):
+    print(arr[i],end=" ")
+        
+
