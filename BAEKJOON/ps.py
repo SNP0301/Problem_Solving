e2c5fc90172bc504c8df003510793d7ea4407ee8
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-23 17:09:04 +0900

    10813

--- BAEKJOON/ps.py
+++ BAEKJOON/ps.py
@@ -1,16 +1,14 @@
 N, M = map(int,input().split())
-arr= []
+arr = []
 
 for i in range(N+1):
-    arr.append(0)
+    arr.append(i)
 
-
-for i in range(M):
-    start, end, num = map(int,input().split())
-    for j in range(start,end+1):
-        arr[j]=num
+for k in range(M):
+    i, j = map(int,input().split())
+    tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
 
 for i in range(1,N+1):
     print(arr[i],end=" ")
-        
-
