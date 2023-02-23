c281c3ca8167262dbca57072ffeed461e843c659
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-23 17:24:30 +0900

    10811

--- BAEKJOON/ps.py
+++ BAEKJOON/ps.py
@@ -6,9 +6,12 @@ for i in range(N+1):
 
 for k in range(M):
     i, j = map(int,input().split())
-    tmp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = tmp
+    cnt = 0
+    for z in range((j-i+1)//2):
+        tmp = arr[i+cnt]
+        arr[i+cnt]=arr[j-cnt]
+        arr[j-cnt]= tmp
+        cnt+=1
 
-for i in range(1,N+1):
+for i in range(1, N+1):
     print(arr[i],end=" ")
