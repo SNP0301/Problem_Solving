4080f571bc719f2b4a9ac05656a6bc81553f9fee
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-03-08 09:22:38 +0900

    5622

--- BAEKJOON/ps.py
+++ BAEKJOON/ps.py
@@ -1,11 +1,22 @@
-A,B = map(int,input().split())
+word = input()
+cnt = 0
 
-def sangsu(givenNum):
-    sangsuNum = 0
-    sangsuNum += (givenNum%10)*100
-    sangsuNum += (givenNum//100)
-    givenNum %= 100
-    sangsuNum += (givenNum//10)*10
-    return sangsuNum
+for i in range(len(word)):
+    if(ord(word[i])<=67):
+        cnt += 3
+    elif(ord(word[i])<=70):
+        cnt += 4
+    elif(ord(word[i])<=73):
+        cnt += 5
+    elif(ord(word[i])<=76):
+        cnt += 6
+    elif(ord(word[i])<=79):
+        cnt += 7
+    elif(ord(word[i])<=83):
+        cnt += 8
+    elif(ord(word[i])<=86):
+        cnt += 9
+    elif(ord(word[i])<=90):
+        cnt += 10
 
-print(max(sangsu(A),sangsu(B)))
+print(cnt)
