a5dca6e0ec0cb016f7b494dfc76671b61b181667
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-03-10 10:36:52 +0900

    2444

--- BAEKJOON/ps.py
+++ BAEKJOON/ps.py
@@ -1,2 +1,18 @@
-
-print(input([int(input())-1]))
+n = int(input())
+space_n = n-1
+for i in range(2*n-1):
+    if(i<n):
+        for j in range(space_n):
+            print(" ",end="")
+        for j in range(2*i+1):
+            print("*",end="")
+        space_n -= 1
+        print("\n",end="")
+    elif(i>=n):
+        space_n += 1
+        for j in range(space_n+1):
+            print(" ",end="")
+        for j in range(2*n-2*space_n-3):
+            print("*",end="")
+        print("\n",end="")
+        
