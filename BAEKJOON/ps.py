0da7de4280d3b30d2e0e4220619128688a60156b
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-03-14 09:00:32 +0900

    2941

--- BAEKJOON/ps.py
+++ BAEKJOON/ps.py
@@ -1,8 +1,40 @@
 word = input()
-isPalindrome = 1
-for i in range(len(word)//2):
-    if(word[i]!=word[len(word)-1-i]):
-        isPalindrome = 0
+cnt = 0
+word_cnt = 0
 
-print(isPalindrome)
+while True:
+    if(cnt<len(word)):
+        currentWord = word[cnt:cnt+2]
+        if(currentWord=='c='):
+            word_cnt += 1
+            cnt += 2
+        elif(currentWord=='c-'):
+            word_cnt += 1
+            cnt += 2
+        elif((currentWord=='dz')and(word[cnt+2]=='=')):
+            word_cnt += 1
+            cnt += 3
+        elif(currentWord=='d-'):
+            word_cnt += 1
+            cnt += 2
+        elif(currentWord=='lj'):
+            word_cnt += 1
+            cnt += 2
+        elif(currentWord=='nj'):
+            word_cnt += 1
+            cnt += 2
+        elif(currentWord=='s='):
+            word_cnt += 1
+            cnt += 2
+        elif(currentWord=='z='):
+            word_cnt += 1
+            cnt += 2
+        else:
+            word_cnt += 1
+            cnt += 1
+    else:
+        break
+
+print(word_cnt)
+
 
