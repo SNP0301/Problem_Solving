29635de8251b72f3ad7bdca369dd2bb4c1fcb147
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-08-11 15:45:59 +0900

    [BOJ] 10825

--- ps.py
+++ ps.py
@@ -4,11 +4,12 @@ input = sys.stdin.readline
 n = int(input())
 arr = []
 for i in range(n):
-    arr.append(list(map(int,input().split())))
+    inputArr = list(input().split())
+    for j in range(1,4):
+        inputArr[j] = int(inputArr[j])
+    arr.append(inputArr)
 
+arr.sort(key = lambda x: (-x[1],x[2],-x[3],x[0]))
 
-arr.sort(key=lambda x: (x[0], x[1]))
-
-
-for i in range(n):
-    print(arr[i][0],arr[i][1])
+for i in range(len(arr)):
+    print(arr[i][0])
