010649c4a4ee8eab6f50ebe4f0478168407385d3
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-08-18 11:29:20 +0900

    [BOJ] 15657 temp

--- ps.py
+++ ps.py
@@ -1,79 +1,24 @@
-'''
-[BOJ] 14500. 테트로미노
-
-1. Blue: 2가지
-2. Yellow: 1가지
-3. Orange: 8가지
-4. Green: 4가지
-5. Pink: 4가지
-'''
-
+from itertools import product
 import sys
 input = sys.stdin.readline
 
-n,m= map(int,input().split())
-arr= []
-for i in range(n):
-    arr.append([int(x) for x in input().split()])
+n,m = map(int,input().split())
 
-tetroArr = [
-    [[1,1,1,1]],
-    [[1],[1],[1],[1]], ### BLUE DONE
+preArr = list(map(int,input().split()))
+preArr.sort()
 
-    [[1,1],[1,1]], ### Yellow DONE
+postArr = list(map(str,preArr))
 
-    [[1,0],[1,0],[1,1]],
-    [[0,1],[0,1],[1,1]],
-    [[1,1],[1,0],[1,0]],
-    [[1,1],[0,1],[0,1]],
+perTuple = product(postArr,repeat=m)
 
-    [[1,0,0],[1,1,1]],
-    [[0,0,1],[1,1,1]],
-    [[1,1,1],[1,0,0]],
-    [[1,1,1],[0,0,1]], ### ORANGE DONE
+printedArr = []
 
-    [[1,0],[1,1],[0,1]],
-    [[0,1],[1,1],[1,0]],
+for i in perTuple:
+    iList = sorted(list(i))
+    ##print(iList)
+    if iList not in printedArr:
+        printedArr.append(iList)
+        i = tuple(iList)
+        print(" ".join(i))
 
-    [[0,1,1],[1,1,0]],
-    [[1,1,0],[0,1,1]], ### GREEN DONE
-
-    [[1,1,1],[0,1,0]],
-    [[0,1,0],[1,1,1]],
-    
-    [[1,0],[1,1],[1,0]],
-    [[0,1],[1,1],[0,1]] ### PINK DONE
-
-
-]
-
-'''
-for i in range(len(tetroArr)):
-   print("CHECK BOUNDARY: DOWN[%d], and RIGHT[%d]"%(len(tetroArr[i])-1,len(tetroArr[i][0])-1))
-
-down boundary = len(tetroArr[i])-1
-right boundary = len(tetroArr[i][0])-1
-'''
-
-print("%dX%d"%(n,m))
-
-iStart = 0
-jStart = 0
-tetroType = 0
-max = -1
-ti = 0
-tj = 0
-
-while(iStart<=n-1 and jStart<=m-1):
-    print("Starts from [%d,%d]"%(iStart,jStart))
-    for t in range(len(tetroArr)-17):
-        if(len(tetroArr[t])-1<=n and len(tetroArr[t][0])-1<=m):
-            print("End at [%d,%d]"%(len(tetroArr[t])-1,len(tetroArr[t][0])-1))
-                
-
-    if(jStart==m-1):
-        iStart += 1
-        jStart = 0
-    else:
-        jStart += 1
 
