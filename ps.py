5b5a12e26cd9491c539c5fee043f07ca99d24eac
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-08-23 17:20:58 +0900

    [BOJ] 2004

--- ps.py
+++ ps.py
@@ -1,35 +1,21 @@
-'''
-[BOJ] 24480. 알고리즘 수업 - 깊이 우선 탐색 2
-'''
-
+from itertools import combinations
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(10**6)
 
-n,m,r = map(int,input().split())
-
-graph = [[] for _ in range(n+1)]
-visitedDfs = [0 for _ in range(n+1)]
-count = 0
-
-for _ in range(m):
-    u,v = map(int,input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-
-for i in graph:
-    i.sort(reverse=True)
-
-
-def dfs(num):
-    global count
-    count += 1
-    visitedDfs[num] = count
-    for i in graph[num]:
-        if not visitedDfs[i]:
-            dfs(i)
-
-dfs(r)
-
+n,m = map(int,input().split())
+arr = []
 for i in range(1,n+1):
-    print(visitedDfs[i])
+    arr.append(str(i+1))
+
+arr = list(combinations(arr,m))
+comLen = len(arr)
+
+cnt = 0
+while True:
+    if(comLen%10==0):
+        cnt += 1
+        comLen = comLen // 10
+    else:
+        print(cnt)
+        break
