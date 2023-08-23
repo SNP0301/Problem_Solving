961b26d47fcf881a2c3239d16696243ee15f48a7
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-08-23 11:11:17 +0900

    [BOJ] 1260

--- ps.py
+++ ps.py
@@ -1,83 +1,42 @@
-### https://doorbw.tistory.com/235 공부하기
-def push_front(deque:list , X:int):
-    xList = list()
-    xList.append(X)
-    print(type(xList))
-    print(type(deque))
-    deque = xList + deque
-
-    return deque
-
-def push_back(deque:list , X:int):
-    return deque.append(X)
-
-def pop_front(deque:list):
-    if(not deque): 
-       print(-1)
-    else:
-        print(deque[0])
-        return deque[1:]
-
-def pop_back(deque:list):
-    if(not deque):
-        print(-1)
-    else:
-        print(deque[len(deque)-1])
-        return deque[:-1]
-    
-def size(deque:list):
-    return len(deque)
-
-def empty(deque:list):
-    if(not deque):
-        print(1)
-    else:
-        print(0)
-
-def front(deque:list):
-    if(not deque):
-        print(-1)
-    else:
-        print(deque[0])
-
-def back(deque:list):
-    if(not deque):
-        print(-1)
-    else:
-        print(deque[len(deque)-1])
-       
-
-import sys
-input = sys.stdin.readline
+'''
+[BOJ] 1260. DFS와 BFS
+'''
 
 
-n = int(input())
-deque = []
+from collections import deque
 
-for _ in range(n):
-    command = list(map(str,input().split()))
-    
-    if(len(command)==2):
-        X = int(command[1])
+N,M,V = map(int, input().split())
 
-    if(command[0] == "push_front"):
-        deque = push_front(*deque,X)
-    elif(command[0] == "push_back"):
-        deque = push_back(*deque,X)
-    elif(command[0] == "pop_front"):
-        deque = pop_front(*deque)
-    elif(command[0] == "pop_back"):
-        deque = pop_back(*deque)
-    elif(command[0] == "size"):
-        size(*deque)
-    elif(command[0] == "empty"):
-        empty(*deque)
-    elif(command[0] == "front"):
-        front(*deque)
-    elif(command[0] == "back"):
-        back(*deque)
+graph = [[False]*(N+1) for _ in range(N+1)]
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    graph[a][b] = True
+    graph[b][a] = True
+
+visitedDfs = [False]*(N+1)
+visitedBfs = [False]*(N+1)
+
+def dfs(V): 
+    visitedDfs[V] = True
+    print(V,end=" ")
+    for i in range(1,N+1):
+        if not visitedDfs[i] and graph[V][i]:
+            dfs(i)
 
 
+def bfs(V):
+    q = deque([V])
+    visitedBfs[V] = True
+    while q:
+        V = q.popleft()
+        print(V,end=" ")
+        for i in range(1, N+1):
+            if not visitedBfs[i] and graph[V][i]:
+                q.append(i)
+                visitedBfs[i] = True
 
 
-
+dfs(V)
+print()
+bfs(V)
