78da1fd4544ee58295b874f2c03cea0794a3468c
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-08-23 16:00:22 +0900

    [BOJ] 24480

--- ps.py
+++ ps.py
@@ -1,37 +1,35 @@
 '''
-[BOJ] 2606. 바이러스
+[BOJ] 24480. 알고리즘 수업 - 깊이 우선 탐색 2
 '''
 
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**5)
 
-C = int(input()) ### # of computers
-P = int(input()) ### # of pair of computer connections
+n,m,r = map(int,input().split())
 
-graph = [[]for _ in range(C+1)]
-visitedDfs = [0 for _ in range(C+1)]
-answer = 0
+graph = [[] for _ in range(n+1)]
+visitedDfs = [0 for _ in range(n+1)]
+count = 0
 
-for _ in range(P):
+for _ in range(m):
     u,v = map(int,input().split())
     graph[u].append(v)
     graph[v].append(u)
 
-
 for i in graph:
-    i.sort()
+    i.sort(reverse=True)
 
-def dfs(n):
-    global answer
-    visitedDfs[n] = 1
 
-    for i in graph[n]:
+def dfs(num):
+    global count
+    count += 1
+    visitedDfs[num] = count
+    for i in graph[num]:
         if not visitedDfs[i]:
             dfs(i)
-            answer += 1
 
+dfs(r)
 
-dfs(1)
-
-print(answer)
+for i in range(1,n+1):
+    print(visitedDfs[i])
