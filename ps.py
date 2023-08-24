90dc319e8d94c21d4a2b3816054f035881f287d5
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-08-24 15:29:59 +0900

    [BOJ] 11576 temp

--- ps.py
+++ ps.py
@@ -1,21 +1,19 @@
-from itertools import combinations
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-n,m = map(int,input().split())
-arr = []
-for i in range(1,n+1):
-    arr.append(str(i+1))
+a,b = map(int,input().split())
+m = int(input())
+arr = list(map(int,input().split()))
 
-arr = list(combinations(arr,m))
-comLen = len(arr)
+decimalNumber = 0
+multiNumber = 1
+beforeBaseNumber = a
+afterBaseNumber = b
 
-cnt = 0
-while True:
-    if(comLen%10==0):
-        cnt += 1
-        comLen = comLen // 10
-    else:
-        print(cnt)
-        break
+for i in range(m-1,-1,-1):
+    decimalNumber += multiNumber * arr[i]
+    multiNumber *= a
+
+print(decimalNumber)
+
+while(beforeafterBaseNumber >= 0)
