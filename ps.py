aea01700c9e39e2e0c367486e2b72cf56f5b43bf
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-01 15:47:43 +0900

    [BOJ] 2667 input temp

--- ps.py
+++ ps.py
@@ -1,42 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input())
+n = int(input())
+dong = list() ## works as a cnt
+arr = list() ## works as a map
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+## def BFS(x,y):
 
-def BFS(x,y):
-    queue = [(x,y)]
-    matrix[x][y] = 0
-    while queue:
-        x,y = queue.pop()
+for i in range(n):
+    line = input().rstrip()
+    print(line)
+    arr.append([int(x) for x in line])
 
-        for i in range(4):
-            nextX = x + dx[i]
-            nextY = y + dy[i]
-
-            if nextX < 0 or nextX >= M or nextY < 0 or nextY >= N:
-                continue
-
-            if matrix[nextX][nextY] == 1:
-                queue.append((nextX, nextY))
-                matrix[nextX][nextY] = 0
-        
-
-for _ in range(T):
-    M,N,K = map(int,input().split())
-    matrix = [[0]*(N) for _ in range(M)]
-    cnt = 0
-
-    for _ in range(K):
-        x,y = map(int,input().split())
-        matrix[x][y] = 1
-    
-    for a in range(M):
-        for b in range(N):
-            if matrix[a][b] == 1:
-                BFS(a,b)
-                cnt += 1
-
-    print(cnt)
+print(arr)
