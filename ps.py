e67d4cc98a993fe1533418d46a332ee7c04ea340
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-01 13:54:36 +0900

    [BOJ] 1012 solved

--- ps.py
+++ ps.py
@@ -1,53 +1,42 @@
-from collections import deque
-
 import sys
 input = sys.stdin.readline
 
+T = int(input())
 
-n = int(input())
-deque = deque()
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-for _ in range(n):
-    command = list(map(str,input().split()))
+def BFS(x,y):
+    queue = [(x,y)]
+    matrix[x][y] = 0
+    while queue:
+        x,y = queue.pop()
 
-    if(len(command)==2):
-        X = int(command[1])
+        for i in range(4):
+            nextX = x + dx[i]
+            nextY = y + dy[i]
 
-    if(command[0] == "push_front"):
-        deque.appendleft(X)
-    elif(command[0] == "push_back"):
-        deque.append(X)
-    elif(command[0] == "pop_front"):
-        if not deque:
-            print(-1)
-        else:
-            N = deque.popleft()
-            print(N)
-    elif(command[0] == "pop_back"):
-        if not deque:
-            print(-1)
-        else:
-            N = deque.pop()
-            print(N)
-    elif(command[0] == "size"):
-        print(len(deque))
-    elif(command[0] == "empty"):
-        if not deque:
-            print(1)
-        else:
-            print(0)
-    elif(command[0] == "front"):
-        if not deque:
-            print(-1)
-        else:
-            N = deque.popleft()
-            print(N)
-            deque.appendleft(N)
-    elif(command[0] == "back"):
-        if not deque:
-            print(-1)
-        else:
-            N = deque.pop()
-            print(N)
-            deque.append(N)
+            if nextX < 0 or nextX >= M or nextY < 0 or nextY >= N:
+                continue
+
+            if matrix[nextX][nextY] == 1:
+                queue.append((nextX, nextY))
+                matrix[nextX][nextY] = 0
         
+
+for _ in range(T):
+    M,N,K = map(int,input().split())
+    matrix = [[0]*(N) for _ in range(M)]
+    cnt = 0
+
+    for _ in range(K):
+        x,y = map(int,input().split())
+        matrix[x][y] = 1
+    
+    for a in range(M):
+        for b in range(N):
+            if matrix[a][b] == 1:
+                BFS(a,b)
+                cnt += 1
+
+    print(cnt)
