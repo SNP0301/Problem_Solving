bd69093844e546367ae2292be568a0e99ddc04b3
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-01 13:03:03 +0900

    [BOJ] 10866

--- ps.py
+++ ps.py
@@ -1,19 +1,53 @@
+from collections import deque
+
 import sys
 input = sys.stdin.readline
 
-a,b = map(int,input().split())
-m = int(input())
-arr = list(map(int,input().split()))
 
-decimalNumber = 0
-multiNumber = 1
-beforeBaseNumber = a
-afterBaseNumber = b
+n = int(input())
+deque = deque()
 
-for i in range(m-1,-1,-1):
-    decimalNumber += multiNumber * arr[i]
-    multiNumber *= a
+for _ in range(n):
+    command = list(map(str,input().split()))
 
-print(decimalNumber)
+    if(len(command)==2):
+        X = int(command[1])
 
-while(beforeafterBaseNumber >= 0)
+    if(command[0] == "push_front"):
+        deque.appendleft(X)
+    elif(command[0] == "push_back"):
+        deque.append(X)
+    elif(command[0] == "pop_front"):
+        if not deque:
+            print(-1)
+        else:
+            N = deque.popleft()
+            print(N)
+    elif(command[0] == "pop_back"):
+        if not deque:
+            print(-1)
+        else:
+            N = deque.pop()
+            print(N)
+    elif(command[0] == "size"):
+        print(len(deque))
+    elif(command[0] == "empty"):
+        if not deque:
+            print(1)
+        else:
+            print(0)
+    elif(command[0] == "front"):
+        if not deque:
+            print(-1)
+        else:
+            N = deque.popleft()
+            print(N)
+            deque.appendleft(N)
+    elif(command[0] == "back"):
+        if not deque:
+            print(-1)
+        else:
+            N = deque.pop()
+            print(N)
+            deque.append(N)
+        
