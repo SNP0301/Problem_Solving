35f24901652a9e69f33843b82f8585930d75fa08
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-04 20:27:33 +0900

    [BOJ] 4963 solved

--- ps.py
+++ ps.py
@@ -1,59 +1,39 @@
 import sys
+sys.setrecursionlimit(10**8)
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-
-n = int(input())
-answer = 0
-cb_answer = 0
-
-graph = list()
-visited = [[False]*n for _ in range(n)]
-
-cb_graph = list()
-cb_visited = [[False]*n for _ in range(n)]
-
-for _ in range(n):
-    line = input().rstrip()
-    graph.append(line)
-    line = line.replace('G','R')
-    cb_graph.append(line)
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+w = -1
+h = -1
+dx = [-1,-1,-1,0,0,1,1,1]
+dy = [-1,0,1,1,-1,-1,0,1]
 
 def dfs(x,y):
     visited[x][y] = True
 
-    for i in range(4):
+    for i in range(8):
         nextX = x + dx[i]
         nextY = y + dy[i]
-        if nextX >= n or nextY >= n or nextX < 0 or nextY < 0:
+        if nextX >= h or nextY >= w or nextX < 0 or nextY < 0:
             continue
         if visited[nextX][nextY] == False and graph[x][y] == graph[nextX][nextY]:
-            dfs(nextX, nextY)
+            dfs(nextX,nextY)
 
-def cb_dfs(x,y):
-    cb_visited[x][y] = True
 
-    for i in range(4):
-        nextX = x + dx[i]
-        nextY = y + dy[i]
-        if nextX >= n or nextY >= n or nextX < 0 or nextY < 0:
-            continue
-        if cb_visited[nextX][nextY] == False and cb_graph[x][y] == cb_graph[nextX][nextY]:
-            cb_dfs(nextX, nextY)
 
-for i in range(n):
-    for j in range(n):
-        if visited[i][j] == False:
-            dfs(i,j)
-            answer += 1
-        if cb_visited[i][j] == False:
-            cb_dfs(i,j)
-            cb_answer += 1
+while w!=0 and h!= 0:
+    w,h = map(int,input().split())
+    if(w==0 and h==0):
+        break
+    graph = list()
+    visited = [[False]*w for _ in range(h)]
+    answer = 0
 
-print(answer)
-print(cb_answer)
-            
+    for i in range(h):
+        graph.append(input().split())
 
+    for i in range(h):
+        for j in range(w):
+            if visited[i][j] == False and graph[i][j] == '1':
+                dfs(i,j)
+                answer += 1
+    print(answer)
