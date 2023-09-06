ef9cfde5ee8e0cb95659def2d6ea1a5549118817
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-06 14:25:11 +0900

    [BOJ] 1697 TypeError

--- ps.py
+++ ps.py
@@ -2,35 +2,26 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n,m = map(int,input().split())
-graph = list()
+n, k = map(int,input().split())
+graph = [0]* 1000001
+cnt = 1
 
-for i in range(n):
-    line = list(input().rstrip())
-    graph.append([int(x) for x in line])
 
-dx = [-1,1,0,0]
-dy = [0,0,1,-1]
-
-def bfs(x,y):
+def bfs(x):
+    global cnt
     q = deque()
-    q.append((x,y))
-
+    q.append((x))
     while q:
-        x,y = q.popleft()
-        for i in range(4):
-            nextX = x+ dx[i]
-            nextY = y+ dy[i]
-
-            if nextX < 0 or nextY < 0 or nextX >= n or nextY >= m:
+        x = q.popleft()
+        nextX = [x-1, x+1, 2*x]
+        for i in range(3):
+            if nextX[i] < 0 or nextX[i] >= 100000:
                 continue
-            if graph[nextX][nextY] == 1:
-                graph[nextX][nextY] = graph[x][y] + 1
-                q.append((nextX, nextY))
+            if graph[nextX[i]-1] == 0:
+                q.append((nextX[i]-1))
+                graph[nextX[i]-1] = cnt
+        if  graph[k] != 0:
+            return(graph[k]-1)
+        cnt += 1
 
-    return graph[n-1][m-1]
-
-print(bfs(0,0))
-
-
-
+print(int(bfs(n)+1))
