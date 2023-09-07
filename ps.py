46abdf8709264640ff812859ee092822bcc9fa33
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-07 11:11:30 +0900

    [BOJ] 1697 solved

--- ps.py
+++ ps.py
@@ -3,25 +3,42 @@ input = sys.stdin.readline
 from collections import deque
 
 n, k = map(int,input().split())
-graph = [0]* 1000001
-cnt = 1
+lastIndex = 100000
 
+graph = [0 for i in range(lastIndex+1)]
 
 def bfs(x):
-    global cnt
     q = deque()
-    q.append((x))
+    cnt = 1
+    q.append(x)
+    graph[x] = 1
+
+
     while q:
         x = q.popleft()
-        nextX = [x-1, x+1, 2*x]
+        dx = [x-1, x+1, x*2]
+        ##print("[%d]: (%d, %d, %d)"%(x,dx[0],dx[1],dx[2]))
         for i in range(3):
-            if nextX[i] < 0 or nextX[i] >= 100000:
-                continue
-            if graph[nextX[i]-1] == 0:
-                q.append((nextX[i]-1))
-                graph[nextX[i]-1] = cnt
-        if  graph[k] != 0:
-            return(graph[k]-1)
-        cnt += 1
+            nextX = dx[i]
 
-print(int(bfs(n)+1))
+            if nextX < 0 or nextX >= lastIndex+1:
+                continue
+                
+            if graph[nextX]==0:
+                q.append(nextX)
+                graph[nextX] = graph[x]+1
+                ##print("from %d, graph[%d] is now %d"%(x,nextX,cnt)) 
+        ##print(q)
+
+        if(graph[k]!=0):
+            print(graph[k]-1)
+            break
+        else:
+            cnt += 1
+
+
+bfs(n)
+
+
+
+
