d18547fde2d4f9d2f095783883cfbaa1dc9c20d8
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-07 19:44:39 +0900

    [BOJ] 7576 solved

--- ps.py
+++ ps.py
@@ -1,46 +1,11 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
+m,n = map(int,input().split())
+arr = list()
+for i in range(n):
+    arr.append(list(map(int,input().split())))
 
-t = int(input())
 
-dx = [-2,-2,-1,-1,1,1,2,2]
-dy = [1,-1,2,-2,2,-2,1,-1]
-
-
-def bfs(x,y):
-    cnt = 1
-    q = deque()
-    q.append((x,y))
-    
-    while q:
-        x,y = q.popleft()
-        graph[x][y] = cnt
-
-        for k in range(8):
-            nextX = x + dx[k]
-            nextY = y + dy[k]
-
-            if nextX < 0 or nextY < 0 or nextX >= i or nextY >= i:
-                continue
-            if x == endX and y == endY:
-                print(graph[endX][endY])
-                return
-            if graph[nextX][nextY] == 0:
-                q.append((nextX,nextY))
-                graph[nextX][nextY] = cnt
-                cnt += 1
-
-for _ in range(t):
-    i = int(input())
-    cnt = 1
-    graph = [[0]*i for _ in range(i)]
-    startX, startY = map(int,input().split())
-    endX, endY = map(int,input().split())
-
-    if(startX == endX and startY == endY):
-        print(0)
-    else:
-        bfs(startX, startY)
-    
+print(arr)
+               
