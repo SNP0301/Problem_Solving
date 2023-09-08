da0421a06899cda4ad519063b2eacb17f972cbcf
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-08 11:16:23 +0900

    [BOJ] 7576 Tc 3 solved

--- ps.py
+++ ps.py
@@ -1,42 +1,48 @@
+'''
+[BOJ] 7576. 토마토
+T: 1 second
+M: 256 MB
+'''
+
+from collections import deque
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-t = int(input())
+m,n = map(int,input().split())
+graph = list()
+q = deque()
 
-dx = [-2,-2,-1,-1,1,1,2,2]
-dy = [1,-1,2,-2,2,-2,1,-1]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
 def bfs(x,y):
-    cnt = 0
-    q = deque()
     q.append((x,y))
-    graph[x][y] = 1
+    
+    while q:
+        for _ in range(len(q)):
+            x,y = q.popleft()
+            for i in range(4):
+                nextX = x + dx[i]
+                nextY = y + dy[i]
 
-    while q: 
-        x,y = q.popleft()
-        
-        
-        for j in range(8):
-            nextX = x + dx[j]
-            nextY = y + dy[j]
-
-            if nextX < 0 or nextY < 0 or nextX >= i or nextY >= i:
-                continue
-            if graph[nextX][nextY] == 0:
-                q.append((nextX,nextY))
-                graph[nextX][nextY] = graph[x][y] + 1
-        
-        if(graph[endX][endY] != 0):
-            print(graph[endX][endY]-1)
-            break
+                if nextX < 0 or nextY < 0 or nextX >= n or nextY >= m:
+                    continue
+                if graph[nextX][nextY] == 0:
+                    q.append((nextX,nextY))
+                    graph[nextX][nextY] = graph[x][y] + 1
+        for k in graph:
+            print(k)
+        print("\n")
 
 
-for _ in range(t):
-    i = int(input())
-    graph = [[0]*i for _ in range(i)]
-    visited = [[False]*i for _ in range(i)]
-    startX, startY = map(int,input().split())
-    endX, endY = map(int,input().split())
 
-    bfs(startX,startY)
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
+
+for a in range(n):
+    for b in range(m):
+        if graph[a][b] == 1:
+            q.append((a,b))
+
+bfs(q[0][0],q[0][1])
+
