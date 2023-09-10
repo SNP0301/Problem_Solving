7e52c75c5deb37762237f143a6eed37c546af6a2
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-10 13:50:43 +0900

    [BOJ] 16928 default board solved

--- ps.py
+++ ps.py
@@ -1,15 +1,51 @@
 '''
-[BOJ] 19532. 수학은 비대면강의입니다
+[BOJ] 16928. 뱀과 사다리 게임
 T: 1초
-M: 1024MB
+M: 512MB
 '''
 
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-a,b,c,d,e,f = map(int,input().split())
+MAX_INDEX = 20 + 1
 
-for x in range(-999,1000):
-    for y in range(-999,1000):
-        if (a*x+b*y==c) and (d*x+e*y==f):
-            print(x,y)
+n,m = map(int,input().split())
+
+item_graph = [0 for _ in range(MAX_INDEX)]
+board_graph = [0 for i in range(MAX_INDEX)]
+board_graph[1] = 1
+board_graph[MAX_INDEX-1] =999
+
+dx = [1,2,3,4,5,6]
+
+def check_graph():
+    print(board_graph)
+    print(item_graph)
+
+def bfs(x):
+    q = deque()
+    q.append(x)
+
+    while q:
+        x = q.popleft()
+        for i in range(6):
+            nextX = x + dx[i]
+            
+            if nextX >= MAX_INDEX:
+                continue
+            if board_graph[nextX] == 0:
+                q.append(nextX)
+                board_graph[nextX] = board_graph[x] + 1
+
+            
+
+for _ in range(n+m):
+    u,v = map(int,input().split())
+    item_graph[u] = v
+
+bfs(1)
+
+check_graph()
+
+print(board_graph[MAX_INDEX-1])
