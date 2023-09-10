d906963a0b10a67b041da8ae37c18be2047e2cb2
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-10 13:22:15 +0900

    [BOJ] 19532 solved

--- ps.py
+++ ps.py
@@ -1,80 +1,15 @@
 '''
-[BOJ] 7569. 토마토
-T: 1 second
-M: 256 MB
-
-3차원 힌트
-print(graph[0][0][0]) ## 1  (시작)
-print(graph[0][0][1]) ## 2  (행)
-print(graph[0][1][0]) ## 4  (열)
-print(graph[1][0][0]) ## 10 (층)
+[BOJ] 19532. 수학은 비대면강의입니다
+T: 1초
+M: 1024MB
 '''
 
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-twoDimensionalGraph = list()
-graph = list()
-q = deque()
-possible = True
-answer = 1
+a,b,c,d,e,f = map(int,input().split())
 
-dx = [-1,1,0,0,0,0]
-dy = [0,0,-1,1,0,0]
-dz = [0,0,0,0,-1,1]
-
-m,n,h = map(int,input().split())
-
-
-for _ in range(h):
-    for _ in range(n):
-        twoDimensionalGraph.append(list(map(int,input().split())))
-    graph.append(twoDimensionalGraph)
-    twoDimensionalGraph = []
-
-
-
-def bfs(x,y,z):
-    q.append((x,y,z))
-
-    while q:
-        for _ in range(len(q)):
-            x,y,z, = q.popleft()
-            for i in range(6):
-                nextX = x + dx[i]
-                nextY = y + dy[i]
-                nextZ = z + dz[i]
-
-                if nextX < 0 or nextY < 0 or nextZ < 0 or nextX >= n or nextY >= m or nextZ >= h:
-                    continue
-                if graph[nextZ][nextX][nextY] == 0:
-                    q.append((nextX, nextY, nextZ))
-                    graph[nextZ][nextX][nextY] = graph[z][x][y] + 1
-
-
-for c in range(h):
-    for a in range(n):
-        for b in range(m):
-            ##print("(a: %d, b: %d, c:%d)"%(a,b,c))
-            if graph[c][a][b] == 1:
-                q.append((a,b,c))
-
-
-
-if q:
-    bfs(q[0][0],q[0][1],q[0][2])
-else:
-    possible = False
-
-
-for u in graph:
-        for v in u:
-            answer = max(answer,max(v))
-            if 0 in v:
-                possible = False
-
-if possible:
-    print(answer-1)
-else:
-    print(-1)
+for x in range(-999,1000):
+    for y in range(-999,1000):
+        if (a*x+b*y==c) and (d*x+e*y==f):
+            print(x,y)
