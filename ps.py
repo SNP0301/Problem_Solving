2a6ae7bbe4860c4e2fa9f1bbeb1ccfbebeee0c0e
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-10 14:32:43 +0900

    [BOJ] 2206 I/O temp

--- ps.py
+++ ps.py
@@ -1,5 +1,5 @@
 '''
-[BOJ] 16928. 뱀과 사다리 게임
+[BOJ] 2206. 벽 부수고 이동하기
 T: 1초
 M: 512MB
 '''
@@ -8,42 +8,36 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-MAX_INDEX = 100 + 1
-
 n,m = map(int,input().split())
+graph = list()
+q = deque
+q.append((0,0))
+breakable = True
 
-item_graph = [0 for _ in range(MAX_INDEX)]
-board_graph = [0 for i in range(MAX_INDEX)]
-board_graph[1] = 0
+for i in range(n):
+    graph.append(list(map(int,input().strip())))
 
-dx = [1,2,3,4,5,6]
 
-def bfs(x):
-    q = deque()
-    q.append(x)
 
-    while q:
-        x = q.popleft()
-        for i in range(6):
-            nextX = x + dx[i]
-            
-            if nextX >= MAX_INDEX:
-                continue
-            if board_graph[nextX] == 0:
-                q.append(nextX)
-                board_graph[nextX] = board_graph[x] + 1
-            if item_graph[nextX] != 0 and board_graph[nextX] >= board_graph[x] +1:
-                q.append(item_graph[nextX])
-                ##print("x is [%d], nextX is [%d], item_graph[%d] is %d"%(x,nextX,nextX,item_graph[nextX]))
-                board_graph[item_graph[nextX]] = board_graph[x]+1
-                ##check_graph()
-                ##print("\n")
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+while q:
+    x,y = q.popleft()
+    
+    for i in range(4):
+        nextX = x + dx[i]
+        nextY = y + dy[i]
+        
+        if nextX < 0 or nextY < 0 or nextX >= n or nextY >= m:
+            continue
+        if graph[nextX][nextY] == 0:
             
 
-for _ in range(n+m):
-    u,v = map(int,input().split())
-    item_graph[u] = v
 
-bfs(1)
 
-print(board_graph[MAX_INDEX-1])
+def check_graph():
+    for i in graph:
+        print(i)
+
+check_graph()
