f67820662ded8e2409ce39d371369030c441ed64
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-11 14:38:42 +0900

    [BOJ] 11726 solved

--- ps.py
+++ ps.py
@@ -1,41 +1,21 @@
 '''
-[BOJ] 11052. 카드 구매하기
+[BOJ] 11726. 2*n 타일링
 T: 1초
 M: 256MB
 '''
+
 import sys
 input = sys.stdin.readline
-from collections import deque
 
 n = int(input())
-whole_price = list(map(int,input().split()))
-per_price = [[0,0] for _ in range(n)]
-answer = 0
 
-##print(whole_price)
+dp = [0 for _ in range(1001)]
+dp[1] = 1
+dp[2] = 2
+l = 3
 
-for i in range(n):
-    per_price[i][0] = i+1
-    per_price[i][1] = whole_price[i]/(i+1)
+while dp[n] == 0:
+    dp[l] = dp[l-1]+dp[l-2]
+    l += 1
 
-
-
-per_price.sort(key=lambda x: (x[1],x[0]),reverse=True)
-
-per_price = deque(per_price)
-
-##print(per_price)
-
-
-remain = n
-
-while remain:
-    number_of_set = remain//per_price[0][0]
-    if number_of_set >= 1:
-        ##print("How many set?: [%d]"%(number_of_set))
-        answer += (number_of_set)*per_price[0][1]*per_price[0][0]
-        remain = remain%per_price[0][0]
-    else:
-        per_price.popleft()
-
-print(int(answer))
+print(dp[n]%10007)
