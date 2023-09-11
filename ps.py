b6f84d3420a4b47a939f70afdf6983fefd046c2b
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-11 11:22:12 +0900

    [BOJ] 11052 not greedy

--- ps.py
+++ ps.py
@@ -1,81 +1,41 @@
 '''
-[BOJ] 3085. N과 M (4)
+[BOJ] 11052. 카드 구매하기
 T: 1초
-M: 128MB
+M: 256MB
 '''
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 n = int(input())
-arr = list()
+whole_price = list(map(int,input().split()))
+per_price = [[0,0] for _ in range(n)]
 answer = 0
 
-for _ in range(n):
-    arr.append(list(map(str,input().strip())))
-    
-
-
-def swap_y_forward(x,y):
-    if y+1 >= n:
-        return
-    else:
-        arr[x][y], arr[x][y+1] = arr[x][y+1], arr[x][y]
-
-def swap_y_backward(x,y):
-    if y+1 >= n:
-        return
-    else:
-        arr[x][y+1], arr[x][y] = arr[x][y], arr[x][y+1]
-
-def swap_x_forward(x,y):
-     if x+1 >= n:
-         return
-     else:
-         arr[x][y], arr[x+1][y] = arr[x+1][y], arr[x][y]
-
-def swap_x_backward(x,y):
-    if x+1 >= n:
-        return
-    else:
-        arr[x+1][y], arr[x][y] = arr[x][y], arr[x+1][y]
-
-def check_row(x):
-    x_max = 0
-    current = 1
-    for k in range(n-1):
-        if arr[x][k] == arr[x][k+1]:
-            current += 1
-        else:
-            current = 1
-        x_max = max(x_max,current)
-    return x_max
-
-def check_column(y):
-    y_max = 0
-    current = 1
-    for k in range(n-1):
-        if arr[k][y] == arr[k+1][y]:
-            current += 1
-        else:
-            current = 1
-        y_max = max(y_max,current)
-    return y_max
+##print(whole_price)
 
 for i in range(n):
-    for j in range(n):
-        swap_y_forward(i,j)
-        answer = max(answer, check_row(i))
-        answer = max(answer,check_column(j))
-        if j+1 < n:
-            answer = max(answer,check_column(j+1))
+    per_price[i][0] = i+1
+    per_price[i][1] = whole_price[i]/(i+1)
 
-        swap_y_backward(i,j)
 
-        swap_x_forward(i,j)
-        answer = max(answer,check_column(j))
-        answer = max(answer, check_row(i))
-        if i+1 < n:
-            answer = max(answer, check_row(i+1))
-        swap_x_backward(i,j)
 
-print(answer)
+per_price.sort(key=lambda x: (x[1],x[0]),reverse=True)
+
+per_price = deque(per_price)
+
+##print(per_price)
+
+
+remain = n
+
+while remain:
+    number_of_set = remain//per_price[0][0]
+    if number_of_set >= 1:
+        ##print("How many set?: [%d]"%(number_of_set))
+        answer += (number_of_set)*per_price[0][1]*per_price[0][0]
+        remain = remain%per_price[0][0]
+    else:
+        per_price.popleft()
+
+print(int(answer))
