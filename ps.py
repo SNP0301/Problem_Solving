aa01268475a3a63d4bf7e42d5e9d3a18e4708a14
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-11 20:20:11 +0900

    [BOJ] 1463 solved

--- ps.py
+++ ps.py
@@ -1,21 +1,39 @@
 '''
-[BOJ] 11726. 2*n 타일링
-T: 1초
-M: 256MB
+[BOJ] 1463. 1로 만들기
+T: 0.15초
+M: 128MB
 '''
-
 import sys
 input = sys.stdin.readline
 
 n = int(input())
+MAX_INDEX = 1000000
+dp = [0 for _ in range(MAX_INDEX*3+1)]
 
-dp = [0 for _ in range(1001)]
-dp[1] = 1
-dp[2] = 2
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
 l = 3
 
-while dp[n] == 0:
-    dp[l] = dp[l-1]+dp[l-2]
+while l <= MAX_INDEX:
+    if dp[l] == 0:
+        if l % 3 == 0:
+            dp[l] = dp[l//3] + 1
+        elif l % 2 == 0:
+            dp[l] = dp[l//2] + 1 
+        else:
+            dp[l] = dp[l-1] + 1
+
+    if dp[l*3] == 0 or dp[l*3] > dp[l]:
+        dp[l*3] = dp[l] + 1
+    
+    if dp[l*2] == 0 or dp[l*2] > dp[l]:
+        dp[l*2] = dp[l] + 1
+    
+    if dp[l+1] == 0 or dp[l+1] > dp[l]:
+        dp[l+1] = dp[l] + 1
+
     l += 1
 
-print(dp[n]%10007)
+print(dp[n])
