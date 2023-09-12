6a8fb2473499ae1ca9af16a115bac35a0f4b9cfc
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-12 11:35:45 +0900

    [BOJ] 15652 solved

--- ps.py
+++ ps.py
@@ -1,39 +1,25 @@
 '''
-[BOJ] 1463. 1로 만들기
-T: 0.15초
-M: 128MB
+[BOJ] 15652. N과 M (4)
+T: 1s
+M: 512MB
 '''
+
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(1000000)
 
-n = int(input())
-MAX_INDEX = 1000000
-dp = [0 for _ in range(MAX_INDEX*3+1)]
+n,m = map(int,input().split())
+ans = list()
 
-dp[1] = 0
-dp[2] = 1
-dp[3] = 1
+def back():
+    if (len(ans) == m) and ans==sorted(ans):
+        print(*ans)
+        return
+    if ans != sorted(ans):
+        return
+    for i in range(1,n+1):
+        ans.append(i)
+        back()
+        ans.pop()
 
-l = 3
-
-while l <= MAX_INDEX:
-    if dp[l] == 0:
-        if l % 3 == 0:
-            dp[l] = dp[l//3] + 1
-        elif l % 2 == 0:
-            dp[l] = dp[l//2] + 1 
-        else:
-            dp[l] = dp[l-1] + 1
-
-    if dp[l*3] == 0 or dp[l*3] > dp[l]:
-        dp[l*3] = dp[l] + 1
-    
-    if dp[l*2] == 0 or dp[l*2] > dp[l]:
-        dp[l*2] = dp[l] + 1
-    
-    if dp[l+1] == 0 or dp[l+1] > dp[l]:
-        dp[l+1] = dp[l] + 1
-
-    l += 1
-
-print(dp[n])
+back()
