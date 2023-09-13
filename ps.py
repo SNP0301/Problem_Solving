e38bb93fdd36b902f5519a5f9aacf373253f1193
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-13 13:45:20 +0900

    [BOJ] 16967 solved

--- ps.py
+++ ps.py
@@ -14,14 +14,20 @@ modified_array = list()
 for i in range(x+h):
     modified_array.append(list(map(int,input().split())))
 
-def check_array(array):
-    for i in array:
-        print(i)
-
-check_array(modified_array)
-
 for i in range(x):
     original_array.append(modified_array[i][:w])
 
+for i in range(h-x):
+    original_array.append([0 for _ in range(w)])
 
-check_array(original_array)
+for i in range(h):
+    for j in range(w):
+        original_array[i][j] = modified_array[i][j]
+
+for i in range(h-x):
+    for j in range(w-y):
+        original_array[i+x][j+y] -= original_array[i][j]
+
+
+for i in original_array:
+    print(*i)
