2a844e1445dbfcbe15b3b7a0842aa7f697d9a1d7
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-18 21:29:37 +0900

    [BOJ] 16931 I/O temp

--- ps.py
+++ ps.py
@@ -15,20 +15,3 @@ print(answer)
 for i in range(n):
     paper.append(list(map(int,input().split())))
 
-for i in range(n):
-    print(max(paper[i])*2)
-    answer += (max(paper[i])*2)
-
-print(answer)
-
-for i in range(m):
-    for j in range(n):## (i,j) = (0,0)(1,0)(2,0) / (0,1)(1,1)(2,1)
-        column_max = max(column_max,paper[j][i])
-        ##print(j,i) round check
-    print("%d column max: %d"%(i,column_max))
-    answer += (column_max*2)
-    column_max = 1
-
-print(answer)
-
-print(answer)
