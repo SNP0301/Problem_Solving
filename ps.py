265d89dfd4f3f9c5f172bd90a1d0f87f3ee0f10b
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-18 16:01:48 +0900

    [BOJ] 14226 temp

--- ps.py
+++ ps.py
@@ -1,18 +1,28 @@
 '''
-[BOJ] 11052. 카드 구매하기
-T: 1초
-M: 256MB
+[BOJ] 14226. 이모티콘
+T: 2초
+M: 512MB
 '''
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 n = int(input())
-card_pack = [0]+ list(map(int,input().split()))
-dp = [0] * (n+1)
+q = deque()
 
-for i in range(n+1):
-    for j in range(n+1):
-        if j-i >= 0:
-            dp[j] = max(dp[j],dp[j-i]+card_pack[i])
+check_array = [i for i in range(10000)]
+time_array = [i for i in range(10000)] ## time_array[n]: n개 이모티콘을 만드는 데에 걸리는 최소 시간
+time = 1
 
-print(dp[n])
+i = 2
+for j in range(n):
+    for i in range(j,2*n+1,i):
+        time_array[2*i] = time_array[i]+1 ## [4]는 [2]에서 2초, #
+        print("%d takes %d"%(2*i,time_array[2*i]))
+
+i = 2
+for i in range(2,2*n):
+    time_array[i] = min(time_array[i+1]+1, time_array[i])
+
+print("\n")
+print(time_array[n])
