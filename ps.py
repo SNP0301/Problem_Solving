3165f3bb795aa51c54da4c9a983179a76ad62484
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-18 11:52:22 +0900

    [BOJ] 11052 temp

--- ps.py
+++ ps.py
@@ -1,20 +1,8 @@
 '''
-[BOJ] 10773. 제로
+[BOJ] 11052. 카드 구매하기
 T: 1초
 M: 256MB
 '''
 import sys
 input = sys.stdin.readline
 
-k = int(input())
-
-jb = list()
-
-for _ in range(k):
-    num = int(input())
-    if num == 0:
-        jb = jb[:-1]
-    else:
-        jb.append(num)
-
-print(sum(jb))
