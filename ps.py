4605de39af0a3d4315a27d96807c4893b5447590
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-18 13:11:43 +0900

    [BOJ] 11052 solved

--- ps.py
+++ ps.py
@@ -6,3 +6,13 @@ M: 256MB
 import sys
 input = sys.stdin.readline
 
+n = int(input())
+card_pack = [0]+ list(map(int,input().split()))
+dp = [0] * (n+1)
+
+for i in range(n+1):
+    for j in range(n+1):
+        if j-i >= 0:
+            dp[j] = max(dp[j],dp[j-i]+card_pack[i])
+
+print(dp[n])
