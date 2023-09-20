3c820286463dd56c21ba4af30858802f69c5b3b9
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-20 11:05:43 +0900

    [BOJ] 13549 REF:(2,7) return 2, not 1

--- ps.py
+++ ps.py
@@ -1,27 +1,48 @@
 '''
-[BOJ] 14501. 퇴사
+[BOJ] 13549. 숨바꼭질 3
 T: 2초
 M: 512MB
 '''
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-n = int(input())
-time_table = list()
-pay_table = list()
-dp = [0] * (n+1)
-max_value = 0
+n, k = map(int,input().split())
+lastIndex = 200000
 
-for i in range(n):
-    time, pay = map(int,input().split())
-    time_table.append(time)
-    pay_table.append(pay)
+graph = [0 for i in range(lastIndex+1)]
+visited = [False for i in range(lastIndex+1)]
 
-for i in range(n-1,-1,-1):
-    t = i + time_table[i]
-    if t <= n:
-        dp[i] = max(pay_table[i] + dp[t], max_value)
-        max_value = dp[i]
-    else:
-        dp[i] = max_value
-print(max_value)
+q = deque()
+
+def bfs(x):
+    q.append(x)
+    graph[x] = 0
+    visited[x] = True
+
+    while q:
+        if visited[k]:
+            print(graph[k])
+            break
+        x = q.popleft()
+        dx = [x-1,x+1,x*2]
+
+        for i in range(3):
+            nextX = dx[i]
+
+            if nextX == 0:
+                visited[nextX] = True
+                graph[nextX] = graph[x]+1
+            elif  nextX < 0 or nextX >= lastIndex+1:
+                continue
+            
+            if not visited[nextX] and i != 2:
+                graph[nextX] = graph[x] + 1
+                visited[nextX] = True
+                q.append(nextX)
+            elif (not visited[nextX] or graph[nextX] >= graph[x])and i == 2:
+                graph[nextX] = graph[x]
+                visited[nextX] = True
+                q.append(nextX)
+
+bfs(n)
