76c2297c403d2ef916f0ab285b5f0e24107cf81d
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-21 12:08:26 +0900

    [BOJ] 1699 tmp

--- ps.py
+++ ps.py
@@ -1,49 +1,34 @@
 '''
-[BOJ] 2529. 부등호
-T: 1s
-M: 256MB
+[BOJ] 1699. 제곱수의 합
+T: 2s
+M: 128MB
 '''
 import sys
+import math
 input = sys.stdin.readline
-MAX_IDX = 10 ## 453 342
 
-k = int(input())
-sign = list(input().split())
-num_list = [i for i in range(0,MAX_IDX)]
 
-## index:int, 채워야 할 자리수
-## num_list:list, 사용할 수 있는 숫자들의 배열
+MAX_IDX = 100000
+n = int(input())
 
-def recur(idx:int, numbers:list, answer:int):
+## 1,4,9,16 ... 
+dp = [999]*(MAX_IDX+1)
+check = [i for i in range(MAX_IDX)]
+dp[1] = 1
+current_dp = 1
 
-    if idx >= k:
-        print(answer)
-    
+for i in range(2,n+1):
+    floor_number = math.floor(math.sqrt(i))
+    basic_number = math.sqrt(i)
+
+    if floor_number == basic_number:
+        dp[i] = 1
+        current_dp = i
+        cnt = 1
+        while cnt*i <= n:
+            dp[i*cnt] = min(dp[i*cnt],cnt)
+            cnt += 1
     else:
-        possible = False
+        dp[i] = min(dp[current_dp] + dp[i-current_dp],dp[i])
 
-        if sign[idx] == ">":
-            for x in numbers:
-                if answer%10 > x:
-                    next_list = numbers
-                    next_list.remove(x)
-                    recur(idx+1, next_list, answer*10+x)
-                    possible = True
-
-        elif sign[idx] == "<":
-            for x in numbers:
-                if answer%10 < x:
-                    next_list = numbers
-                    next_list.remove(x)
-                    recur(idx+1, next_list, answer*10+x)
-                    possible = True
-
-        if not possible:
-            return
-
-
-for i in range(MAX_IDX):
-    first_list = [i for i in range(0,MAX_IDX)]
-    first_list.remove(i)
-    print(i, first_list)
-    recur(0,first_list,i)
+print(dp[n])
