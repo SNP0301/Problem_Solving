0bf25d9be71f01fba4807102241b40cae8b75cb6
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-21 17:37:06 +0900

    [BOJ] 2529 solved

--- ps.py
+++ ps.py
@@ -1,34 +1,58 @@
 '''
-[BOJ] 1699. 제곱수의 합
-T: 2s
-M: 128MB
+[BOJ] 2529. 부등호
+T: 1s
+M: 256MB
 '''
 import sys
-import math
 input = sys.stdin.readline
+MAX_IDX = 10 ## 453 342
 
+k = int(input())
+sign = list(input().split())
+num_list = [i for i in range(0,MAX_IDX)]
+answer_list = list()
+## index:int, 채워야 할 자리수
+## num_list:list, 사용할 수 있는 숫자들의 배열
 
-MAX_IDX = 100000
-n = int(input())
-
-## 1,4,9,16 ... 
-dp = [999]*(MAX_IDX+1)
-check = [i for i in range(MAX_IDX)]
-dp[1] = 1
-current_dp = 1
-
-for i in range(2,n+1):
-    floor_number = math.floor(math.sqrt(i))
-    basic_number = math.sqrt(i)
-
-    if floor_number == basic_number:
-        dp[i] = 1
-        current_dp = i
-        cnt = 1
-        while cnt*i <= n:
-            dp[i*cnt] = min(dp[i*cnt],cnt)
-            cnt += 1
+def recur(idx:int, input_array:list, answer:int):
+    numbers = input_array
+    if idx >= k:
+        answer_list.append(answer)
+        return
     else:
-        dp[i] = min(dp[current_dp] + dp[i-current_dp],dp[i])
+        
+        possible = False
 
-print(dp[n])
+        if sign[idx] == ">":
+            for x in numbers:
+                if answer%10 > x:
+                    #print("after %d, %d is small"%(answer%10,x))
+                    next_list = [i for i in numbers]
+                    next_list.remove(x)
+                    recur(idx+1, next_list, answer*10+x)
+                    possible = True
+
+        elif sign[idx] == "<":
+            for x in numbers:
+                if answer%10 < x:
+                    #print("after %d, %d is large"%(answer%10,x))
+                    next_list = [i for i in numbers]
+                    next_list.remove(x)
+                    recur(idx+1, next_list, answer*10+x)
+                    possible = True
+
+        if not possible:
+            return
+
+
+for i in range(MAX_IDX):
+    first_list = [i for i in range(0,MAX_IDX)]
+    first_list.remove(i)
+    recur(0,first_list,i)
+
+print(max(answer_list))
+
+if sign[0] == "<":
+    print("0"+str(min(answer_list)))
+else:
+    print(min(answer_list))
