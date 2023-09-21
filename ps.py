02252b22a153127f5a6c42534e2a2dbb4e25db20
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-21 10:28:07 +0900

    [BOJ] 2529 solved

--- ps.py
+++ ps.py
@@ -1,36 +1,26 @@
 '''
 [BOJ] 2529. 부등호
-T: 1초
+T: 1s
 M: 256MB
 '''
 import sys
 input = sys.stdin.readline
+MAX_IDX = 5
 
-n = int(input())
-sign = input().split()
-numbers = [i for i in range(0,10)]
-print(numbers[1:])
+k = int(input())
+sign = list(input().split())
+num_list = [i for i in range(0,MAX_IDX)]
 
 
-def recur(idx:int,numbers:list,answer:int): ## 숫자의 idx번째를 채워야하는데, 재료는 numbers가 있다. 부등호에 맞는 숫자(x)가 있으면 answer = answer*10+x로 recur해라
-    if idx == n:
+## index:int, 채워야 할 자리수
+## num_list:list, 사용할 수 있는 숫자들의 배열
+## 
+
+def recur(idx:int, numbers:list, answer:int):
+    if idx > k:
         print(answer)
-    possible = False
-    if sign[idx-1] == ">":
-        for x in numbers:
-            if answer%10 > x:
-                next_numbers = numbers.remove(x)
-                recur(idx+1,next_numbers,answer*10+x)
-                possible = True
-    elif sign[idx-1] == "<": ## sign=="<"
-        for x in numbers:
-            if answer%10 < x:
-                next_numbers = numbers.remove(x)
-                recur(idx+1,next_numbers,answer*10+x)
-                possible = True
-    if not possible:
-        return
+    else:
+        print(idx)
 
-for i in range(1,10):
-    first_numbers = numbers.remove(i)
-    recur(1,first_numbers,i)
+
+recur(0,num_list,0)
