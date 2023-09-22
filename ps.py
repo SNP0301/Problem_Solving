1038c0f682c98fa6d003d2c198c49b0f620d57e8
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-22 15:33:06 +0900

    [BOJ] 1912 Mem ERROR

--- ps.py
+++ ps.py
@@ -1,55 +1,27 @@
 '''
-[BOJ] 14889. 스타트와 링크
-
-T: 2s
-M: 512MB
+[BOJ] 1912. 연속합
+T: 1s
+M: 128MB
 '''
 import sys
 input = sys.stdin.readline
-from itertools import combinations
+sys.setrecursionlimit(10**6)
 
 n = int(input())
-stat_max = -1
-stat_min = 10001
-start_stat = 0
-link_stat = 0
-stat = list()
-entries = list()
-member_list = [i for i in range(n)]
+arr = list(map(int,input().split()))
+dp = list()
 
-for _ in range(n):
-    stat.append(list(map(int,input().split())))
+dp.append(arr)
 
-for i in combinations(member_list, n//2):
-    entries.append(list(i))
+## i: 1,2,3,...,n-1
+answer = -99999
+cnt = 2
+for i in range(1,n):
+    dp.append([])
+    for j in range(n-i):
+        ##print(i,j,dp[i-1][j],arr[j+cnt-1])
+        dp[i].append(dp[i-1][j]+arr[j+cnt-1])
+    cnt += 1
+    answer = max(answer,max(dp[i]))
 
-##print(len(entries)) ## nCn//2
-#print(entries)
-
-stat_diff = 99999
-for e in entries:
-    stat_min = 10001
-
-    #print(e)
-    start = e
-    link = list()
-    for m in member_list:
-        if m not in e:
-            link.append(m)
-
-    for u in range(n):
-        for v in range(n):
-            if u in start and v in start:
-                start_stat += stat[u][v]
-            elif u in link and v in link:
-                link_stat += stat[u][v]
-    #print(start, start_stat)
-    #print(link, link_stat)
-    stat_diff = min(max(start_stat-link_stat, link_stat-start_stat),stat_diff)
-    #print(stat_diff, stat_min,"\n")
-
-
-    start_stat = 0
-    link_stat = 0
-
-print(stat_diff)
+print(max(answer,max(arr)))
