9ab7db394fd1798dba9d86c7b42fe05b41b47dbe
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-25 11:42:16 +0900

    [BOJ] 10974 solved

--- ps.py
+++ ps.py
@@ -1,37 +1,27 @@
 '''
-[BOJ] 14226. 이모티콘
-T: 2s
-M: 512MB
+[BOJ] 10974. 모든 순열
+T: 1s
+M: 128MB
 '''
 import sys
 input = sys.stdin.readline
-from collections import deque
+sys.setrecursionlimit(10**6)
+
 
 n = int(input())
-MAX_INDEX = n*2+1
+input_arr = [i for i in range(1,n+1)]
 
-graph = [i for i in range(MAX_INDEX)]
-graph[1] = 0
+def recur(answer_arr, set_arr):
+    if len(answer_arr) == n:
+        print(*answer_arr)
+    
+    else:
+        for i in set_arr:
+            if i in answer_arr:
+                continue
+            answer_arr.append(i)
+            recur(answer_arr,set_arr)
+            answer_arr.remove(i)
 
-def bfs(x):
-    q = deque()
-    q.append(x)
+recur(list(),input_arr)
 
-    while q:
-        x = q.popleft()
-        i = 1
-        while x*i < MAX_INDEX:
-            if i == 2:
-                graph[x*i] = min(graph[x*i],graph[x]+2)
-                i += 1
-            else:
-                if graph[x*i] > graph[x]+1:
-                    graph[x*i] = graph[x]+i
-                    q.append(x*i)
-                if graph[x*i-1] > graph[x*i]+1:
-                    graph[x*i-1] = graph[x*i]+1
-                i += 1
-
-bfs(1)
-print(graph)
-print(graph[n])
