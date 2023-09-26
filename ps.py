019c2933bc307d64797bf496cf84b03ebd60e71b
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-26 16:31:23 +0900

    [BOJ] 10844 solved

--- ps.py
+++ ps.py
@@ -1,27 +1,26 @@
 '''
-[BOJ] 10974. 모든 순열
+[BOJ] 10844. 쉬운 계단 수
 T: 1s
-M: 128MB
+M: 256MB
 '''
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
-
 n = int(input())
-input_arr = [i for i in range(1,n+1)]
 
-def recur(answer_arr, set_arr):
-    if len(answer_arr) == n:
-        print(*answer_arr)
-    
-    else:
-        for i in set_arr:
-            if i in answer_arr:
-                continue
-            answer_arr.append(i)
-            recur(answer_arr,set_arr)
-            answer_arr.remove(i)
+dp = [ [0 for _ in range(10)] for _ in range(n+1)]
 
-recur(list(),input_arr)
+for i in range(1,10):
+    dp[1][i] = 1
 
+for i in range(2,n+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][8]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+
+print((sum(dp[n])%1000000000))
