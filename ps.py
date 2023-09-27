5b8b1faaa77b836d77a8fa14df8b22f3e20a209e
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-27 12:08:27 +0900

    [BOJ] ***** solution refer

--- ps.py
+++ ps.py
@@ -1,26 +1,35 @@
 '''
-[BOJ] 10844. 쉬운 계단 수
-T: 1s
+[BOJ] 1107. 리모컨
+T: 2s
 M: 256MB
 '''
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 n = int(input())
+m = int(input())
 
-dp = [ [0 for _ in range(10)] for _ in range(n+1)]
+if m == 0:
+    broken_button = list()
+else:
+    broken_button = list(map(int,input().split()))
 
-for i in range(1,10):
-    dp[1][i] = 1
+available_button = {i for i in range(0,10)} - set(broken_button)
 
-for i in range(2,n+1):
-    for j in range(10):
-        if j == 0:
-            dp[i][j] = dp[i-1][1]
-        elif j == 9:
-            dp[i][j] = dp[i-1][8]
-        else:
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+def is_possible_channel(channel, available_button):
+    for i in str(channel):
+        if int(i) not in available_button:
+            return False
+    return True ### 숫자만으로 갈 수 있으면 False, +/- 조작해야 하면 False
 
-print((sum(dp[n])%1000000000))
+def solution(goal, available_button):
+    current_min = abs(goal-100)
+
+    for channel in range(1000001):
+        if is_possible_channel(channel,available_button):
+            current_press = abs(goal-channel) + len(str(channel))
+            current_min = min(current_min, current_press)
+    print(current_min)
+    return
+
+solution(n,available_button)
