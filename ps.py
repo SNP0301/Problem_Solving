219c659d88baf72a2066f867c13d47f9503dcb44
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-27 13:31:42 +0900

    [BOJ] 2839 WRONG

--- ps.py
+++ ps.py
@@ -1,42 +1,23 @@
 '''
-[BOJ] 1759. 암호 만들기
-T: 2s
+[BOJ] 2839. 설탕 배달
+T: 1s
 M: 128MB
 '''
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-l, c = map(int,input().split())
-input_character = sorted(list(map(str,input().split())))
-vowel = ["a","e","i","o","u"]
+n = int(input())
 
-def make_password(password, available_character):
-    vowel_check = False
-    if len(password) == l:
-        '''
-        for x in vowel:
-            if x in password:
-                print(password)
-                return
-        '''
-        not_vowel_cnt = 0
-        for x in password:
-            if x in vowel:
-                vowel_check = True
-            elif x not in vowel:
-                not_vowel_cnt += 1
-        if vowel_check and (not_vowel_cnt >= 2):
-            print(password)
-            return
-    for i in available_character:
-        if not password:
-            next_character_list = [y for y in available_character]
-            next_character_list.remove(i)
-            make_password(i, next_character_list)
-        elif password[-1] < i:
-            next_character_list = [y for y in available_character]
-            next_character_list.remove(i)
-            make_password(password+i, next_character_list)
+def sugar(num):
+    total = 2
+    while total<=num:
+        for for_five in range(0,total+1):
+            for_three = total-for_five
+            #print("%d = %d + %d"%(total,for_five,for_three))
+            if for_five*5 + for_three*3 == num:
+                return for_five + for_three
+        total += 1
+    return -1
 
-make_password("",input_character)
+
+print(sugar(n))
