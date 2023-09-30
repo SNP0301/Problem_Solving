f7b5af2d1d4d0acb82bd133dd77478ad96c8f562
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-09-30 18:29:14 +0900

    [BOJ] 1181 solved

--- ps.py
+++ ps.py
@@ -1,18 +1,23 @@
 '''
-[BOJ] 10989. 수 정렬하기 3
-T: 5초
-M: 8MB
+[BOJ] 1181. 단어 정렬 
+T: 2초
+M: 256MB
 '''
 import sys
 input = sys.stdin.readline
 
-num_arr = [0 for _ in range(10001)]
-
 n = int(input())
+word_set = set()
 for _ in range(n):
-    num_arr[int(input())] += 1
+    word_set.add(input().rstrip())
 
-for i in range(10001):
-    if num_arr[i] != 0:
-        for j in range(num_arr[i]):
-            print(i)
+word_list = list(word_set)
+answer_list = list()
+
+for i in range(len(word_list)):
+    answer_list.append([word_list[i],len(word_list[i])])
+
+answer_list.sort(key = lambda x: (x[1],x[0]))
+
+for i in range(len(word_list)):
+    print(answer_list[i][0])
