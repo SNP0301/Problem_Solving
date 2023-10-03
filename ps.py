34e48b261e9f96260cf2898df02853495efe0a4f
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-03 21:55:33 +0900

    [BOJ] 2741 solved

--- ps.py
+++ ps.py
@@ -1,17 +1,10 @@
 '''
-[BOJ] 18870. 좌표 압축
-T: 2초
-M: 512MB
+[BOJ] 2741. N 찍기
+T: 1s
+M: 128MB
 '''
 import sys
 input = sys.stdin.readline
-
-x = input()
-cnt = 0
-
-for i in range(len(x)):
-    print(x[i],end="")
-    cnt += 1
-    if cnt == 10:
-        print("\n",end="")
-        cnt = 0
+for i in range(1,int(input())+1):
+    print(i)
+               
