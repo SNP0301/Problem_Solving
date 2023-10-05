9da376628011c4bce189422ab58355b1212d9eb1
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-05 12:54:35 +0900

    [BOJ] 1912 solved

--- ps.py
+++ ps.py
@@ -1,30 +1,17 @@
 '''
-[BOJ] 15990. 1,2,3 더하기 5
+[BOJ] 1912. 연속합
 T: 1s
-M: 512MB
+M: 128MB
 
-dp[i][j]의 의미: i를 1~3의 숫자를 조합해 만들 때, 첫 숫자가 j인 경우의 수
+dp[i][j]의 의미: 
 '''
 import sys
 input=sys.stdin.readline
 
-MAX_IDX = 100000
+n = int(input())
+data = list(map(int, input().split()))
 
-dp = [[0]*4 for _ in range(MAX_IDX+1)]
-
-dp[1] = [0,1,0,0]
-dp[2] = [0,0,1,0]
-dp[3] = [0,1,1,1]
-
-for i in range(4,MAX_IDX+1):
-    dp[i][1] = dp[i-1][2] + dp[i-1][3]
-    dp[i][2] = dp[i-2][1] + dp[i-2][3]
-    dp[i][3] = dp[i-3][1] + dp[i-3][2]
-
-    dp[i][1] %= 1000000009
-    dp[i][2] %= 1000000009
-    dp[i][3] %= 1000000009
-
-t = int(input())
-for _ in range(t):
-    print(sum(dp[int(input())])%1000000009)
+for i in range(1, n):
+  data[i] = max(data[i], data[i] + data[i-1])
+  
+print(max(data))
