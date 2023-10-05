a4d048f895ad20504bf5f81fce8633c4abc9814c
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-05 11:05:42 +0900

    [BOJ] 6603 solved

--- ps.py
+++ ps.py
@@ -1,33 +1,35 @@
 '''
-[BOJ] 15990. 1,2,3 더하기 5
+[BOJ] 6603. 로또
 T: 1s
-M: 512MB
+M: 128MB
 '''
 import sys
 input=sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-max=100000
-mod=1000000009
+def show(K:int, S:list, is_visited:list):
+    for i in range(0,K):
+        if is_visited[i]:
+            print(S[i], end=' ')
+    print()
 
+def dfs(l,arr,visited,i,cnt):
+    if cnt == 6:
+        show(l,arr,visited)
+        return
+    for x in range(i,l):
+        if visited[x]:
+            continue
+        visited[x] = True
+        dfs(l,arr,visited,x,cnt+1)
+        visited[x] = False
 
-dp=[[0]*4 for _ in range(max+1)]
-
-dp[1]=[0,1,0,0]
-dp[2]=[0,0,1,0]
-dp[3]=[0,1,1,1]
-
-for i in range(4, max+1):
-    dp[i][1]=dp[i-1][2] +dp[i-1][3]
-    dp[i][2]=dp[i-2][1] +dp[i-2][3]
-    dp[i][3]=dp[i-3][1] +dp[i-3][2]
-
-
-    dp[i][1]%=mod
-    dp[i][2]%=mod
-    dp[i][3]%=mod
-
-
-t=int(input())
-for _ in range(t):
-    n=int(input())
-    print(sum(dp[n])%mod)
+while True:
+    arr = list(map(int,input().split()))
+    if arr[0] == 0:
+        break
+    else:
+        l = arr[0]
+        visited = [False for _ in range(l)]
+        dfs(l,arr[1:],visited,0,0)
+        print()
