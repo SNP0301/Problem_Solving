ea8b6003290e5193d0b7c4eb159114debe15a33b
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-10 18:07:41 +0900

    [BOJ] 24416 solved

--- ps.py
+++ ps.py
@@ -1,6 +1,19 @@
-N = int(input())
+'''
+[BOJ] 24416. 알고리즘 수업 - 피보나치 수 1
+T: 1s
+M: 512MB
+'''
+import sys
+input = sys.stdin.readline
 
-for i in range(N,0,-1):
-    for j in range(1,i+1):
-        print("*",end="")
-    print("\n",end="")
+n = int(input())
+accum = 0
+def fib(n):
+    global accum
+    if (n == 1 or n == 2):
+        accum += 1
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+
+print(fib(n), n-2)
