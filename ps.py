42b27205f83a7292f22756f461c5979fcd7f5692
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-11 09:01:56 +0900

    [BOJ] merge sort outline

--- ps.py
+++ ps.py
@@ -1,18 +1,47 @@
 '''
-[BOJ] 9461. 파도반 수열
+[BOJ] 27433. 팩토리얼 2
 T: 1s
-M: 128MB
+M: 1024MB
 '''
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**7)
 
-t = int(input())
-dp = [0,1,1,1] + [0 for _ in range(99)]
+n, k = map(int,input().split())
+a = list(map(int,input().split()))
 
-for i in range(4,101):
-    dp[i] = dp[i-2]+dp[i-3]
+print(a)
 
-for _ in range(t):
-    n = int(input())
-    print(dp[n])
+def merge_sort(arr,p,r):
+    if arr[p] < arr[r]:
+        q = (p+r)//2
+        merge_sort(arr,p,q)
+        merge_sort(arr,q+1,r)
+        merge(arr,p,q,r)
+
+def merge(arr,p,q,r):
+    i = p
+    j = q+1
+    t = 1
+    tmp = [0 for _ in range(r)]
+    while (i<=q and j <= r):
+        if (arr[i]<=arr[j]):
+            tmp[t] = arr[i]
+            t += 1
+            i += 1
+        else:
+            tmp[t] = arr[j]
+            t += 1
+            j += 1
+    while (i<=q):
+        tmp[t] = arr[i]
+        i += 1
+    while (j <= r):
+        tmp[t] = arr[j]
+        j += 1
+    i = p
+    t = 1
+    while (i <= r):
+        arr[i] = tmp[t]
+        i += 1
+        t += 1
