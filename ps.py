4e2693c2daa1ee7e3635dd27b2ce512da803c5c0
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-12 09:36:23 +0900

    [BOJ] 1932 solved

--- ps.py
+++ ps.py
@@ -1,38 +1,23 @@
 '''
-[BOJ] 4779. 칸토어 집합
-T: 1s
+[BOJ] 1932. 정수 삼각형
+T: 2s
 M: 128MB
 '''
 import sys
-sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
 
+n = int(input())
+triangle = list()
+for _ in range(n):
+    triangle.append(list(map(int,input().split())))
 
-def cantorian(arr):
-    if (len(arr) == 1):
-        new_arr.append(arr[0])
-        return
-
-    tres = len(arr)//3
-    for i in range(len(arr)):
-        if i >= tres and i < tres*2:
-            arr[i] = " "
-
-    cantorian(arr[:tres])
-    cantorian(arr[tres:tres*2])
-    cantorian(arr[tres*2:])
-
-while True:
-    try:
-        n = int(input())
-        if n == 0:
-            print("-")
+for i in range(1,n):
+    for j in range(i+1):
+        if j == 0:
+            triangle[i][j] = triangle[i][j]+triangle[i-1][j]
+        elif j == i:
+            triangle[i][j] = triangle[i][j]+triangle[i-1][j-1]
         else:
-            arr = ["-" for _ in range(3**n)]
-            new_arr = list()
-            cantorian(arr)
-            #cantorian(arr)
-            for x in new_arr:
-                print(x,end="")
-            print("")
-    except EOFError:
-        break
+            triangle[i][j] = triangle[i][j]+max(triangle[i-1][j],triangle[i-1][j-1])
+
+print(max(triangle[n-1]))
