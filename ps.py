eae94a1abde7996f474c4805d4fbe74349a61f83
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-13 09:50:59 +0900

    [BOJ] 18870 solved

--- ps.py
+++ ps.py
@@ -1,23 +1,21 @@
 '''
-[BOJ] 1932. 정수 삼각형
-T: 2s
-M: 128MB
+[BOJ] 18870. 좌표 압축
+T: 2초
+M: 512MB
 '''
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-triangle = list()
-for _ in range(n):
-    triangle.append(list(map(int,input().split())))
+points = list(map(int,input().split()))
+points_set = set(points)
 
-for i in range(1,n):
-    for j in range(i+1):
-        if j == 0:
-            triangle[i][j] = triangle[i][j]+triangle[i-1][j]
-        elif j == i:
-            triangle[i][j] = triangle[i][j]+triangle[i-1][j-1]
-        else:
-            triangle[i][j] = triangle[i][j]+max(triangle[i-1][j],triangle[i-1][j-1])
+points_list = list(points_set)
+points_list.sort()
+points_dict = dict()
 
-print(max(triangle[n-1]))
+for i in range(len(points_list)):
+    points_dict.update({points_list[i]:i})
+
+for i in points:
+    print(points_dict[i],end=" ")
