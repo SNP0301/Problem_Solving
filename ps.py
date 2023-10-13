8e6704d6dc88d718801245b0af036ee23be9d2c3
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-13 10:04:29 +0900

    [BOJ] 15988 solved

--- ps.py
+++ ps.py
@@ -1,21 +1,21 @@
 '''
-[BOJ] 18870. 좌표 압축
-T: 2초
+[BOJ] 15988. 1,2,3 더하기 3
+T: 1초
 M: 512MB
 '''
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-points = list(map(int,input().split()))
-points_set = set(points)
+t = int(input())
 
-points_list = list(points_set)
-points_list.sort()
-points_dict = dict()
+dp = [0 for _ in range(1000002)]
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
 
-for i in range(len(points_list)):
-    points_dict.update({points_list[i]:i})
+for i in range(4,1000001):
+        dp[i] = (dp[i-1] + dp[i-2] + dp[i-3])%1000000009
 
-for i in points:
-    print(points_dict[i],end=" ")
+for _ in range(t):
+    n = int(input())
+    print(dp[n]%1000000009)
