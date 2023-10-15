426e0d0a88bd531bf7c74b7d5592fee65d3ec9e8
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-15 15:29:57 +0900

    [BOJ] 9012 solved

--- ps.py
+++ ps.py
@@ -1,20 +1,42 @@
 '''
-[BOJ] 1026. 보물
-T: 2초
+[BOJ] 9012. 괄호
+T: 1초
 M: 128MB
 '''
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-a = list(map(int,input().split()))
-b = list(map(int,input().split()))
-answer = 0
+ps_open = []
 
-a.sort(reverse=True)
-b.sort()
+stack = [1,2,3]
 
-for i in range(n):
-    answer += a[i]*b[i]
+t = int(input())
 
-print(answer)
+for _ in range(t):
+    ps = input()
+    ps_open = list()
+    possible = True
+    for i in range(len(ps)):
+        if ps[i] == "(":
+            ps_open.append("(")
+        elif ps[i] == ")":
+            if ps_open:
+                current = ps_open[-1]
+                if current == "(":
+                    ps_open.pop()
+                i += 1
+            elif not ps_open:
+                possible = False
+                i += 1
+                break
+
+    
+    if ps_open:
+        possible = False
+    
+    if possible:
+        print("YES")
+    else:
+        print("NO")
+
+            
