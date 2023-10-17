a977191e9a35b3bdf23c852f206c00ce35048ae7
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-17 10:02:30 +0900

    [BOJ] 2669 solved

--- ps.py
+++ ps.py
@@ -1,20 +1,28 @@
 '''
-[BOJ] 11047. 동전 0
+[BOJ] 2669. 직사각형 네개의 합집합의 면적 구하기
 T: 1초
-M: 256MB
+M: 128MB
 '''
 import sys
 input = sys.stdin.readline
 
-n, k = map(int,input().split())
-coins = list()
+board = [[0 for _ in range(100)] for _ in range(100)]
 answer = 0
 
-for _ in range(n):
-    coins.append(int(input()))
+for _ in range(4):
+    points = list(map(int, input().split()))
+    x_one = points[0]
+    y_one = points[1]
+    x_two = points[2]
+    y_two = points[3]
 
-for i in range(n-1,-1,-1):
-    answer += k//coins[i]
-    k -= (k//coins[i])*coins[i]
+    for i in range(x_one,x_two,1):
+        for j in range(y_one,y_two,1):
+            if board[i][j] == 0:
+                board[i][j] = 1
+                answer += 1
 
 print(answer)
+
+
+
