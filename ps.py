7e931d22fabf149701621c0d74e66c494c64eb62
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-18 08:53:50 +0900

    [BOJ] 18258 solved

--- ps.py
+++ ps.py
@@ -1,28 +1,42 @@
 '''
-[BOJ] 2669. 직사각형 네개의 합집합의 면적 구하기
-T: 1초
-M: 128MB
+[BOJ] 18258. 큐 2
+T: 1 s
+M: 512 MB
 '''
+
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-board = [[0 for _ in range(100)] for _ in range(100)]
-answer = 0
+n = int(input())
+q = deque()
 
-for _ in range(4):
-    points = list(map(int, input().split()))
-    x_one = points[0]
-    y_one = points[1]
-    x_two = points[2]
-    y_two = points[3]
-
-    for i in range(x_one,x_two,1):
-        for j in range(y_one,y_two,1):
-            if board[i][j] == 0:
-                board[i][j] = 1
-                answer += 1
-
-print(answer)
-
-
-
+for _ in range(n):
+    command = list(input().split())
+    if len(command) == 2:
+        x = command[1]
+    if command[0] == 'push':
+        q.append(x)
+    elif command[0] == 'pop':
+        if not q:
+            print(-1)
+        else:
+            print(q[0])
+            q.popleft()
+    elif command[0] == 'size':
+        print(len(q))
+    elif command[0] == 'empty':
+        if not q:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if not q:
+            print(-1)
+        else:
+            print(q[0])
+    elif command[0] == 'back':
+        if not q:
+            print(-1)
+        else:
+            print(q[-1])
