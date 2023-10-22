3f12b5afed52b2f26c38b1fc67e380e267294972
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-22 22:57:07 +0900

    [BOJ] 2083 solved

--- ps.py
+++ ps.py
@@ -1,19 +1,18 @@
 '''
-[BOJ] 27889. 특별한 학교 이름
+[BOJ] 2083. 럭비 클럽
 T: 1 s
-M: 512 MB
+M: 128 MB
 '''
 
 import sys
 input = sys.stdin.readline
 
-n = input().rstrip()
+while True:
+    member = list(input().split())
+    if member[0] == '#' and member[1] == '0' and member[2] == '0':
+        exit()
 
-if n == "NLCS":
-    print("North London Collegiate School")
-elif n == "BHA":
-    print("Branksome Hall Asia")
-elif n == "KIS":
-    print("Korea International School")
-elif n == "SJA":
-    print("St. Johnsbury Academy")
+    if int(member[1]) > 17 or int(member[2]) >= 80:
+        print(member[0],"Senior")
+    else:
+        print(member[0],"Junior")
