590708f08157b117d536773950609fcb3211cf09
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-23 08:56:58 +0900

    [BOJ] 2752 solved

--- ps.py
+++ ps.py
@@ -1,5 +1,5 @@
 '''
-[BOJ] 2083. 럭비 클럽
+[BOJ] 2752. 세수정렬
 T: 1 s
 M: 128 MB
 '''
@@ -7,12 +7,8 @@ M: 128 MB
 import sys
 input = sys.stdin.readline
 
-while True:
-    member = list(input().split())
-    if member[0] == '#' and member[1] == '0' and member[2] == '0':
-        exit()
+s = list(map(int,input().split()))
+s.sort()
 
-    if int(member[1]) > 17 or int(member[2]) >= 80:
-        print(member[0],"Senior")
-    else:
-        print(member[0],"Junior")
+for i in s:
+    print(i,end=" ")
