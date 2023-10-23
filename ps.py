52b6e1f8fedbcfda807e800cc1fa53bc92584373
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-24 08:21:17 +0900

    [BOJ] 5532 solved

--- ps.py
+++ ps.py
@@ -1,5 +1,5 @@
 '''
-[BOJ] 2752. 세수정렬
+[BOJ] 5532. 방학 숙제
 T: 1 s
 M: 128 MB
 '''
@@ -7,8 +7,20 @@ M: 128 MB
 import sys
 input = sys.stdin.readline
 
-s = list(map(int,input().split()))
-s.sort()
+l = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
 
-for i in s:
-    print(i,end=" ")
+if a%c == 0:
+    k = a//c
+else:
+    k = a//c+1
+
+if b%d == 0:
+    m = b//d
+else:
+    m = b//d+1
+
+print(l-max(k,m))
