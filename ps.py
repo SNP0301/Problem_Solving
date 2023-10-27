55aec4827d8db4961b77d278206f33297c755f16
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-27 09:49:01 +0900

    [BOJ] 1075 solved

--- ps.py
+++ ps.py
@@ -1,5 +1,5 @@
 '''
-[BOJ] 5596. 시험 점수
+[BOJ] 1075. 나누기
 T: 1 s
 M: 128 MB
 '''
@@ -7,7 +7,14 @@ M: 128 MB
 import sys
 input = sys.stdin.readline
 
-m = sum(list(map(int,input().split())))
-s = sum(list(map(int,input().split())))
-
-print(max(m,s))
+n = int(input())//100*100
+n_last_two_digit = str(n)[-2:]
+f = int(input())
+answer = 0
+for i in range(100):
+    if (i+n)%f == 0:
+        if i<= 9:
+            print('0'+str(i))
+        else:
+            print(i)
+        break
