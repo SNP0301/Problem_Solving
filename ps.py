20425ef3c2a6b78f626d49b21fd7703c50b1a94e
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-29 11:46:33 +0900

    [BOJ] 10768 solved

--- ps.py
+++ ps.py
@@ -1,5 +1,5 @@
 '''
-[BOJ] 1076. 저항
+[BOJ] 10768. 특별한 날
 T: 1 s
 M: 128 MB
 '''
@@ -7,17 +7,12 @@ M: 128 MB
 import sys
 input = sys.stdin.readline
 
-f = input().rstrip()
-s = input().rstrip()
-t = input().rstrip()
+m = int(input())
+d = int(input())
 
-color_name = ['black','brown','red','orange','yellow','green','blue','violet','grey','white']
-color_value = [i for i in range(0,10)]
-color_multiplier = [10**i for i in range(10)]
-
-before_multiple = int(color_name.index(f))*10+int(color_name.index(s))
-
-for i in range(len(color_name)):
-    if t == color_name[i]:
-        print(before_multiple*color_multiplier[i])
-        break
+if (m<2) or (m <=2 and d<18): 
+    print("Before")
+elif m==2 and d == 18:
+    print("Special")
+else:
+    print("After")
