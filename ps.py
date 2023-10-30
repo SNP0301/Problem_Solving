fe76f38996d6e39ffb0740e3c26574448f886116
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-10-30 15:50:29 +0900

    [BOJ] 1436 temp

--- ps.py
+++ ps.py
@@ -1,18 +1,35 @@
 '''
-[BOJ] 10768. 특별한 날
-T: 1 s
+[BOJ] 1436. 영화감독 숌
+T: 2 s
 M: 128 MB
 '''
 
 import sys
 input = sys.stdin.readline
 
-m = int(input())
-d = int(input())
+n = int(input())
+jm = ['0']
+number = 666
+def isDoom(x):    
+    x_str = str(x)
+    x_len = len(x_str)
+    cnt = 0
+    if x_len < 3:
+        return False
+    else:
+        while cnt+3 <= x_len:
+            if int(x_str[cnt:3+cnt]) == 666:
+                return True
+            else:
+                cnt += 1
 
-if (m<2) or (m <=2 and d<18): 
-    print("Before")
-elif m==2 and d == 18:
-    print("Special")
-else:
-    print("After")
+
+while True:
+    if isDoom(number):
+        jm.append(number)
+        #print(jm)
+    
+    number += 1
+    if n == len(jm)-1:
+        print(jm[-1])
+        break
