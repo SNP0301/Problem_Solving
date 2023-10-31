09d2029335e1e8138ab7cac7ab1a74225d77b8f5
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-11-01 07:44:29 +0900

    [BOJ] 1247 solved

--- ps.py
+++ ps.py
@@ -1,21 +1,20 @@
 '''
-[BOJ] 1003. 피보나치 함수
-T: 0.25 s
-M: 128 MB
+[BOJ] 1247. 부호
+T: 2 s
+M: 256 MB
 '''
+
 import sys
 input = sys.stdin.readline
 
-
-zero_call = [1,0]
-one_call = [0,1]
-
-for i in range(2,41):
-    zero_call.append(zero_call[i-1]+zero_call[i-2])
-    one_call.append(one_call[i-1]+one_call[i-2])
-
-t = int(input())
-
-for _ in range(t):
+for _ in range(3):
     n = int(input())
-    print(zero_call[n], one_call[n])
+    cnt = 0
+    for i in range(n):
+        cnt += int(input())
+    if cnt == 0:
+        print(0)
+    elif cnt < 0:
+        print("-")
+    else:
+        print("+")
