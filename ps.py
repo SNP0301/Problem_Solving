c8986317bc8d3f266a7127bcfe84c21eab32a665
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-11-05 21:12:06 +0900

    [BOJ] 11945 solved

--- ps.py
+++ ps.py
@@ -1,5 +1,5 @@
 '''
-[BOJ] 9093. 단어 뒤집기
+[BOJ] 11943. 파일 옮기기
 T: 1 s
 M: 128 MB
 '''
@@ -7,11 +7,11 @@ M: 128 MB
 import sys
 input = sys.stdin.readline
 
-t = int(input())
-for _ in range(t):
-    words = list(input().split())
-    for i in words:
-        for j in range(len(i)-1,-1,-1):
-            print(i[j],end="")
-        print(" ",end="")
-    print()
+n,m = map(int,input().split())
+
+
+for _ in range(n):
+    bread = input().strip()
+    for i in range(len(bread)-1,-1,-1):
+        print(bread[i],end="")
+    print("")
