1b147eeacb171c91ef7fe65a6070d98eed0a9e6c
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-11-08 08:52:15 +0900

    [BOJ] 1267 solved

--- ps.py
+++ ps.py
@@ -1,17 +1,28 @@
 '''
-[BOJ] 2455. 지능형 기차
-T: 1 s
+[BOJ] 1267. 핸드폰 요금
+T: 2 s
 M: 128 MB
 '''
 
 import sys
 input = sys.stdin.readline
 
-h = list()
-c = 0
-for _ in range(4):
-    d, u = map(int,input().split())
-    c += u-d
-    h.append(c)
+n = int(input())
+arr = list(map(int, input().split()))
 
-print(max(h))
+y = 0
+m = 0
+
+for i in range(n):
+    y += arr[i]//30+1
+    m += arr[i]//60+1
+
+if y*10 == m*15:
+    print("Y M", y*10)
+elif y*10 < m*15:
+    print("Y", y*10)
+else:
+    print("M", m*15)
+
+
+
