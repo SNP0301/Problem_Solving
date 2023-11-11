e77c2ee8b0905313eca12ccaa547876513239435
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-11-11 20:50:47 +0900

    [BOJ] 16430 solved

--- ps.py
+++ ps.py
@@ -1,5 +1,5 @@
 '''
-[BOJ] 2953. 나는 요리사다
+[BOJ] 16430. 제리와 톰
 T: 1 s
 M: 128 MB
 '''
@@ -7,10 +7,5 @@ M: 128 MB
 import sys
 input = sys.stdin.readline
 
-score = list()
-
-for i in range(5):
-    score.append(sum(list(map(int,input().split()))))
-
-
-print(score.index(max(score))+1, max(score))
+a, b = map(int,input().split())
+print(b-a, b)
