f80665261dc41ec627525f74a8dba3a8cb2b05de
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-11-22 20:13:39 +0900

    [BOJ] 20492 solved

--- ps.py
+++ ps.py
@@ -1,5 +1,5 @@
 '''
-[BOJ] 27918. 탁구 경기
+[BOJ] 20492. 세금
 T: 1 s
 M: 1024 MB
 '''
@@ -8,20 +8,5 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-d = 0
-p = 0
 
-for _ in range(n):
-    s = input().rstrip()
-    if s == 'D':
-        d += 1
-    elif s == 'P':
-        p += 1
-    if d-p >= 2:
-        print("%d:%d"%(d,p))
-        exit()
-    elif p-d >= 2:
-        print("%d:%d"%(d,p))
-        exit()
-
-print("%d:%d"%(d,p))
+print(int(n*0.78), int(n*0.8)+int(n*0.2*0.78))
