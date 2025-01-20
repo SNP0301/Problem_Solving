730a477e15ed1f7b4d11fcead3c90a3d6c592936
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-20 21:51:24 +0900

    test

--- ps.py
+++ ps.py
@@ -1,14 +1,1 @@
-'''
-[BOJ] 14581. 팬들에게 둘러싸인 홍준
-T: 1 s
-M: 1024 MB
-'''
-
-import sys
-input = sys.stdin.readline
-
-id = input().rstrip()
-
-print(":fan::fan::fan:")
-print(":fan::%s::fan:"%(id))
-print(":fan::fan::fan:")
+print(type(3))
