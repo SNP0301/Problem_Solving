66e4adc9e2d152bed76d59f96dd94b8e6190d1bc
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-20 22:52:22 +0900

    [SELF] input message & formatting

--- ps.py
+++ ps.py
@@ -1,17 +1,3 @@
-##2진수
-print(0b1001)
+name = input('enter name:')
 
-##8진수
-print(0o1001)
-
-##10진수
-print(1001)
-
-##16진수
-print(0x1001)
-
-##실수
-print(1.0)
-
-##복소수, complex
-print(2+3j)
+print(f'name is {name}')
