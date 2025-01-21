a39e20c7e0aeb2db97bc565066cee2b91184fb23
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-21 23:22:27 +0900

    [BOJ] 2670 solved

--- ps.py
+++ ps.py
@@ -1,13 +1,29 @@
 N = int(input())
-numbers = list()
 
+dp = [-1]*(N+1)
+answer = -1
+
+numbers = [-1]
 for _ in range(N):
-    numbers.append(int(input()))
+    numbers.append(float(input()))
 
-for i in range(len(numbers)):
-    for j in range(len(numbers)):
-        if numbers[i] < numbers[j]:
-            numbers[i], numbers[j] = numbers[j], numbers[i]
+""" 메모리 초과 -> 바로 2차원 배열을 만들 이유가 없었다.
+dp = [[0 for _ in range(N)] for _ in range(N)]
+answer = -1
 
-for x in numbers:
-    print(x)
+for i in range(N):
+    dp[i][i] = numbers[i]
+
+for i in range(N):
+    for j in range(i+1, N):
+        dp[i][j] = dp[i][j-1]*numbers[j]
+        answer = max(answer,dp[i][j])
+"""
+for i in range(1, N+1):
+    if dp[i-1] > 1:
+        dp[i] = dp[i-1] * numbers[i]
+    else:
+        dp[i] = numbers[i]
+    answer = max(answer,dp[i])
+
+print('%.3f'%answer)
