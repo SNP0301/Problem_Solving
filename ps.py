4f42c168fee6f387646b2e27a9ad67f52efdc840
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-24 00:27:54 +0900

    group mock solved - array

--- ps.py
+++ ps.py
@@ -1,33 +1,38 @@
-T = int(input())
+"""
+* 주제가 가려져 있어도 2차원 문제인 척하는 1차원 문제 구분할 것
+
+[배운 것]
+
+[제약사항]
+1. 가로 길이는 항상 100으로 고정
+2. 모든 위치에서 상자의 높이는 1 이상 100 이하
+3. 덤프 횟수는 1 이상 1000 이하
+4. 평탄화가 완료되면 덤프를 수행할 수 없으므로 그 때 최고점과 최저점의 차를 반환
+    - 평탄화 완료 여부는 max(arr)-min(arr) <= 1 으로 검증
+5. 테스트 케이스가 10개로 고정
+"""
+
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
+for test_case in range(1, 11):
     # ///////////////////////////////////////////////////////////////////////////////////
-    N, M = map(int,input().split())
-    a = list(map(int,input().split()))
-    b = list(map(int,input().split()))
-    answer = list()
-    score = 0
-    if N == M:
-        for i in range(N):
-            score += a[i]*b[i]
-        answer.append(score)
-        score = 0
-    elif N > M:
-        for i in range(N-M+1):
-            for j in range(M):
-                score += a[i+j]*b[j]
-            answer.append(score)
-            score = 0
-    elif N < M:
-        for i in range(M-N+1):
-            for j in range(N):
-                score += a[j]*b[i+j]
-            answer.append(score)
-            score = 0
-    else:
-        print("?")
+    answer = 0
+    dump_limit = int(input())
+    boxes = list(map(int,input().split()))
 
+    while dump_limit > 0 :
+        if max(boxes) - min(boxes) <= 1 :
+            answer = max(boxes) - min(boxes)
+            break
+        elif max(boxes) - min(boxes) > 1:
+            A = boxes.index(max(boxes))
+            B = boxes.index(min(boxes))
+            boxes[A] -= 1
+            boxes[B] += 1
+            dump_limit -= 1
+            answer = max(boxes) - min(boxes)
+        else:
+            print("why")
+            break
 
-    ##print(answer)
-    print("#%d %d"%(test_case, max(answer)))
+    print("#%d %d"%(test_case, answer))
     # ///////////////////////////////////////////////////////////////////////////////////
