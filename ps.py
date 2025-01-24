0a87478b2d49767e562a28c648933cc953c31339
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-25 08:41:56 +0900

    [BOJ] 2164 solved

--- ps.py
+++ ps.py
@@ -1,10 +1,18 @@
+"""
+* 카드가 1장 남을 때까지
+** 1<= N <= 500,000
+"""
+
 N = int(input())
-
-members = list()
-
-for i in range(N):
-    age, name = input().split()
-    members.append([int(age), name])
-
-for person in sorted(members, key = lambda x: x[0]) : ## 가입한 순서로 입력이 주어져서 x[2]가 필요없나?
-    print(person[0], person[1])
+cnt = 1
+if N == 1:
+    print(1)
+elif N == 2:
+    print(2)
+else:
+    while True:
+        if 2**cnt<N<=2**(cnt+1):
+            print(2**(cnt+1)-(2**(cnt+1)-N)*2)
+            break
+        else:
+            cnt += 1
