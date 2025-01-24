aa6d71a73e4e4433b47d83842c9d9ec09a45ed1e
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-25 00:32:20 +0900

    [BOJ] 10814 solved

--- ps.py
+++ ps.py
@@ -1,40 +1,10 @@
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    # ///////////////////////////////////////////////////////////////////////////////////
-    answer = 0
-    N, K = map(int,input().split())
-    arr = list()
+N = int(input())
 
-    for _ in range(N):
-        arr.append(list(map(int,input().split())))
+members = list()
 
-    for i in range(N):
-        cnt = 0
-        for j in range(N):
-            if arr[i][j] == 1:
-                cnt += 1
-            elif arr[i][j] == 0:
-                if cnt == K:
-                    answer += 1
-                cnt = 0
-        if cnt == K:
-            answer += 1
-    zipped_arr = list(zip(*arr))
-    for i in range(N):
-        cnt = 0
-        for j in range(N):
-            if zipped_arr[i][j] == 1:
-                cnt += 1
-            elif zipped_arr[i][j] == 0:
-                if cnt == K:
-                    answer += 1
-                cnt = 0
+for i in range(N):
+    age, name = input().split()
+    members.append([int(age), name])
 
-        if cnt == K:
-            answer += 1
-
-
-
-    print("#%d %d"%(test_case,answer))
-    # ///////////////////////////////////////////////////////////////////////////////////
+for person in sorted(members, key = lambda x: x[0]) : ## 가입한 순서로 입력이 주어져서 x[2]가 필요없나?
+    print(person[0], person[1])
