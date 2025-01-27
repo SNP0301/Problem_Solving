451e1bbe2e1a357b169b5c2d0f8db845a4582b19
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-27 23:11:40 +0900

    [BOJ] 1929 solved

--- ps.py
+++ ps.py
@@ -1,31 +1,22 @@
 """
-[배운 것]
-- round의 인자로 들어가는 수의 소수점 자릿수가 커지면 오차가 발생할 수 있다.
-- my_round로 대체해야 한다는 아이디어는 떠올리지 못했다
-    - 강사님 말씀대로, 함수/메서드가 어떻게 동작하는지 정확하게 알고 사용할 것.
+* M이상 N이하, 소수가 하나 이상 있는 입력만
+** 한 줄에 하나씩, 증가하는 순서대로 소수 출력
+
+[구상]
+    - 1~N 사이에 존재하는 소수 x를 미리 구해 is_prime[x]에 'True' 로 저장
 """
 
-def my_round(i):
-    if i - int(i) >= 0.5:
-        return int(i) + 1
-    else: return int(i)
+M, N = map(int,input().split())
+is_prime = [True for _ in range(N+1)]
+is_prime[1] = False
 
-n = int(input())
-score_list = list()
-cut = 0
-answer = 0
+for i in range(2, N):
+    if is_prime[i]:
+        n = i*2
+        while n <= N:
+            is_prime[n] = False
+            n += i
 
-for _ in range(n):
-    score_list.append(int(input()))
-
-if n == 0:
-    print(0)
-elif 1<= n <= 3:
-    print(my_round(sum(score_list)/len(score_list)))
-elif n > 3:
-    score_list = sorted(score_list)
-    cut = my_round(n * 0.15)
-    ##print(n,n*0.15,cut)
-    score_list = score_list[cut:-cut]
-    ##print(score_list)
-    print(my_round(sum(score_list) / len(score_list)))
+for i in range(M,N+1):
+    if is_prime[i]:
+        print(i)
