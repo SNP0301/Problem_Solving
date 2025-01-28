140eaf413112e643a316f24a3928f29445c32bc8
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-29 00:11:03 +0900

    [BOJ] 2606 not graph, temp

--- ps.py
+++ ps.py
@@ -1,34 +1,31 @@
 """
-N = int(input())
-answer = 0
-while N != 1:
-    if N%3 == 0:
-        N = N//3
-    elif (N-1)%3 == 0:
-            N -= 1
-    elif N%2 == 0:
-        N = N//2
-    else:
-        N -= 1
-    answer += 1
-print(answer)
-
+set.union()으로는 못 푸나?
+    - 컴퓨터 1대당 각 1개씩, 총 100개의 set을 만들고, 해당 컴퓨터와 연결된 node 번호를 set.add()
+    - answer_set = answer_set.union(~~)
 """
-N = int(input())
-answer = [1000001 for _ in range(N+1)]
-if N == 1:
-    print(0)
-elif N == 2:
-    print(1)
-else:
-    answer[2] = 1
-    answer[3] = 1
+computers = int(input())
+num_of_edges = int(input())
+answer_list = [ set() for _ in range(computers+1)]
 
-    for n in range(2,N+1):
-        if n*3 <= N:
-            answer[n*3] = min(answer[n*3],answer[n]+1)
-        if n*2 <= N:
-            answer[n*2] = min(answer[n*2],answer[n]+1)
-        if n+1 <= N:
-            answer[n+1] = min(answer[n+1],answer[n]+1)
-    print(answer[N])
+if computers == 1:
+    print(0)
+else:
+    for _ in range(num_of_edges):
+        start, end = map(int,input().split())
+        #print(start,end)
+        answer_list[start].add(end)
+
+    answer_set = answer_list[1]
+    for i in range(1,computers+1):
+        if i in answer_list[1]:
+            answer_set = answer_set.union(answer_list[i])
+            #print(i,answer_set)
+    for i in range(computers,0,-1):
+        if i in answer_list[1]:
+            answer_set = answer_set.union(answer_list[i])
+            #print(i,answer_set)
+
+    if 1 in answer_set:
+        print(len(answer_set)-1)
+    else:
+        print(len(answer_set))
