e447b7670d5c533f27308e2dc2aa485fbb5156a0
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-28 23:06:26 +0900

    [BOJ] 1003 solved

--- ps.py
+++ ps.py
@@ -1,8 +1,15 @@
-N = int(input())
-waiting = sorted(list(map(int,input().split())))
-answer = 0
-
-for i in range(len(waiting)):
-    answer += waiting[i]*(N-i)
-
-print(answer)
+"""
+"""
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    fib_zero = [0 for _ in range(41)]
+    fib_one = [1 for _ in range(41)]
+    fib_zero[0] = 1
+    fib_zero[1] = 0
+    fib_one[0] = 0
+    fib_one[1] = 1
+    for i in range(2,41):
+        fib_zero[i] = fib_zero[i-1] + fib_zero[i-2]
+        fib_one[i] = fib_one[i-1] + fib_one[i-2]
+    print(fib_zero[N],fib_one[N])
