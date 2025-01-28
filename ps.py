42e9c9e515a6236cb9fc6d1ac0b62a7248574a8e
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-29 06:41:13 +0900

    [BOJ] 11659 solved

--- ps.py
+++ ps.py
@@ -1,12 +1,7 @@
-N = int(input())
-given_lst = list(map(int,input().split()))
-my_lst = [0 for _ in range(63)]
-my_lst[0] = given_lst.count(1)
+n = int(input())
 
-for i in range(1,63):
-    my_lst[i] += given_lst.count(2**i) + my_lst[i-1]//2
+arr = [0,1,2,3] + [0 for _ in range(n-3)]
 
-for i in range(62,-1,-1):
-    if my_lst[i] != 0:
-        print(2**i)
-        break
+for i in range(4,n+1):
+    arr[i] = arr[i-1] + arr[i-2]
+print(arr[n]%10007)
