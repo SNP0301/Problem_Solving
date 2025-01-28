51ecd80de3ec04de6ac7b88fd3e7204625cb9c1f
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-28 22:41:46 +0900

    [BOJ] 1874 solved

--- ps.py
+++ ps.py
@@ -2,60 +2,38 @@
 
 """
 
-def get_cur_N(cable_size,K):
-    cur_N = 0
-    for i in range(K):
-        cur_N += cables[i]//cable_size
-    return int(cur_N)
+N = int(input())
+given_lst = list()
+chk_lst = [i for i in range(N,0,-1)]
+given_dct = dict()
 
-K, N = map(int,input().split())
-cables = list()
+tmp_stk = list()
+my_stk = list()
 
-for _ in range(K):
-    cables.append(int(input()))
-cable_size = 1
-move_size = 1
-going_up = True
-going_down = not going_up
-answer = list()
-while True:
-    if N == 1:
-        print(cables[0])
-        break
+answer_lst = list()
+
+is_stackable = True
+cur = N
+
+for i in range(1,N+1):
+    n = int(input())
+    given_lst.append(n) # 4 3 6 8 7 5 2 1
+    given_dct[n] = i ## given_dct[숫자]=위치
+
+while len(my_stk) < N:
+    if given_dct[cur] != 0:
+        mov = given_lst.pop()
+        tmp_stk.append(mov)
+        answer_lst.append("-")
+        given_dct[mov]=0
     else:
-        cur_N = get_cur_N(cable_size,K)
+        mov = tmp_stk.pop()
+        my_stk.append(mov)
+        answer_lst.append("+")
+        cur -= 1
 
-        if going_up:
-            if cur_N >= N: ## P1. 등호 위치
-                move_size *= 2
-                cable_size += move_size
-            elif cur_N < N:
-                move_size /= 2
-                cable_size -= move_size
-                going_down = True
-                going_up = False
-        elif going_down:
-            if cur_N < N: ## P1. 등호 위치
-                move_size /= 2
-                cable_size -= move_size
-            elif cur_N >= N:
-                move_size /= 2
-                cable_size += move_size
-                going_down = False
-                going_up = True
-        if move_size == 1:
-            for a in range(-1,2,1):
-                answer.append([int(cable_size+a),get_cur_N(cable_size+a,K)])
-                answer = sorted(answer,key = lambda x: (x[1],-x[0]))
-            for i in range(len(answer)):
-                if answer[i][1] >= N:
-                    print(answer[i][0])
-                    break
-            break
-
-
-
-
-    
-
-    
+if my_stk == chk_lst:
+    for i in range(len(answer_lst)-1,-1,-1):
+        print(answer_lst[i])
+else:
+    print("NO")
