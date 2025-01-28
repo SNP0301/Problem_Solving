67ebf907b482ce1c3858cee498f47715542c7bff
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-29 06:44:40 +0900

    [BOJ] 11726 solved

--- ps.py
+++ ps.py
@@ -1,10 +1,7 @@
-N, M = map(int,input().split())
-number_list = list(map(int,input().split()))+[0] ## 5 4 3 2 1
-sum_list = [0 for _ in range(N+1)] ## 0 5 9 12 14 15
-sum_list[1] = number_list[0]
-for i in range(2,N+1):
-    sum_list[i] = sum_list[i-1] + number_list[i-1]
-#print(sum_list)
-for _ in range(M):
-    start, end = map(int,input().split())
-    print(sum_list[end]-sum_list[start-1])
+n = int(input())
+
+arr = [0,1,2,3] + [0 for _ in range(n-3)]
+
+for i in range(4,n+1):
+    arr[i] = arr[i-1] + arr[i-2]
+print(arr[n]%10007)
