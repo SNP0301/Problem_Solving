5e8945aad275f9498b4145b106fa5d8bcbf72da3
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-28 23:38:58 +0900

    [BOJ] 1463 solved

--- ps.py
+++ ps.py
@@ -1,15 +1,34 @@
 """
+N = int(input())
+answer = 0
+while N != 1:
+    if N%3 == 0:
+        N = N//3
+    elif (N-1)%3 == 0:
+            N -= 1
+    elif N%2 == 0:
+        N = N//2
+    else:
+        N -= 1
+    answer += 1
+print(answer)
+
 """
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    fib_zero = [0 for _ in range(41)]
-    fib_one = [1 for _ in range(41)]
-    fib_zero[0] = 1
-    fib_zero[1] = 0
-    fib_one[0] = 0
-    fib_one[1] = 1
-    for i in range(2,41):
-        fib_zero[i] = fib_zero[i-1] + fib_zero[i-2]
-        fib_one[i] = fib_one[i-1] + fib_one[i-2]
-    print(fib_zero[N],fib_one[N])
+N = int(input())
+answer = [1000001 for _ in range(N+1)]
+if N == 1:
+    print(0)
+elif N == 2:
+    print(1)
+else:
+    answer[2] = 1
+    answer[3] = 1
+
+    for n in range(2,N+1):
+        if n*3 <= N:
+            answer[n*3] = min(answer[n*3],answer[n]+1)
+        if n*2 <= N:
+            answer[n*2] = min(answer[n*2],answer[n]+1)
+        if n+1 <= N:
+            answer[n+1] = min(answer[n+1],answer[n]+1)
+    print(answer[N])
