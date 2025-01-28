0e584bb6e4f161c6f27f3d1f68c4e90d28ddc0ba
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-28 22:51:11 +0900

    [BOJ] 11399 solved

--- ps.py
+++ ps.py
@@ -1,19 +1,8 @@
-"""
+N = int(input())
+waiting = sorted(list(map(int,input().split())))
+answer = 0
 
-"""
+for i in range(len(waiting)):
+    answer += waiting[i]*(N-i)
 
-N, M = map(int,input().split())
-never_heard = set()
-never_seen = set()
-
-for i in range(N):
-    never_heard.add(input())
-
-for i in range(M):
-    never_seen.add(input())
-
-answer = sorted(list(never_heard.intersection(never_seen)))
-
-print(len(answer))
-for i in answer:
-    print(i)
+print(answer)
