ab171d714120bc66893558dcd6f854ea23a65ee6
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-29 00:53:34 +0900

    [BOJ] 27514 solved

--- ps.py
+++ ps.py
@@ -1,14 +1,12 @@
-"""
-?
-"""
-import sys
-input = sys.stdin.readline
-sites = dict()
+N = int(input())
+given_lst = list(map(int,input().split()))
+my_lst = [0 for _ in range(63)]
+my_lst[0] = given_lst.count(1)
 
-N, M = map(int,input().split())
-for _ in range(N):
-    info = list(input().split())
-    sites[info[0]] = info[1] ## sites[사이트명] = 비밀번호
-for i in range(M):
-    info = input().rstrip()
-    print(sites[info])
+for i in range(1,63):
+    my_lst[i] += given_lst.count(2**i) + my_lst[i-1]//2
+
+for i in range(62,-1,-1):
+    if my_lst[i] != 0:
+        print(2**i)
+        break
