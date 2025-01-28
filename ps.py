7225c4e8642af1a7b1f7864f0f06330f41febbec
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-29 00:34:49 +0900

    [BOJ] 17219 solved

--- ps.py
+++ ps.py
@@ -1,17 +1,14 @@
 """
-set.union()으로는 못 푸나?
-    - 컴퓨터 1대당 각 1개씩, 총 100개의 set을 만들고, 해당 컴퓨터와 연결된 node 번호를 set.add()
-    - answer_set = answer_set.union(~~)
+?
 """
-T = int(input())
+import sys
+input = sys.stdin.readline
+sites = dict()
 
-answer_list = [0 for _ in range(11)]
-answer_list[1] = 1
-answer_list[2] = 2
-answer_list[3] = 4
-for i in range(4,11):
-    answer_list[i] = answer_list[i-1] + answer_list[i-2] + answer_list[i-3]
-
-for _ in range(T):
-    n = int(input())
-    print(answer_list[n])
+N, M = map(int,input().split())
+for _ in range(N):
+    info = list(input().split())
+    sites[info[0]] = info[1] ## sites[사이트명] = 비밀번호
+for i in range(M):
+    info = input().rstrip()
+    print(sites[info])
