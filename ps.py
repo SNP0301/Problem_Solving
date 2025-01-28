7f18f051fea47eec77678f5c71051dfe5424ab17
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-29 00:26:53 +0900

    [BOJ] 9095 solved

--- ps.py
+++ ps.py
@@ -3,29 +3,15 @@ set.union()으로는 못 푸나?
     - 컴퓨터 1대당 각 1개씩, 총 100개의 set을 만들고, 해당 컴퓨터와 연결된 node 번호를 set.add()
     - answer_set = answer_set.union(~~)
 """
-computers = int(input())
-num_of_edges = int(input())
-answer_list = [ set() for _ in range(computers+1)]
+T = int(input())
 
-if computers == 1:
-    print(0)
-else:
-    for _ in range(num_of_edges):
-        start, end = map(int,input().split())
-        #print(start,end)
-        answer_list[start].add(end)
+answer_list = [0 for _ in range(11)]
+answer_list[1] = 1
+answer_list[2] = 2
+answer_list[3] = 4
+for i in range(4,11):
+    answer_list[i] = answer_list[i-1] + answer_list[i-2] + answer_list[i-3]
 
-    answer_set = answer_list[1]
-    for i in range(1,computers+1):
-        if i in answer_list[1]:
-            answer_set = answer_set.union(answer_list[i])
-            #print(i,answer_set)
-    for i in range(computers,0,-1):
-        if i in answer_list[1]:
-            answer_set = answer_set.union(answer_list[i])
-            #print(i,answer_set)
-
-    if 1 in answer_set:
-        print(len(answer_set)-1)
-    else:
-        print(len(answer_set))
+for _ in range(T):
+    n = int(input())
+    print(answer_list[n])
