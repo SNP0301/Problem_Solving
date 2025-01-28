80793e9f72c3de6fe9f575d94a6a8c09977a1f8b
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-28 22:45:52 +0900

    [BOJ] 1764 solved

--- ps.py
+++ ps.py
@@ -2,38 +2,18 @@
 
 """
 
-N = int(input())
-given_lst = list()
-chk_lst = [i for i in range(N,0,-1)]
-given_dct = dict()
+N, M = map(int,input().split())
+never_heard = set()
+never_seen = set()
 
-tmp_stk = list()
-my_stk = list()
+for i in range(N):
+    never_heard.add(input())
 
-answer_lst = list()
+for i in range(M):
+    never_seen.add(input())
 
-is_stackable = True
-cur = N
+answer = sorted(list(never_heard.intersection(never_seen)))
 
-for i in range(1,N+1):
-    n = int(input())
-    given_lst.append(n) # 4 3 6 8 7 5 2 1
-    given_dct[n] = i ## given_dct[숫자]=위치
-
-while len(my_stk) < N:
-    if given_dct[cur] != 0:
-        mov = given_lst.pop()
-        tmp_stk.append(mov)
-        answer_lst.append("-")
-        given_dct[mov]=0
-    else:
-        mov = tmp_stk.pop()
-        my_stk.append(mov)
-        answer_lst.append("+")
-        cur -= 1
-
-if my_stk == chk_lst:
-    for i in range(len(answer_lst)-1,-1,-1):
-        print(answer_lst[i])
-else:
-    print("NO")
+print(len(answer))
+for i in answer:
+    print(i)
