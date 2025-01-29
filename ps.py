7e1198aab7fd421c3f408b3188fc64ee612515fa
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-29 20:13:18 +0900

    [BOJ] 1260 solved

--- ps.py
+++ ps.py
@@ -1,27 +1,47 @@
 """
-* 1 <= N <= 1000000
-** Range = 2*10**9+1
+
 """
-import sys
-input = sys.stdin.readline
+def dfs(c):
+    visited[c] = True
+    ans_dfs.append(c)
+    for i in adj[c]:
+        if not visited[i]: dfs(i)
 
-N = int(input())
-lst = list(map(int,input().split()))
-chk_lst = sorted(list(set(lst)))
-dct = dict()
-cnt = 0
-
-for i in chk_lst:
-    dct[i] = cnt
-    cnt += 1
-
-for i in range(len(lst)):
-    print(dct[lst[i]],end=" ")
-
-
-
+def bfs(s):
+    queue = []
+    visited[s] = True
+    queue.append(s)
+    ans_bfs.append(s)
+    while queue:
+        c = queue.pop(0)
+        for i in adj[c]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+                ans_bfs.append(i)
+        ##visited[c] = True
 
 
+N,M,V = map(int,input().split())
+visited = [False for _ in range(N+1)]
+ans_dfs = []
+ans_bfs = []
+
+adj = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    start,end = map(int,input().split())
+    adj[start].append(end)
+    adj[end].append(start)
+
+for i in adj:
+    i.sort()
+
+dfs(V)
+visited = [False for _ in range(N+1)]
+bfs(V)
+print(*ans_dfs)
+print(*ans_bfs)
 
 
 
