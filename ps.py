ce2971d75c1b733b4d04d1a2e0264a623ef88cac
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-29 14:16:18 +0900

    [BOJ] 18870 solved

--- ps.py
+++ ps.py
@@ -1,43 +1,27 @@
 """
-* 반복문,조건문의 역할을 mece하게 이해했다면 안 틀렸을 것,,,
-** 손구현 먼저 하자.
+* 1 <= N <= 1000000
+** Range = 2*10**9+1
 """
-def BFS(x,N):
-    queue = [x]
-    while queue:
-        x = queue.pop()
-        for j in range(1,N+1):
-            if arr[x][j] == 1:
-                ##print("from %d to %d"%(x,j))
-                queue.append(j)
-                answer_set.add(j)
-                arr[x][j] = -1
+import sys
+input = sys.stdin.readline
 
-computers = int(input())
+N = int(input())
+lst = list(map(int,input().split()))
+chk_lst = sorted(list(set(lst)))
+dct = dict()
+cnt = 0
 
-if computers == 1:
-    print(0)
-else:
-    num = int(input())
-    arr = [[0 for _ in range(computers+1)] for _ in range(computers+1)]
-    answer = 0
-    answer_set = set()
+for i in chk_lst:
+    dct[i] = cnt
+    cnt += 1
 
-    for _ in range(num):
-        start,end = map(int,input().split())
-        
-        if start > end:
-            start,end = end,start
-        arr[start][end] = 1
-        arr[end][start] = 1
+for i in range(len(lst)):
+    print(dct[lst[i]],end=" ")
 
-    for i in range(2,computers+1):
-        if arr[1][i] == 1:
-            BFS(1,computers)
-    
 
-    if 1 in answer_set:
-        print(len(answer_set)-1)
-    else:
-        print(len(answer_set))
+
+
+
+
+
 
