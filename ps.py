a7fb67a84f493a35a27893e54c3eeaaa3af1830e
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-30 23:36:23 +0900

    [BOJ] 1620 solved

--- ps.py
+++ ps.py
@@ -1,17 +1,19 @@
+N, M = map(int,input().split())
+pocketmon_name_dct = dict()
+pocketmon_num_dct = dict()
 
-test = 0b1101101010100
-print(test)
-test = 0b11011010101
-print(test)
-test = 0b110110101
-print(test)
-test = 0b1101101
-print(test)
-test = 0b11011
-print(test)
+for i in range(1,N+1):
+    name = input()
+    pocketmon_name_dct[i] = name
+    pocketmon_num_dct[name] = i
 
-one = 0b101
-two = 0b10101
-three = 0b1010101
+for i in range(M):
+    quiz = input()
+    if 60<=ord(quiz[0])<=90:
+        print(pocketmon_num_dct[quiz])
+    else:
+        quiz = int(quiz)
+        print(pocketmon_name_dct[quiz])
 
-print(one, two, three)
+
+
