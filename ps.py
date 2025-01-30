6544af696a06d53f2f5f8e56c56de873da89493b
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-30 14:32:49 +0900

    [BOJ] 4396 solved

--- ps.py
+++ ps.py
@@ -1,24 +1,55 @@
 """
+* 열렸거나, 안 열렸거나
 """
+def my_print(i):
+    for x in i:
+        print(x)
+    print()
 
-N, K = map(int,input().split())
-arr = [[K-N+1,K-N+1,K-N+1] for _ in range(0,2*K+5)]
-cur = 1
-if N == K:
-    print(0)
-else:
-    arr[K] = [0,0,0]
-    arr[K*2] = [1,1,1]
-    if N == 0:
-        arr[N+1][0] = 1
-    else:
-        for i in range(K*2,N,-1):
-            cur = min(arr[i])
-            #print("%d goes to %d, %d, %d"%(i, i+1, i-1, i//2))
-            arr[i+1][0] = min(min(arr[i+1]), cur + 1)
-            arr[i-1][1] = min(min(arr[i-1]), cur + 1)
-            arr[i//2][2] = min(min(arr[i//2]), cur + 1)
+N = int(input())
+original_arr = list()
+played_arr = list()
+answer_arr = [["." for _ in range(N)] for _ in range(N)]
+bombed = False
 
-for i in range(N,2*K+2):
-    print(i, arr[i])
-    
+dx = [0,-1,0,1,1,1,0,-1,-1]
+dy = [0,1,1,1,0,-1,-1,-1,0]
+
+
+for _ in range(N): # *이거나 .이거나
+    original_arr.append(list(input()))
+for _ in range(N): # x이거나 .이거나
+    played_arr.append(list(input()))
+
+
+
+for x in range(N):
+    for y in range(N):
+        cnt = 0
+        if played_arr[x][y] == "x":
+            for n in range(9): # n of nine
+                nx = x + dx[n]
+                ny = y + dy[n]
+                if 0<=nx<N and 0<=ny<N:
+                    if original_arr[nx][ny] == "*":
+                        cnt += 1
+            answer_arr[x][y] = cnt
+            if original_arr[x][y] == "*":
+                bombed = True
+
+#my_print(answer_arr)
+
+if bombed:
+    for x in range(N):
+        for y in range(N):
+            if original_arr[x][y] == "*":
+                answer_arr[x][y] = "*"
+
+for x in range(N):
+    for y in range(N):
+        print(answer_arr[x][y],end="")
+    print()
+
+
+##if bombed: ## 10*10이라 수제로 매칭시켜도 괜찮을듯?
+
