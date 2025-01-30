b52d241d600eee73015460ef94af4d44a920c647
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-30 09:39:36 +0900

    [BOJ] 1697 tmp

--- ps.py
+++ ps.py
@@ -1,37 +1,24 @@
 """
-* 리스트 슬라이스 연습용 문제
-
-[배운 것]
-- 조건문 분기를 최대한 꼼꼼하게 해야 구현도 쉽다
-- idx 0, 1인 경우를 따로 세면 나중에 불편해진다.
 """
 
-P = int(input())
-for p in range(1,P+1):
-    answer = 0
-    info = list(map(int,input().split()))
-    info.pop(0)
-    stood = [info.pop(0)]
-    for _ in range(19):
-        cur = info.pop(0)
-        #print(cur,stood)
-        if cur > stood[-1]: ##최대값보다 크다면
-            stood.append(cur)
-        elif cur < stood[0]: ##최소값보다 작다면
-            #print("%d made +%d"%(cur,len(stood)))
-            answer += len(stood)
-            stood = [cur] + stood
-        elif stood[0]<cur<stood[-1]: ##그 사이라면
-            for i in range(len(stood)-1,-1,-1):
-                if cur > stood[i]: ## 자리를 찾으면
-                    ##print("%d made +%d"%(cur,len(stood[i:])))
-                    answer += len(stood[i:])-1
-                    #print(stood[:i+1])
-                    #print(stood[i+1:])
-                    new_stood = stood[:i+1] + [cur] + stood[i+1:]
-                    stood = new_stood
-                    break
-                
-    ##print(info)
-    #print(stood)
-    print(p, answer)
+N, K = map(int,input().split())
+arr = [[K-N+1,K-N+1,K-N+1] for _ in range(0,2*K+5)]
+cur = 1
+if N == K:
+    print(0)
+else:
+    arr[K] = [0,0,0]
+    arr[K*2] = [1,1,1]
+    if N == 0:
+        arr[N+1][0] = 1
+    else:
+        for i in range(K*2,N,-1):
+            cur = min(arr[i])
+            #print("%d goes to %d, %d, %d"%(i, i+1, i-1, i//2))
+            arr[i+1][0] = min(min(arr[i+1]), cur + 1)
+            arr[i-1][1] = min(min(arr[i-1]), cur + 1)
+            arr[i//2][2] = min(min(arr[i//2]), cur + 1)
+
+for i in range(N,2*K+2):
+    print(i, arr[i])
+    
