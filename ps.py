b49527187498cb623a688868b5827b5721e33c82
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-30 22:46:02 +0900

    [SELF] binary test

--- ps.py
+++ ps.py
@@ -1,29 +1,17 @@
-"""
-* 리스트 슬라이스 연습용 문제
 
-[배운 것]
-- 조건문 분기를 최대한 꼼꼼하게 해야 구현도 쉽다
-- idx 0, 1인 경우를 따로 세면 나중에 불편해진다.
-"""
+test = 0b1101101010100
+print(test)
+test = 0b11011010101
+print(test)
+test = 0b110110101
+print(test)
+test = 0b1101101
+print(test)
+test = 0b11011
+print(test)
 
-P = int(input())
-for p in range(1,P+1):
-    answer = 0
-    info = list(map(int,input().split()))
-    info.pop(0)
-    stood = [info.pop(0)]
-    for _ in range(19):
-        cur = info.pop(0)
-        if cur > stood[-1]: ##최대값보다 크다면
-            stood.append(cur)
-        elif cur < stood[0]: ##최소값보다 작다면
-            answer += len(stood)
-            stood = [cur] + stood
-        elif stood[0]<cur<stood[-1]: ##그 사이라면
-            for i in range(len(stood)-1,-1,-1):
-                if cur > stood[i]: ## 자리를 찾으면
-                    answer += len(stood[i:])-1
-                    stood = stood[:i+1] + [cur] + stood[i+1:]
-                    break
-                    
-    print(p, answer)
+one = 0b101
+two = 0b10101
+three = 0b1010101
+
+print(one, two, three)
