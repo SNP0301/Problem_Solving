cc09b33ef1cab6c0ee41845fd0d0937fe16c02c0
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-01-30 15:00:39 +0900

    [BOJ] 1697 solved

--- tmp.py
+++ tmp.py
@@ -5,25 +5,28 @@
 
 * K*2로 두거나, 반복문으로 앞으로 갔다가 뒤로 갔다면 못 풀었던 이유 찾아내기
 """
-def bfs(s): # 방문 표시?
-    q = [s]
 
+from collections import deque
+def bfs(s): # 방문 표시?
+    q = deque()
+    q.append(s)
+    visited[s] = True
     while q:
-        cur = q.pop()
+        cur = q.popleft()
+        #print(cur, q)
         for next in [cur-1,cur+1,cur*2]:
             if next == K:
-                return arr[cur] + 1
-            elif 0<=next<=100000 and not arr[next]:
+                return arr[cur] + 1 ##
+            elif 0<=next<=100000 and not visited[next]:
                 arr[next] = arr[cur] + 1
                 q.append(next)
-                
-
+                visited[next] = True
 
 
 
 N, K = map(int,input().split())
 arr = [0 for _ in range(100001)] # 0부터니까 갯수만큼 선언해도 ㄱㅊ
-
+visited = [ False for _ in range(100001)]
 if K == N:
     print(0)
 else:
