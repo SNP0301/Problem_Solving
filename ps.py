f66a2ead8a85dbb179230ce19315af1736ecdf2c
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-01 22:26:09 +0900

    [BOJ] 2346 solved

--- ps.py
+++ ps.py
@@ -1,44 +1,37 @@
 """
 [복잡도] O(n)
-    - 명령의 수 N (1 <= N <= 2000000)만큼 연산
 """
-import sys
-input = sys.stdin.readline
-from collections import deque
 
 N = int(input())
-my_deque = deque()
+numbers = [0]+ list(map(int,input().split()))
+bal_value = 0
+cur = 1
 
-for _ in range(N):
-    cmd = list(map(int,input().split()))
-    if cmd[0] == 1:
-        my_deque.append(cmd[1])
-    elif cmd[0] == 2:
-        my_deque.appendleft(cmd[1])
-    elif cmd[0] == 3:
-        if my_deque:
-            print(my_deque.pop())
-        elif not my_deque:
-            print(-1)
-    elif cmd[0] == 4:
-        if my_deque:
-            print(my_deque.popleft())
-        elif not my_deque:
-            print(-1)
-    elif cmd[0] == 5:
-        print(len(my_deque))
-    elif cmd[0] == 6:
-        if not my_deque:
-            print(1)
-        elif my_deque:
-            print(0)
-    elif cmd[0] == 7:
-        if my_deque:
-            print(my_deque[-1])
-        elif not my_deque:
-            print(-1)
-    elif cmd[0] == 8:
-        if my_deque:
-            print(my_deque[0])
-        elif not my_deque:
-            print(-1)
+for i in range(N): ## 풍선 순서는 자연수를 기준으로 하므로 array index에 맞추기 위해 -1
+    bal_value = numbers[cur]
+    numbers[cur] = 0
+    print(cur,end=" ")
+    if bal_value > 0:
+        cnt = bal_value
+        while cnt > 0:
+            #print(bal_value,cnt)
+            cur += 1
+            if cur > N:
+                cur = 0
+            if numbers[cur] == 0:
+                cnt += 1
+            cnt -= 1
+            if sum(numbers) == 0:
+                break
+    elif bal_value < 0:
+        cnt = bal_value*-1
+        while cnt > 0:
+            #print(bal_value,cnt)
+            cur -= 1
+            if cur < 0:
+                cur = N
+            if numbers[cur] == 0:
+                cnt += 1
+            cnt -= 1
+            if sum(numbers) == 0:
+                break
