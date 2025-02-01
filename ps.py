1c4df38ee0e622d6eeac0a8a711fd3e1b762c203
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-01 09:11:37 +0900

    [BOJ] 28278 solved

--- ps.py
+++ ps.py
@@ -1,15 +1,34 @@
-T = int(input())
+"""
+[복잡도] O(n)
+    - 명령의 수 1,000,000
+    - 명령당 연산/입출력 1~2
+"""
+import sys
+input = sys.stdin.readline
 
-for _ in range(T):
-    n = int(input())
-    fashion_dct = dict()
-    answer = 1
-    for _ in range(n):
-        info = list(input().split())
-        if info[1] in fashion_dct:
-            fashion_dct[info[1]] += 1
-        else:
-            fashion_dct[info[1]] = 1
-    for x in fashion_dct:
-        answer *= fashion_dct[x]+1
-    print(answer-1)
+N = int(input())
+stk = []
+for _ in range(N):
+    cmd = list(map(int,input().split()))
+
+    if cmd[0] == 1:
+        stk.append(cmd[1])
+    elif cmd[0] == 2:
+        if stk:
+            print(stk.pop())
+        elif not stk:
+            print(-1)
+    elif cmd[0] == 3:
+        print(len(stk))
+    elif cmd[0] == 4:
+        if stk:
+            print(0)
+        elif not stk:
+            print(1)
+    elif cmd[0] == 5:
+        if stk:
+            print(stk[-1])
+        elif not stk:
+            print(-1)
+    else:
+        print("???")
