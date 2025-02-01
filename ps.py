9e29ecb69573d29e333a00297f28285ab076a580
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-01 16:18:02 +0900

    [BOJ] 2630 solved

--- ps.py
+++ ps.py
@@ -16,21 +16,42 @@ N = int(input())
 sz = N*2
 arr = [list(map(int,input().split())) for _ in range(N)]
 
-my_print(arr)
-
+blue = 0
+white = 0
 while True: #한번 돌 때 마다 128 -> 64 -> ... 으로 사이즈 줄여가며 탐색
     sz = sz//2
-    print(sz)
 
-    for start_x in range(N//sz):
-        for start_y in range(N//sz):
-            #print(start_x*sz,start_y*sz)
+    for start_x in range(0,N,sz):
+        for start_y in range(0,N,sz):
+            #print("FROM: [%d,%d]"%(start_x,start_y))
+            valid = True
             for x in range(start_x,start_x+sz):
                 for y in range(start_y,start_y+sz):
-                    print(x,y)
-            print("####")
+                    if arr[x][y] != 1:
+                        valid = False
+            #print("END: [%d,%d]"%(start_x,start_y))
+            if valid:
+                for x in range(start_x,start_x+sz):
+                    for y in range(start_y,start_y+sz):
+                        arr[x][y] = -1
+                blue += 1
 
-    print()
-
+    for start_x in range(0,N,sz):
+        for start_y in range(0,N,sz):
+            #print("FROM: [%d,%d]"%(start_x,start_y))
+            valid = True
+            for x in range(start_x,start_x+sz):
+                for y in range(start_y,start_y+sz):
+                    if arr[x][y] != 0:
+                        valid = False
+            #print("END: [%d,%d]"%(start_x,start_y))
+            if valid:
+                for x in range(start_x,start_x+sz):
+                    for y in range(start_y,start_y+sz):
+                        arr[x][y] = -1
+                white += 1
     if sz == 1:
         break
+
+print(white)
+print(blue)
