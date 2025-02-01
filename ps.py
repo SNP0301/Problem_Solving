23394c3796666c89e08d0ece59a4cc1b1d24524a
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-01 10:36:12 +0900

    [BOJ] 12789 solved

--- ps.py
+++ ps.py
@@ -1,34 +1,39 @@
 """
 [복잡도] O(n)
-    - 명령의 수 1,000,000
-    - 명령당 연산/입출력 1~2
+    - 학생들의 수 <= 1000인 자연수
+    - 반대로 정렬([5,4,3,2,1])된 경우여도 
 """
-import sys
-input = sys.stdin.readline
 
 N = int(input())
+stdnts = list(map(int,input().split()))
+#stk = [stdnts.pop(0)]
 stk = []
-for _ in range(N):
-    cmd = list(map(int,input().split()))
+turn = 1
 
-    if cmd[0] == 1:
-        stk.append(cmd[1])
-    elif cmd[0] == 2:
-        if stk:
-            print(stk.pop())
-        elif not stk:
-            print(-1)
-    elif cmd[0] == 3:
-        print(len(stk))
-    elif cmd[0] == 4:
-        if stk:
-            print(0)
-        elif not stk:
-            print(1)
-    elif cmd[0] == 5:
-        if stk:
-            print(stk[-1])
-        elif not stk:
-            print(-1)
-    else:
-        print("???")
+while stdnts or stk:
+    #print(stdnts,stk)
+    if stdnts:
+        if stdnts[0] == turn:
+            stdnts.pop(0)
+            turn += 1
+        elif stdnts[0] != turn:
+            if stk and stk[-1] == turn:
+                stk.pop()
+                turn += 1
+            elif stk and stk[-1] != turn:
+                stk.append(stdnts.pop(0))
+            elif not stk:
+                stk.append(stdnts.pop(0))
+    elif not stdnts:
+        if stk and stk[-1] != turn:
+            break
+        if stk and stk[-1] == turn:
+            stk.pop()
+            turn += 1
+            #print(stk)
+
+if not stk and not stdnts:
+    print("Nice")
+else:
+    print("Sad")
+    
