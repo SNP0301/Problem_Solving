03bd728ab4cc67ded07fd44ffb9d2336d8504cdf
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-01 17:41:42 +0900

    [BOJ] 2805 solved

--- ps.py
+++ ps.py
@@ -1,57 +1,61 @@
 """
-[복잡도] O(n^2)? O(nlgn)??
-- K=7이어서 전체 종이 크기가 128*128인 경우
-    - 128*128 사이즈 1번 탐색
-    - 64*64 사이즈 4번 탐색
-    - 32*32 사이즈 16번 탐색
-    - ...
-    - (2**14개에 7번 접근) * 2 (0,1을 각각 탐색)
+[복잡도] O(n)?
+    - 나무의 수 * 탐색 수
+    - 나무 길이는 탐색 크기 설정할 때 말고는 별로 안 중요할 것 같은데 맞나?
+
+* sum이랑 전체에 대해 조건문 한번, 계산 한번 거는 것 중 뭐가 더 오래 걸리나?
 """
-def my_print(a):
-    for x in a:
-        print(x)
-    print()
 
-N = int(input())
-sz = N*2
-arr = [list(map(int,input().split())) for _ in range(N)]
+def get_wood(cur_trees):
+    wood = 0
+    for i in cur_trees:
+        if i >= 0:
+            wood += i
+    return wood
 
-blue = 0
-white = 0
-while True: #한번 돌 때 마다 128 -> 64 -> ... 으로 사이즈 줄여가며 탐색
-    sz = sz//2
+N, M = map(int,input().split())
+trees = list(map(int,input().split()))
+answer = list()
 
-    for start_x in range(0,N,sz):
-        for start_y in range(0,N,sz):
-            #print("FROM: [%d,%d]"%(start_x,start_y))
-            valid = True
-            for x in range(start_x,start_x+sz):
-                for y in range(start_y,start_y+sz):
-                    if arr[x][y] != 1:
-                        valid = False
-            #print("END: [%d,%d]"%(start_x,start_y))
-            if valid:
-                for x in range(start_x,start_x+sz):
-                    for y in range(start_y,start_y+sz):
-                        arr[x][y] = -1
-                blue += 1
+sz = 2000000000 ## 2 000 000 000
 
-    for start_x in range(0,N,sz):
-        for start_y in range(0,N,sz):
-            #print("FROM: [%d,%d]"%(start_x,start_y))
-            valid = True
-            for x in range(start_x,start_x+sz):
-                for y in range(start_y,start_y+sz):
-                    if arr[x][y] != 0:
-                        valid = False
-            #print("END: [%d,%d]"%(start_x,start_y))
-            if valid:
-                for x in range(start_x,start_x+sz):
-                    for y in range(start_y,start_y+sz):
-                        arr[x][y] = -1
-                white += 1
+while sz != 1:
+    cur_trees = [max(0,i-sz) for i in trees]
+    if get_wood(cur_trees) >= M:
+        answer.append(sz)
+        #print(sz)
+    sz = max(1,int(sz//2))
+
+sz = 1
+
+if answer:
+    cur = max(answer)
+else:
+    cur = 0
+
+while True:
+    cur_trees = [max(0,i-sz-cur) for i in trees]
+    if get_wood(cur_trees) >= M:
+        answer.append(cur+sz)
+        #print(cur+sz)
+        sz *= 2
+        cur += sz
+    elif get_wood(cur_trees) < M:
+        sz = max(1,int(sz//2))
+        cur -= sz
     if sz == 1:
+        while True:
+            cur_trees = [max(0,i-sz-cur) for i in trees]
+            if get_wood(cur_trees) >= M:
+                answer.append(cur+sz)
+                #print("#%d"%cur)
+                cur += 1
+            elif get_wood(cur_trees) < M:
+                #print("%d is failed"%cur)
+                break
         break
 
-print(white)
-print(blue)
+if answer:
+    print(max(answer))
+else:
+    print(1)
