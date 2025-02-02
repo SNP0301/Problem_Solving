ba55256ebeff1529990a46d438620e2968d5f4ca
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-02 21:52:22 +0900

    [BOJ] 2178 solved

--- ps.py
+++ ps.py
@@ -2,45 +2,40 @@
 [시간복잡도] O(N**2)
     - 전체 사람에 대해 완전탐색하므로, 100*100
 
-    
-* 베이컨 수 최소값이 같은 사람이 둘 이상인 경우 번호가 가장 작은 사람 출력 -> 작은 사람부터 bfs 시작, 더 작을 때만 갱신(같을 때는 갱신 x)
-** 친구 관계가 중복으로 들어올 수도 있다 -> 집합으로 adj 관리
+* 항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
+** 칸을 셀 때는 시작 위치와 도착 위치도 포함한다.
+
+소요시간 총 17분 내외
+    - 구상: 5분
+    - 구현: 12분
 """
 from collections import deque
 
-def bfs(s):
-    visited = [1] + [0 for _ in range(N)]
-    arr = [0] + [0 for _ in range(N)]
+
+def bfs(x,y):
     queue = deque()
-    queue.append((s,0))
-    visited[s] = 1
+    queue.append((x,y))
+    visited[x][y] = True
+    score_arr[x][y] = 1
     while queue:
-        s, score = queue.popleft()
-        for x in adj[s]:
-            if not visited[x]:
-                arr[x] += score + 1
-                visited[x] = True
-                queue.append((x,score+1))
-
-    return sum(arr)
-
-
-
+        x,y = queue.popleft()
+        for f in range(4): ## f of four
+            nx = x + dx[f]
+            ny = y + dy[f]
+            if 0<=nx<N and 0<=ny<M and not visited[nx][ny] and arr[nx][ny] != 0:
+                score_arr[nx][ny] = score_arr[x][y]+1
+                queue.append((nx,ny))
+                visited[nx][ny] = True
+                
 
 N, M = map(int,input().split())
-adj = [set() for _ in range(N+1)]
-cur_min = N*M+1
-answer = 1
-for _ in range(M):
-    s,e = map(int,input().split())
-    adj[s].add(e)
-    adj[e].add(s)
+arr = [list(map(int,input())) for _ in range(N)]
+score_arr = [[0 for _ in range(M)] for _ in range(N)]
+visited = [[False for _ in range(M)] for _ in range(N)]
 
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-for i in range(1,N+1):
-    b = bfs(i)
-    if b < cur_min:
-        answer = i
-        cur_min = b
+bfs(0,0)
 
-print(answer)
+print(score_arr[N-1][M-1])
