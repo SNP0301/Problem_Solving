d499603df7d044c690361959ade3ccdcaaefd473
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-02 21:29:37 +0900

    [BOJ] 1389 solved

--- ps.py
+++ ps.py
@@ -1,81 +1,46 @@
+"""
+[시간복잡도] O(N**2)
+    - 전체 사람에 대해 완전탐색하므로, 100*100
+
+    
+* 베이컨 수 최소값이 같은 사람이 둘 이상인 경우 번호가 가장 작은 사람 출력 -> 작은 사람부터 bfs 시작, 더 작을 때만 갱신(같을 때는 갱신 x)
+** 친구 관계가 중복으로 들어올 수도 있다 -> 집합으로 adj 관리
+"""
 from collections import deque
 
-def my_print(a):
-    for x in a:
-        print(x)
-    print()
+def bfs(s):
+    visited = [1] + [0 for _ in range(N)]
+    arr = [0] + [0 for _ in range(N)]
+    queue = deque()
+    queue.append((s,0))
+    visited[s] = 1
+    while queue:
+        s, score = queue.popleft()
+        for x in adj[s]:
+            if not visited[x]:
+                arr[x] += score + 1
+                visited[x] = True
+                queue.append((x,score+1))
 
-def check_move(gears):
-    do_move = [0,0,0]
-    for i in range(3):
-        if gears[i][2] != gears[i+1][6]:
-            do_move[i] = 1
-    return do_move
+    return sum(arr)
 
-def rotate(gear_num,watch): ## rotate(돌릴 기어의 번호, 시계 Or 반시계, from 왼쪽 방향 -1 or 오른쪽 방향 +1)
-    if watch == 1: ## 시계방향으로 돌리면
-        rotate_right(gear_num)
-    elif watch == -1: ## 반시계방향으로 돌리면
-        rotate_left(gear_num)
 
-def rotate_left(gear_num):                              #반시계 방향
-    gears[gear_num].append(gears[gear_num].popleft())
 
-def rotate_right(gear_num):                             ## 시계 방향
-    gears[gear_num].appendleft(gears[gear_num].pop())
 
-gears = list()
-for _ in range(4):
-    gears.append(deque(map(int,input())))
-#my_print(gears)
-M = int(input())
+N, M = map(int,input().split())
+adj = [set() for _ in range(N+1)]
+cur_min = N*M+1
+answer = 1
 for _ in range(M):
-    start_gear, watch = map(int,input().split())
-    do_move = check_move(gears)
-
-    if start_gear == 1:
-        rotate(0,watch)
-        if do_move[0] == 1:
-            rotate(1,-1*watch)
-            if do_move[1] == 1:
-                rotate(2,watch)
-                if do_move[2] == 1:
-                    rotate(3,-1*watch)
-    elif start_gear == 2:
-        rotate(1,watch)
-        if do_move[0] == 1:
-            rotate(0,-1*watch)
-        if do_move[1] == 1:
-            rotate(2,-1*watch)
-            if do_move[2] == 1:
-                rotate(3,watch)
-    elif start_gear == 3:
-        rotate(2,watch) 
-        if do_move[2] == 1:
-            rotate(3,-1*watch)
-        if do_move[1] == 1:
-            rotate(1,-1*watch)
-            if do_move[0] == 1:
-                rotate(0,watch)
+    s,e = map(int,input().split())
+    adj[s].add(e)
+    adj[e].add(s)
 
 
-    elif start_gear == 4:
-        rotate(3,watch)
-        if do_move[2] == 1:
-            rotate(2,-1*watch)
-            if do_move[1] == 1:
-                rotate(1,watch)
-                if do_move[0] == 1:
-                    rotate(0,-1*watch)
-    #my_print(gears)
-
-    ##my_print(gears)
-
-#my_print(gears)
-answer = 0
-cnt = 1
-for g in gears:
-    answer += g[0]*cnt
-    cnt *= 2
+for i in range(1,N+1):
+    b = bfs(i)
+    if b < cur_min:
+        answer = i
+        cur_min = b
 
 print(answer)
