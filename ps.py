a253d988fe0b8dc683e3cdf0513511b836d2407f
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-02 17:22:08 +0900

    [BOJ] 2257 solved:RE

--- ps.py
+++ ps.py
@@ -1,145 +1,39 @@
 """
-arr 안 쓰는게 더 시간/공간 측면에서 좋을 것.
+[복잡도] O(n)
+    - 화학식의 길이가 10,000을 넘지 않는다.
 
-
-[개인]
-    - 급하면 arr부터 세우고 보는 습관 버리기..
-        - FTR) Z
-    - x,y축 지금이라도 바꿔야하나?
+[실수]
+    - 분기를 너무 자세하게 나눠서, mece하지 않았다.
+    - 간단하게 생각할 필요가 있다 (겹치는 부분 합치기 등)
 """
 
-start = list(input().split())
 
-## ord("A") = 65
-## ord("H") = 72
+formula = input()
+chem_dct = dict()
+chem_dct["H"] = 1
+chem_dct["C"] = 12
+chem_dct["O"] = 16
+stk = []
 
-kx,ky = 8-int(start[0][1]), ord(start[0][0])-65## king 시작점 
-sx,sy = 8-int(start[1][1]), ord(start[1][0])-65## stone 시작점
+for x in formula:
+    if x in chem_dct:
+        stk.append(chem_dct[x])
+    elif x == "(":
+        stk.append(x)
+    elif x == ")":
+        tmp_answer = 0
+        while True:
+            cur = stk.pop()
+            if cur == "(":
+                break
+            tmp_answer += cur
+        if tmp_answer == 0:
+            continue
+        else:
+            stk.append(tmp_answer)
+    else:
+        n = stk.pop()
+        tmp_answer = n*int(x)
+        stk.append(tmp_answer)
 
-
-def my_print(a):
-    for x in a:
-        print(x)
-    print()
-arr = [[0 for _ in range(8)] for _ in range(8)]
-arr[kx][ky] = "K"
-arr[sx][sy] = "S"
-#print(kx,ky)
-#print(sx,sy)
-#my_print(arr)
-
-for _ in range(int(start[2])):
-    cmd = input()
-    #print(cmd)
-    if cmd == "B":
-        if 0<=kx+1<8 and 0<=ky<8:
-            if arr[kx+1][ky] == "S":
-                if 0<=sx+1<8:
-                    arr[sx][sy] = 0
-                    sx = sx+1
-                    arr[kx][ky] = 0
-                    kx = kx+1
-
-            elif arr[kx+1][ky] != "S":
-                arr[kx][ky] = 0
-                kx = kx+1
-
-    elif cmd == "L":
-        if 0<=kx<8 and 0<=ky-1<8:
-            if arr[kx][ky-1] == "S":
-                if 0<=sy-1<8:
-                    arr[sx][sy] = 0
-                    sy = sy-1
-                    arr[kx][ky] = 0
-                    ky = ky-1
-            else:
-                arr[kx][ky] = 0
-                ky = ky-1
-    elif cmd == "R":
-        if 0<=kx<8 and 0<=ky+1<8:
-            if arr[kx][ky+1] == "S":
-                if 0<=sy+1<8:
-                    arr[sx][sy] = 0
-                    sy = sy+1
-                    arr[kx][ky] = 0
-                    ky = ky+1
-            else:
-                arr[kx][ky] = 0
-                ky = ky+1
-    elif cmd == "T":
-        if 0<=kx-1<8 and 0<=ky<8:
-            if arr[kx-1][ky] == "S":
-                if 0<=sx-1<8:
-                    arr[sx][sy] = 0
-                    sx -= 1
-                    arr[kx][ky] = 0
-                    kx -= 1
-            else:
-                arr[kx][ky] = 0
-                kx -= 1
-    elif cmd == "RT":
-        if 0<=kx-1<8 and 0<=ky+1<8:
-            if arr[kx-1][ky+1] == "S":
-                if 0<=sx-1<8 and 0<=sy+1<8:
-                    arr[sx][sy] = 0
-                    sx -= 1
-                    sy += 1
-                    arr[kx][ky] = 0
-                    kx -= 1
-                    ky += 1
-            else:
-                arr[kx][ky] = 0
-                kx -= 1
-                ky += 1
-    elif cmd == "LT":
-        if 0<=kx-1<8 and 0<=ky-1<8:
-            if arr[kx-1][ky-1] == "S":
-                if 0<=sx-1<8 and 0<=sy-1<8:
-                    arr[sx][sy] = 0
-                    sx -= 1
-                    sy -= 1
-                    arr[kx][ky] = 0
-                    kx -= 1
-                    ky -= 1
-            else:
-                arr[kx][ky] = 0
-                kx -= 1
-                ky -= 1
-    elif cmd == "RB":
-        if 0<=kx+1<8 and 0<=ky+1<8:
-            if arr[kx+1][ky+1] == "S":
-                if 0<=sx+1<8 and 0<=sy+1<8:
-                    arr[sx][sy] = 0
-                    sx += 1
-                    sy += 1
-                    arr[kx][ky] = 0
-                    kx += 1
-                    ky += 1
-            else:
-                arr[kx][ky] = 0
-                kx += 1
-                ky += 1
-    elif cmd == "LB":
-        if 0<=kx+1<8 and 0<=ky-1<8:
-            if arr[kx+1][ky-1] == "S":
-                if 0<=sx+1<8 and 0<=sy-1<8:
-                    arr[sx][sy] = 0
-                    sx += 1
-                    sy -= 1
-                    arr[kx][ky] = 0
-                    kx += 1
-                    ky -= 1
-            else:
-                arr[kx][ky] = 0
-                kx += 1
-                ky -= 1
-
-    arr[kx][ky] = "K"
-    arr[sx][sy] = "S"
-  
-    #my_print(arr)
-y_arr = ["","A","B","C","D","E","F","G","H",""]
-print(y_arr[ky+1],end="")
-print(8-kx)
-print(y_arr[sy+1],end="")
-print(8-sx)
+print(sum(stk))
--- tmp.py
+++ tmp.py
@@ -1,75 +1,119 @@
-"""
-[복잡도] O(N**2)? O(NlgN)?
-    - 재귀의 경우 depth로 따지는건가?
 
-[실수]
-    - 이미 계산했던 값을 사용해야 하는거면 arr가 맞지만, 하나의 값을 찾는데 다른 값이 영향을 주지 않는다면 arr를 선언할 필요는 없다.
-        - 특히 이 문제에서 (2**15)*(2**15) arr를 사용하면 서버 터진다.
-    - 표가 나왔냐/안 나왔냐를 보고 배열을 짤게 아니고, 찾으려는 값이 어떤 값인지, 그 값이 이전 값의 영향을 받는지/안받는지, 순서가 정해진 목록 중 단일 값을 찾는 것인지를 먼저 살펴볼 것.
+def left(gear):
+    gear.append(gear.popleft())
 
-[배운 것]
-    - 함수를 완벽하게 짰다는 전제 하에 재귀함수에서 return 자기자신을 하면, 해당 함수에서 얻고자 하는 출력/값을 얻을 때까지 계산한다.
-        - 예) 피보나치도 return fib 하면 자기가 호출했던 fib 결과를 모두 합쳐서 되돌려준다.
+def right(gear):
+    gear.appendleft(gear.pop())
 
-[틀린 코드] - 메모리 메모리 메모리
-import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
+gears = [deque() for _ in range(4)]
+
+for i in range(4):
+    info = input()
+    for j in range(len(info)):
+        gears[i].append(info[j])
 
 
-def my_print(a):
-    for x in a:
-        print(x)
-    print()
-
-def do_z(x,y,N,cur):
-    if N == 1:
-        for f in range(4): ## f of four
-            nx = x+dx[f]
-            ny = y+dy[f]
-            answer[nx][ny] = cur
-            cur += 1
-    else:
-        do_z(x,y,N-1,cur+2**(2*N-2)*0)
-        do_z(x,y+2**(N-1),N-1,cur+2**(2*N-2)*1)
-        do_z(x+2**(N-1),y,N-1,cur+2**(2*N-2)*2)
-        do_z(x+2**(N-1),y+2**(N-1),N-1,cur+2**(2*N-2)*3)
-
-N, r, c = map(int,input().split())
-answer = [[0 for _ in range(2**N)] for _ in range(2**N)]
-cur = 0
-dx = [0,0,1,1]
-dy = [0,1,0,1]
-
-do_z(0,0,N,cur)
-print(answer[r][c])
-"""
-import sys
-sys.setrecursionlimit(10**6)
-
-def do_z(x,y,N,cur):
-    if N == 1:
-        for f in range(4): ## f of four
-            nx = x+dx[f]
-            ny = y+dy[f]
-            if nx == r and ny == c:
-                return cur
-            cur += 1
-    else: ## 2**(2*N-2)만큼 갔는데도 r,c각각 못 찾으면, 못 찾은 방향으로 2**(2*N-2) 만큼 이동
-        if r < x+2**(N-1) and c < y+2**(N-1): ## 이미 지나간 경우, 지나온 방향으로 크기 줄여서 탐색
-            return do_z(x,y,N-1,cur+2**(2*N-2)*0)
-        
-        elif r < x+2**(N-1) and c >= y+2**(N-1): ## x는 아직 남았는데 y는 못 지나갔으면
-            return do_z(x,y+2**(N-1),N-1,cur+2**(2*N-2)*1)
-        
-        elif  r >= x+2**(N-1) and c < y+2**(N-1): ## y는 아직 남았는데 x는 못 지나갔으면
-            return do_z(x+2**(N-1),y,N-1,cur+2**(2*N-2)*2)
-        
+M = int(input())
+for _ in range(M):
+    cmd = list(map(int,input().split()))
+    do_move = list()
+    for i in range(3):
+        if gears[i][2] == gears[i+1][-2]:
+            do_move.append(0)
         else:
-            return do_z(x+2**(N-1),y+2**(N-1),N-1,cur+2**(2*N-2)*3)
+            do_move.append(1)
+    #print(do_move)
+    if cmd[0] == 1: ## 1-> 2-> 3-> 4
+        if cmd[1] == -1:
+            left(gears[0])
+        else:
+            right(gears[0])
 
-N, r, c = map(int,input().split())
-cur = 0
-dx = [0,0,1,1]
-dy = [0,1,0,1]
+        if do_move[0] == 1:
+            if cmd[1] == -1:
+                right(gears[1])
+                if do_move[1] == 1:
+                    left(gears[2])
+                    if do_move[2] == 1:
+                        right(gears[3])
+            if cmd[1] == 1:
+                left(gears[1])
+                if do_move[1] == 1:
+                    right(gears[2])
+                    if do_move[2] == 1:
+                        left(gears[3])
 
-print(do_z(0,0,N,0))
+    elif cmd[0] == 2: ## 1<- 2-> 3-> 4
+        if cmd[1] == -1:
+            left(gears[1])
+        else:
+            right(gears[1])
+        if do_move[0] == 1:
+            if cmd[1] == -1:
+                right(gears[0])
+            elif cmd[1] == 1:
+                left(gears[0])
+
+        if do_move[1] == 1:
+            if cmd[1] == -1:
+                right(gears[2])
+            elif cmd[1] == 1:
+                left(gears[2])
+                if do_move[2] == 1:
+                    if cmd[1] == -1:
+                        right(gears[3])
+                    elif cmd[1] == 1:
+                        left(gears[3])
+
+    elif cmd[0] == 3: ## 1<- 2<- 3-> 4
+        if cmd[1] == -1:
+            left(gears[2])
+        else:
+            right(gears[2])
+
+        if do_move[2] == 1:
+            if cmd[1] == -1:
+                right(gears[3])
+            if cmd[1] == 1:
+                left(gears[3])
+        
+        if do_move[1] == 1:
+            if cmd[1] == -1:
+                right(gears[1])
+            elif cmd[1] == 1:
+                left(gears[1])
+                if do_move[0] == 1:
+                    if cmd[1] == -1:
+                        left(gears[0])
+                    elif cmd[1] == 1:
+                        right(gears[0])
+        
+                
+    elif cmd [0] == 4: ## 1<- 2<- 3<- 4
+        if cmd[1] == -1:
+            left(gears[3])
+        else:
+            right(gears[3])
+
+        if do_move[2] == 1:
+            if cmd[1] == -1:
+                right(gears[2])
+                if do_move[1] == 1:
+                    left(gears[1])
+                    if do_move[0] == 1:
+                        right(gears[0])
+            if cmd[1] == 1:
+                left(gears[2])
+                if do_move[1] == 1:
+                    right(gears[1])
+                    if do_move[0] == 1:
+                        left(gears[0])
+    #my_gears(gears)
+answer = 0
+cnt = 1
+for i in range(4):
+    answer += int(gears[i][0])*cnt
+    cnt *= 2
+
+print(answer)
