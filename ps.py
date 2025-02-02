bec30a9dfa3a454d9b60dd2ccc8cd76e0a927485
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-02 12:57:51 +0900

    [BOJ] 1074 solved:RE

--- ps.py
+++ ps.py
@@ -1,32 +1,83 @@
 """
-[복잡도] O(N)
-    - left 포인터와 right 포인터가 각각 N번 이동하므로 O(n)
-        - right 포인터가 한번 움직일 때, 최악의 경우 left 포인터가 N-1회 움직이는 경우 N*(N-1)로 생각할 수 있으나, 이런 케이스는 최대 1번 발생
+[복잡도] O(lgN)?
+    - 이분탐색은 input이 시행마다 지수승으로 줄어드니까?
 
-*무조건 다시 풀어볼 것.
+[실수]
+    1. 이미 계산했던 값을 사용해야 하는거면 arr가 맞지만, 하나의 값을 찾는데 다른 값이 영향을 주지 않는다면 arr를 선언할 필요는 없다.
+        - 특히 이 문제에서 (2**15)*(2**15) arr를 사용하면 서버 터진다.
+    2. 표가 나왔냐/안 나왔냐를 보고 배열을 짤게 아니고, 찾으려는 값이 어떤 값인지, 그 값이 이전 값의 영향을 받는지/안받는지, 순서가 정해진 목록 중 단일 값을 찾는 것인지를 먼저 살펴볼 것.
+
+[배운 것]
+    1. 함수를 완벽하게 짰다는 전제 하에 재귀함수에서 return 자기자신을 하면, 해당 함수에서 얻고자 하는 출력/값을 얻을 때까지 계산한다.
+        - 예) 피보나치도 return fib 하면 자기가 호출했던 fib 결과를 모두 합쳐서 되돌려준다.
+    2. 이분탐색 자체는 탐색 범위를 반으로 줄여가면서 탐색한다. 정렬된 배열에서 사용한다.
+        - 정렬됐는가?: 이 문제는 값 자체가 정렬된 것은 아니지만 Z모양 탐색 순서가 변칙없이 정해져있다.
+        - 반으로 줄일 수 있는가? : 주어진 표가 2**N 길이의 변만 가지므로 반으로 줄여가면서 탐색할 수 있다.
+        - FTR) 랜선 자르기, 나무 자르기
+
+[틀린 코드] - 메모리 메모리 메모리
+import sys
+sys.setrecursionlimit(10**6)
+
+
+def my_print(a):
+    for x in a:
+        print(x)
+    print()
+
+def do_z(x,y,N,cur):
+    if N == 1:
+        for f in range(4): ## f of four
+            nx = x+dx[f]
+            ny = y+dy[f]
+            answer[nx][ny] = cur
+            cur += 1
+    else:
+        do_z(x,y,N-1,cur+2**(2*N-2)*0)
+        do_z(x,y+2**(N-1),N-1,cur+2**(2*N-2)*1)
+        do_z(x+2**(N-1),y,N-1,cur+2**(2*N-2)*2)
+        do_z(x+2**(N-1),y+2**(N-1),N-1,cur+2**(2*N-2)*3)
+
+N, r, c = map(int,input().split())
+answer = [[0 for _ in range(2**N)] for _ in range(2**N)]
+cur = 0
+dx = [0,0,1,1]
+dy = [0,1,0,1]
+
+do_z(0,0,N,cur)
+print(answer[r][c])
 """
-N = int(input())
-thr = list(map(int,input().split()))
-thr_dct = dict()
-fruit_type_counter = 0
-answer = 0
-lft = 0
-rgt = 0
 
-for rgt in range(N):
-    if thr[rgt] in thr_dct:
-        thr_dct[thr[rgt]] += 1
-    elif thr[rgt] not in thr_dct:
-        thr_dct[thr[rgt]] = 1
-        fruit_type_counter += 1
-    
-    while fruit_type_counter > 2:
-        thr_dct[thr[lft]] -= 1
-        if thr_dct[thr[lft]] == 0:
-            del thr_dct[thr[lft]]
-            fruit_type_counter -= 1
-        lft += 1
+import sys
 
-    answer = max(answer,rgt-lft+1)
+N, r, c = map(int, sys.stdin.readline().split())
 
-print(answer)
+cur = 0
+sz = 2 ** (N - 1)  ## N이 1일 때 1*1씩, 2일 때 2*2씩
+
+while sz > 0: 
+    cur_full_size = sz * sz  ## 지금 탐색하려는 범위 전체의 사이즈
+
+    ## 2**(2*N-2)만큼 갔는데도 r,c각각 못 찾으면, 못 찾은 방향으로 2**(2*N-2) 만큼 이동
+    if r < sz and c < sz:
+        cur += cur_full_size * 0
+
+    ## x는 아직 남았는데 y는 못 지나갔으면
+    elif r < sz and c >= sz:
+        cur += cur_full_size * 1
+        c -= sz
+
+    ## y는 아직 남았는데 x는 못 지나갔으면
+    elif r >= sz and c < sz:
+        cur += cur_full_size * 2
+        r -= sz  
+
+    ## 나머지 경우
+    else:
+        cur += cur_full_size * 3
+        r -= sz
+        c -= sz
+
+    sz //= 2  # 크기를 절반으로 줄이면서 탐색
+
+print(cur)
--- tmp.py
+++ tmp.py
@@ -1,35 +1,75 @@
 """
-* 리스트 슬라이스 연습용 문제
+[복잡도] O(N**2)? O(NlgN)?
+    - 재귀의 경우 depth로 따지는건가?
+
+[실수]
+    - 이미 계산했던 값을 사용해야 하는거면 arr가 맞지만, 하나의 값을 찾는데 다른 값이 영향을 주지 않는다면 arr를 선언할 필요는 없다.
+        - 특히 이 문제에서 (2**15)*(2**15) arr를 사용하면 서버 터진다.
+    - 표가 나왔냐/안 나왔냐를 보고 배열을 짤게 아니고, 찾으려는 값이 어떤 값인지, 그 값이 이전 값의 영향을 받는지/안받는지, 순서가 정해진 목록 중 단일 값을 찾는 것인지를 먼저 살펴볼 것.
 
 [배운 것]
-- 조건문 분기를 최대한 꼼꼼하게 해야 구현도 쉽다
-- idx 0, 1인 경우를 따로 세면 나중에 불편해진다.
+    - 함수를 완벽하게 짰다는 전제 하에 재귀함수에서 return 자기자신을 하면, 해당 함수에서 얻고자 하는 출력/값을 얻을 때까지 계산한다.
+        - 예) 피보나치도 return fib 하면 자기가 호출했던 fib 결과를 모두 합쳐서 되돌려준다.
+
+[틀린 코드] - 메모리 메모리 메모리
+import sys
+sys.setrecursionlimit(10**6)
+
+
+def my_print(a):
+    for x in a:
+        print(x)
+    print()
+
+def do_z(x,y,N,cur):
+    if N == 1:
+        for f in range(4): ## f of four
+            nx = x+dx[f]
+            ny = y+dy[f]
+            answer[nx][ny] = cur
+            cur += 1
+    else:
+        do_z(x,y,N-1,cur+2**(2*N-2)*0)
+        do_z(x,y+2**(N-1),N-1,cur+2**(2*N-2)*1)
+        do_z(x+2**(N-1),y,N-1,cur+2**(2*N-2)*2)
+        do_z(x+2**(N-1),y+2**(N-1),N-1,cur+2**(2*N-2)*3)
+
+N, r, c = map(int,input().split())
+answer = [[0 for _ in range(2**N)] for _ in range(2**N)]
+cur = 0
+dx = [0,0,1,1]
+dy = [0,1,0,1]
+
+do_z(0,0,N,cur)
+print(answer[r][c])
 """
+import sys
+sys.setrecursionlimit(10**6)
 
-P = int(input())
-for p in range(1,P+1):
-    answer = 0
-    info = list(map(int,input().split()))
-    info.pop(0)
-    stood = [info.pop(0)]
-    for _ in range(19):
-        cur = info.pop(0)
-        ##print(cur,answer)
-        if cur > stood[-1]: ##최대값보다 크다면
-            stood.append(cur)
-        elif cur < stood[0]: ##최소값보다 작다면
-            #print("%d made +%d"%(cur,len(stood)))
-            answer += len(stood)
-            stood = [cur] + stood
-        elif stood[0]<cur<stood[-1]: ##그 사이라면
-            for i in range(len(stood)-1,-1,-1):
-                if cur > stood[i]: ## 자리를 찾으면
-                    #print("%d made +%d"%(cur,len(stood[i:])))
-                    answer += len(stood[i:])-1
-                    stood = stood[:i+1] + [cur] + stood[i+1:]
+def do_z(x,y,N,cur):
+    if N == 1:
+        for f in range(4): ## f of four
+            nx = x+dx[f]
+            ny = y+dy[f]
+            if nx == r and ny == c:
+                return cur
+            cur += 1
+    else: ## 2**(2*N-2)만큼 갔는데도 r,c각각 못 찾으면, 못 찾은 방향으로 2**(2*N-2) 만큼 이동
+        if r < x+2**(N-1) and c < y+2**(N-1): ## 이미 지나간 경우, 지나온 방향으로 크기 줄여서 탐색
+            return do_z(x,y,N-1,cur+2**(2*N-2)*0)
+        
+        elif r < x+2**(N-1) and c >= y+2**(N-1): ## x는 아직 남았는데 y는 못 지나갔으면
+            return do_z(x,y+2**(N-1),N-1,cur+2**(2*N-2)*1)
+        
+        elif  r >= x+2**(N-1) and c < y+2**(N-1): ## y는 아직 남았는데 x는 못 지나갔으면
+            return do_z(x+2**(N-1),y,N-1,cur+2**(2*N-2)*2)
+        
+        else:
+            return do_z(x+2**(N-1),y+2**(N-1),N-1,cur+2**(2*N-2)*3)
 
+N, r, c = map(int,input().split())
+cur = 0
+dx = [0,0,1,1]
+dy = [0,1,0,1]
 
-
-
-        #print(stood)
-    print(p, answer)
+print(do_z(0,0,N,0))
