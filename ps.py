bd118d8a2ec43245d9bc8143363e77018cbdb488
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-02 22:54:44 +0900

    [BOJ] 2740 solved

--- ps.py
+++ ps.py
@@ -1,52 +1,22 @@
 """
-[시간복잡도] O(N**2)
-    - 전체 지도에 대해 완전탐색 -> 25*25
+[시간복잡도] O(N*M)
 
-* 연결되었다 = 상하좌우로 인접 -> 대각선은 연결된 것이 아니다.
-** 단지의 수를 출력, 각 단지의 세대수를 오름차순으로 출력
-
-소요시간 총 12분 내외
-    - 구상: 3분
-    - 구현: 9분
+소요시간 총 20분 내외
+    - 구상: ~15분
+    - 구현: ~5분
 """
-from collections import deque
-
-def bfs(x,y):
-    queue = deque()
-    queue.append((x,y))
-    visited[x][y] = True
-    num = 1
-    while queue:
-        x,y = queue.popleft()
-        for f in range(4): # f of four
-            nx = x+dx[f]
-            ny = y+dy[f]
-            if 0<=nx<N and 0<=ny<N and mp[nx][ny] and not visited[nx][ny]:
-                num += 1 ## 세대 수 하나 추가
-                queue.append((nx,ny)) ## 해당 단지 내에서 탐색할 다음 세대
-                visited[nx][ny] = True
-    return num
-
-N = int(input())
-mp = [list(map(int,input())) for _ in range(N)]
-visited = [[False for _ in range(N)] for _ in range(N)]
-answer_arr = list() ## 단지 내 세대수를 오름차순으로 정렬하기 위한 배열
-cnt = 0 ## 단지 수
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+N, M = map(int,input().split())
+A = [list(map(int,input().split())) for _ in range(N)]
+M, K = map(int,input().split())
+B = [list(map(int,input().split())) for _ in range(M)]
+C = [[0 for _ in range(K)] for _ in range(N)]
 
 for x in range(N):
-    for y in range(N):
-        if mp[x][y] == 1 and not visited[x][y]:
-            answer_arr.append(bfs(x,y))
-            cnt += 1
+    for y in range(K):
+        for m in range(M):
+            C[x][y] += A[x][m] * B[m][y]
 
-
-##############################################################
-
-print(cnt)
-
-answer_arr = sorted(answer_arr)
-for i in answer_arr:
-    print(i)
+for c in C:
+    for e in c:
+        print(e,end=" ")
+    print()
