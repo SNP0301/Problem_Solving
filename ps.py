2dd931d6f59f7bbf05e8ccf245d9a15894e9e6dd
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-02 09:13:41 +0900

    [BOJ] 21736 solved

--- ps.py
+++ ps.py
@@ -1,21 +1,51 @@
 """
-[복잡도] O(log n)
-    - heapq는 push, pop 모두 O(log n)을 따른다
+[복잡도] O(N**2)
+    - 캠퍼스 최대 크기인 600*600에 대해 접근
+        - 접근시마다 최대 4방향에 대해 탐색
 """
-import heapq
-import sys
-input = sys.stdin.readline
+from collections import deque
 
-N = int(input())
-hpq = list()
-heapq.heapify(hpq)
+def bfs(x,y):
+    answer = 0
+    queue = deque()
+    queue.append((x,y))
+    visited[x][y] = True
+    while queue:
+        x, y = queue.popleft()
+        for f in range(4): # f of four
+            nx = x+ dx[f]
+            ny = y+ dy[f]
+            if 0<=nx<N and 0<=ny<M and not visited[nx][ny]:
+                if campus[nx][ny] == "O":
+                    visited[nx][ny] = True
+                    queue.append((nx,ny))
+                elif campus[nx][ny] == "P":
+                    visited[nx][ny] = True
+                    queue.append((nx,ny))
+                    answer += 1
+                elif campus[nx][ny] == "X":
+                    visited[nx][ny] = True
+                
+    return answer
 
-for _ in range(N):
-    cmd = int(input())
-    if cmd == 0:
-        if not hpq:
-            print(0)
-        elif hpq:
-            print(heapq.heappop(hpq)*-1)
-    else:
-        heapq.heappush(hpq,cmd*-1)
+
+
+N, M = map(int,input().split())
+campus = [list(input()) for _ in range(N)]
+visited = [[False for _ in range(M)] for _ in range(N)]
+dx = [1,-1,0,0]
+dy = [0,0,-1,1]
+
+for x in range(N):
+    for y in range(M):
+        if campus[x][y] == 'I':
+            start_x = x
+            start_y = y
+
+answer = bfs(start_x, start_y)
+
+if answer == 0:
+    print("TT")
+else:
+    print(answer)
+
