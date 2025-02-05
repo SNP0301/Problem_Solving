c2640291d56a0a5fc0231c3f65459b261f8eb460
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-05 23:09:43 +0900

    [BOJ] 1325 solved

--- ps.py
+++ ps.py
@@ -1,69 +1,51 @@
 """
-* 스터디
-[복잡도] O(N**2)
-    - 5*5 배열에 대해 빙고 검사
-        - 빙고 검사 1회마다 60개의 칸에 대해 연산
-
-
+[복잡도] O(N+M)
+    - N개의 노드와 M개의 간선에 대해 완전탐색
 [구상]
-    I: 빙고판에 적힌 수, 사회자가 부르는 수
-    O: 사회자가 몇번째 수를 불렀을 때 철수가 빙고를 외치는지
+    I: 노드와 간선의 수, 단방향 간선의 연결 정보
+    O: (오름차순) 한 번에 가장 많은 컴퓨터를 해킹할 수 있는 컴퓨터의 번호
 
-* 선이 3개 이상 그어지는 순간 빙고를 외친다
-
-[] 문제 읽기 -> [] I/O 확인 -> [] 제약조건/특이사항 확인 -> [] 구상 -> [] 복잡도 계산 -> [] 손 구현 -> [] 구현 -> [] 오픈테케 -> []히든테케
+[보완할 점]
+    - (89590503) N이 10,000이고 M이 100,000이어서 시간초과/메모리초과를 과도하게 고려
+                                                ㄴ 감각,,,
 """
 from collections import deque
-
-def count_bingo():
+def bfs(s):
     cnt = 0
-    for x in range(5):
-        if sum(board[x]) == 0:
-            cnt += 1
-        if sum(zip_board[x]) == 0:
-            cnt += 1
-    
-    bd_cnt = 0
-    zbd_cnt = 0
-    for i in range(5):
-        if board[i][i] == 0:
-            bd_cnt += 1
-        if zip_board[i][4-i] == 0:
-            zbd_cnt += 1
+    visited = [False]*(N+1)        ## 컴퓨터 수가 1부터 시작하므로 N+1로 선언
+    queue = deque()
+    queue.append(s)
+    visited[s] = True
 
-    if zbd_cnt == 5:
-        cnt += 1
-    if bd_cnt == 5:
-        cnt += 1
-    return cnt
+    while queue:
+        s = queue.popleft()
+        for i in adj[s]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+                cnt += 1
+    return cnt                      ## 해킹할 수 있는 컴퓨터 수 반환
 
-def check_num(num):
-    for i in range(5):
-        for j in range(5):
-            if board[i][j] == num:
-                board[i][j] = 0
-            if zip_board[i][j] == num:
-                zip_board[i][j] = 0
+N, M = map(int,input().split())
+adj = [[] for _ in range(N+1)]      ## 컴퓨터 수가 1부터 시작하므로 N+1로 선언
+mx = -1
+mx_lst = list()
 
+for _ in range(M):
+    e,s = map(int,input().split()) 
+    adj[s].append(e)                ##  단방향
 
-board = [list(map(int,input().split())) for _ in range(5)]
-zip_board = [list(tmp) for tmp in zip(*board)]
-ment = deque()
-num = 0
-answer = 1
-
-for _ in range(5):
-    info = list(map(int,input().split()))
-    for x in info:
-        ment.append(x)
-
-while ment:        
-    num = ment.popleft()
-    check_num(num)
+for i in range(1,N+1):
+    h = bfs(i)
+    if h > mx:                      # 최대값 갱신
+        mx = h
+        mx_lst = list()
+        mx_lst.append(i)
+    elif h == mx:                   # 현재의 최대값과 같은 값을 갖는 컴퓨터 추가
+        mx_lst.append(i)
 
 
-    if count_bingo() >= 3:
-        print(answer)
-        break
-    else:
-        answer += 1
+mx_lst.sort()
+
+for i in mx_lst:
+    print(i,end=" ")
