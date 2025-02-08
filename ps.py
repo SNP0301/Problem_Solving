912ceba390d0b90edebe36a003e33fdaae0d6f93
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-08 14:22:29 +0900

    [BOJ] 30336 solved

--- ps.py
+++ ps.py
@@ -1,76 +1,100 @@
 """
-* 중난도 스터디
-[복잡도] O(N)
-    - 총 4개의 입력 명령을 수행
-        - 명령당 최대 3<=R,C<=1_000 개의 배열 요소 탐색
+* 팀 스터디
+[복잡도] 
 
 [구상]
-    I: 방의 크기 3<=R,C<=1_000, 장애물의 개수 0<=K<=1_000
-        - 장애물의 위치 0<=br<=R-1, 0<=bc<=C-1
-        - 로봇의 시작 위치 0<=sr<=R-1, 0<=sc<=C-1
-    O: 로봇의 마지막 위치 r,c
+    I: 잉크 문자열의 크기 I, 맵의 크기 N, 커맨드의 길이 K
+        - 잉크 문자열
+        - N*N 사이즈의 스테이지
+        - K개의 커맨드 (공백 없음)
+    O: N*N 사이즈의 스테이지 최종 모습
+        - 사각형은 "@", 빈칸은 ".", 염색되지 않은 장애물은 "#", 염색된 장애물은 염색 잉크 알파벳 (대문자)
 
 [고려사항]
-    - 로봇의 시작 위치에 장애물이 있는 경우는 없다.
-    - 로봇은 지정한 방향을 일직선으로 움직인다 => 장애물을 만나거나, 벽을 만나거나, 방문했던 지역을 만날 때까지 이동
-        - 장애물이 아니고, 벽도 아니고, 방문했던 지역도 아니어야 이동 가능
-    - 사용자가 지정한 다음 방향이 없다면 맨 처음 방향으로 돌아가서 위의 과정을 반복
-        - 4개의 명령이 끝나면 다시 명령[0]부터 시행
-    - 로봇이 움직일 수 없을 경우 동작을 멈춘다.
-        - 움직임을 for에서 관리하고, while 밑의 else에서 움직임 flag를 확인해 동작 멈춤 여부 결정
+    - y행 x열
+    - 이동
+        - 움직이는 물체 이름이 사각형, "@"로 표현.
+        - 밖으로 벗어나거나 이동하려는 칸에 장애물이 있으면 해당 커맨드 무시
+    - 잉크 충전
+        - j가 들어오면 잉크 양 += 1
+    - 점프
+        - J가 들어오면
+            1) "제자리에서" 점프
+            2) 사각형이 가진 잉크 양만큼 범위 내의 "장애물"에 잉크를 뿌린다
+                - 색상은 지금 써야 하는 잉크 색상
+                - 범위는 abs(sx-x)-abs(sy-y) <= 잉크 양을 만족하는 x,y
+                - 잉크 양은 0이 된다
+                - 잉크 색상은 점프 커맨드 입력 횟수에 의해 정해진다
+                    - (점프횟수)%잉크문자열 길이
+                        R -> G -> B -> R -> G -> B -> R -> ...
 
 """
-from collections import defaultdict, deque
+from collections import deque
 
 def my_print(a):
     for x in a:
         print(x)
     print()
-R,C = map(int,input().split())
-k = int(input())
-room = [[0 for _ in range(C)] for _ in range(R)]
-for _ in range(k):
-    br, bc = map(int,input().split())
-    room[br][bc] = "X"
-sx, sy = map(int,input().split())
-room[sx][sy] = 1
-cmd = list(map(int,input().split()))
-move_dct = defaultdict(int)
-not_moved_cnt = 0
 
-#my_print(room)
-for i in range(4):
-    if cmd[i] == 1:
-        move_dct[i] = (-1,0)
-    elif cmd[i] == 2:
-        move_dct[i] = (1,0)
-    elif cmd[i] == 3:
-        move_dct[i] = (0,-1)
-    elif cmd[i] == 4:
-        move_dct[i] = (0,1)
+I, N, K = map(int,input().split())
+my_ink = deque(input())
+stage = [list(input()) for _ in range(N)]
+commands = deque(input())
 
+ix = 0
+iy = 0
+ink_amnt = 0
+found = False
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-d = 0
-while not_moved_cnt < 4:
-    cnt = 1
-    moved= False
-    while True:
-        nx = sx + move_dct[d][0]
-        ny = sy + move_dct[d][1]
-        if 0<=nx<R and 0<=ny<C and room[nx][ny] == 0:
-            #print(sx,sy,"---->",nx,ny,move_dct[d])
-            room[nx][ny] = 1
-            #my_print(room)
-            sx = nx
-            sy = ny
-            moved = True
-            not_moved_cnt = 0
-
-        else:
-            d = (d+1)%4         ## 움직임 방향 바꿔주기
+for x in range(N):
+    for y in range(N):
+        if stage[x][y] == "@":
+            ix, iy = x, y
+            found = True
             break
-    if not moved:
-        not_moved_cnt += 1
+    if found:
+        break
 
-    
-print(sx,sy)
+while commands:
+    cur_cmd = commands.popleft()
+
+    if cur_cmd == 'j':
+        ink_amnt += 1
+
+    elif cur_cmd == "J":
+        for x in range(N):
+            for y in range(N):
+                if abs(ix - x) + abs(iy - y) <= ink_amnt:
+                    if stage[x][y] != "." and stage[x][y] != "@":
+                        stage[x][y] = my_ink[0]
+        my_ink.append(my_ink.popleft())
+        ink_amnt = 0
+
+    elif cur_cmd in ["U","D","L","R"]:
+        if cur_cmd == "U":
+            if 0<=ix-1<N and stage[ix-1][iy] == ".":
+                stage[ix][iy] = "."
+                ix -= 1
+                stage[ix][iy] = "@"
+        elif cur_cmd == "D":
+            if 0<=ix+1<N and stage[ix+1][iy] == ".":
+                stage[ix][iy] = "."
+                ix += 1
+                stage[ix][iy] = "@"
+        elif cur_cmd == "L":
+            if 0<=iy-1<N and stage[ix][iy-1] == ".":
+                stage[ix][iy] = "."
+                iy -= 1
+                stage[ix][iy] = "@"
+        elif cur_cmd == "R":
+            if 0<=iy+1<N and stage[ix][iy+1] == ".":
+                stage[ix][iy] = "." 
+                iy += 1
+                stage[ix][iy] = "@"
+        
+for x in stage:
+    for y in x:
+        print(y,end="")
+    print()
