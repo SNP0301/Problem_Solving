c14010da1810bd4a761fb94a4b7eac62ff42a817
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-02-08 23:50:45 +0900

    [BOJ] 30036 solved

--- ps.py
+++ ps.py
@@ -1,100 +1,120 @@
 """
-* 팀 스터디
-[복잡도] 
+* 중난도 스터디
+가장 큰 잘못: 백신일 수 있는 기준 자체에 틈이 있었던 것 같다.
+    - 무조건 항체 맞은 자리에서 뻗어나갈 수 있을만큼 쭉 뻗어나가는 것으로 전제
+        - 근데 애초에 백신이 아니면... 안 뻗어나갈 수도 있다.
+    - 문제를 조금 더 간결하게 생각했으면 어땠을까?
+        - 너무 막연하다고만 느끼니까 뭔가 더 큰게 있다고 생각하고 스스로 문제를 어렵게 만들었다.
+
+[복잡도] O(N**2)
+    - 30*30의 촬영 결과에 대해 완전 탐색
 
 [구상]
-    I: 잉크 문자열의 크기 I, 맵의 크기 N, 커맨드의 길이 K
-        - 잉크 문자열
-        - N*N 사이즈의 스테이지
-        - K개의 커맨드 (공백 없음)
-    O: N*N 사이즈의 스테이지 최종 모습
-        - 사각형은 "@", 빈칸은 ".", 염색되지 않은 장애물은 "#", 염색된 장애물은 염색 잉크 알파벳 (대문자)
+    I: 촬영 결과의 크기 1<=N,M<=30
+        - N개의 줄에 걸쳐 백신 놓기 전의 촬영 결과
+        - N개의 줄에 걸쳐 백신 놓은 후의 촬영 결과
+    O: 백신일 수 있다면 "YES", 그럴 수 없다면 "NO"
+
 
 [고려사항]
-    - y행 x열
-    - 이동
-        - 움직이는 물체 이름이 사각형, "@"로 표현.
-        - 밖으로 벗어나거나 이동하려는 칸에 장애물이 있으면 해당 커맨드 무시
-    - 잉크 충전
-        - j가 들어오면 잉크 양 += 1
-    - 점프
-        - J가 들어오면
-            1) "제자리에서" 점프
-            2) 사각형이 가진 잉크 양만큼 범위 내의 "장애물"에 잉크를 뿌린다
-                - 색상은 지금 써야 하는 잉크 색상
-                - 범위는 abs(sx-x)-abs(sy-y) <= 잉크 양을 만족하는 x,y
-                - 잉크 양은 0이 된다
-                - 잉크 색상은 점프 커맨드 입력 횟수에 의해 정해진다
-                    - (점프횟수)%잉크문자열 길이
-                        R -> G -> B -> R -> G -> B -> R -> ...
+    - 백신일 "수" 있다?
+        - 백신은 4방으로 인접한 칸의 데이터 값이 같다면 퍼져나갈 수 있다
+        => 백신 맞은 후의 촬영 결과를 바탕으로 bfs를 한다면
+            - 접종 후 촬영 결과를 기준으로, 접종 전 촬영 결과와 다른 값을 갖는 영역의 갯수가 1개 이하여야 한다.
+                a 즉, 다른 값을 갖는 영역의 수가 2 이상인 경우 "NO" 출력
+                b 혹은 접종 전,후 촬영 결과의 bfs 호출 횟수를 나란히 비교하는 것도 가능
+    -> 위의 a,b 접근 둘 다 틀리길래 우선 하나라도 다른 지점 찾으면 해당 좌표 before 값을 after 값으로 바꿔주고, 직후에 before과 after이 다르다면 백신일 수 없는 것으로 판단
 
+[테스트 케이스]
+---
+5 5
+4 4 4 4 4
+4 3 4 3 4
+4 4 5 4 4
+4 3 4 3 4
+4 4 4 4 4
+4 4 4 4 4
+4 3 4 5 4
+4 4 4 4 4
+4 3 4 3 4
+4 4 4 4 4
+---
+5 5
+4 4 4 4 4
+4 3 4 3 4
+4 4 5 4 4
+4 3 4 3 4
+4 4 4 4 4
+4 4 4 4 4
+4 4 4 4 4
+4 4 4 4 4
+4 4 4 4 4
+4 4 4 4 4
+---
+4 4
+2 2 2 1
+2 2 1 3
+2 1 3 3
+1 3 3 3
+2 2 2 1
+1 2 1 3
+2 1 3 3
+1 3 3 3
+---
+* 항체가 퍼졌던 칸들의 데이터 값은 모두 "어떤 동일한 값"으로 업데이트 된다.
+** 원래의 데이터 값과 업데이트 된 데이터 값이 동일할 수도 있다.
 """
 from collections import deque
+
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    v[x][y] = True    ## before 사진의 visited 배열
+    started_value = before[x][y]    ## 항체가 타고 나갈 수 있는 값 (이 값과 같을 때만 퍼져나갈 수 있다)
+    to_be = after[x][y]             ## 항체가 타고 나가 바꿔 놓을 값
+    while queue:
+        x,y = queue.popleft()
+        before[x][y] = to_be        ## 항체 퍼져 나감
+        for f in range(4):
+            nx = x + dx[f]
+            ny = y + dy[f]
+            if 0<=nx<N and 0<=ny<M and not v[nx][ny]:
+                if before[nx][ny] == started_value: ## 퍼져나갈 수 있는 값이면 무조건 퍼져나간다 (아마 여기서 틀린 것 같은데, 퍼질 수 있는 값인데 안 나가도 백신이 아니다)
+                    queue.append((nx,ny))
+                    v[nx][ny] = True
 
 def my_print(a):
     for x in a:
         print(x)
     print()
 
-I, N, K = map(int,input().split())
-my_ink = deque(input())
-stage = [list(input()) for _ in range(N)]
-commands = deque(input())
+N, M = map(int,input().split())
+before = [list(map(int,input().split())) for _ in range(N)]
+after = [list(map(int,input().split())) for _ in range(N)]
+v = [[False for _ in range(M)] for _ in range(N)]
+possible = True
 
-ix = 0
-iy = 0
-ink_amnt = 0
-found = False
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 for x in range(N):
-    for y in range(N):
-        if stage[x][y] == "@":
-            ix, iy = x, y
-            found = True
+    for y in range(M):
+        if before[x][y] != after[x][y]:
+            bfs(x,y)
+            for x in range(N):
+                for y in range(M):
+                    if after[x][y] != before[x][y]: ## 항체 맞았는데도 다른 값이 남아있으면
+                        possible = False
+                        break
+                if not possible:
+                    break
+        if not possible:
             break
-    if found:
+    if not possible:
         break
 
-while commands:
-    cur_cmd = commands.popleft()
-
-    if cur_cmd == 'j':
-        ink_amnt += 1
-
-    elif cur_cmd == "J":
-        for x in range(N):
-            for y in range(N):
-                if abs(ix - x) + abs(iy - y) <= ink_amnt:
-                    if stage[x][y] != "." and stage[x][y] != "@":
-                        stage[x][y] = my_ink[0]
-        my_ink.append(my_ink.popleft())
-        ink_amnt = 0
-
-    elif cur_cmd in ["U","D","L","R"]:
-        if cur_cmd == "U":
-            if 0<=ix-1<N and stage[ix-1][iy] == ".":
-                stage[ix][iy] = "."
-                ix -= 1
-                stage[ix][iy] = "@"
-        elif cur_cmd == "D":
-            if 0<=ix+1<N and stage[ix+1][iy] == ".":
-                stage[ix][iy] = "."
-                ix += 1
-                stage[ix][iy] = "@"
-        elif cur_cmd == "L":
-            if 0<=iy-1<N and stage[ix][iy-1] == ".":
-                stage[ix][iy] = "."
-                iy -= 1
-                stage[ix][iy] = "@"
-        elif cur_cmd == "R":
-            if 0<=iy+1<N and stage[ix][iy+1] == ".":
-                stage[ix][iy] = "." 
-                iy += 1
-                stage[ix][iy] = "@"
-        
-for x in stage:
-    for y in x:
-        print(y,end="")
-    print()
+if not possible:
+    print("NO")
+if possible:
+    print("YES")
--- tmp.py
+++ tmp.py
@@ -1,39 +1,91 @@
-T = int(input())
-for tc in range(1, T + 1):
-    # N : 화덕 크기, M : 피자 개수
-    N, M = map(int, input().split())
-    arr = list(map(int, input().split()))
+"""
+* 중난도 스터디
+[복잡도] O(N**2)
+    - 30*30의 촬영 결과에 대해 완전 탐색
 
-    temp = [0] * (M + 1)
-    for i in range(M):
-        temp[i + 1] = arr[i]
+[구상]
+    I: 촬영 결과의 크기 1<=N,M<=30
+        - N개의 줄에 걸쳐 백신 놓기 전의 촬영 결과
+        - N개의 줄에 걸쳐 백신 놓은 후의 촬영 결과
+    O: 백신일 수 있다면 "YES", 그럴 수 없다면 "NO"
 
-    pizza = []
-    for idx, c in enumerate(temp):  # idx : 피자번호, c : 치즈양
-        pizza.append([idx, c])  # [[0, 0], [1, 7], [2, 2], [3, 6], [4, 5], [5, 3]]
-    pizza = pizza[1:]  # [[1, 7], [2, 2], [3, 6], [4, 5], [5, 3]]
 
-    # 빈 화덕에 피자 넣기
-    bake = []
-    for _ in range(N):
-        p = pizza.pop(0)
-        bake.append(p)
+[고려사항]
+    - 백신일 "수" 있다?
+        - 백신은 4방으로 인접한 칸의 데이터 값이 같다면 퍼져나갈 수 있다
+        => 백신 맞은 후의 촬영 결과를 바탕으로 bfs를 한다면
+            - 접종 후 촬영 결과를 기준으로, 접종 전 촬영 결과와 다른 값을 갖는 영역의 갯수가 1개 이하여야 한다.
+                - 즉, 다른 값을 갖는 영역의 수가 2 이상인 경우 "NO" 출력
+                - 혹은 접종 전,후 촬영 결과의 bfs 호출 횟수를 나란히 비교하는 것도 가능
 
-    # print(bake)     # [[1, 7], [2, 2], [3, 6]] >> 화덕에 있는 피자
-    # print(pizza)    # [[4, 5], [5, 3]] >> 대기중인 피자
+[테스트 케이스]
+---
+5 5
+4 4 4 4 4
+4 3 4 3 4
+4 4 5 4 4
+4 3 4 3 4
+4 4 4 4 4
+4 4 4 4 4
+4 3 4 5 4
+4 4 4 4 4
+4 3 4 3 4
+4 4 4 4 4
+---
 
-    # 피자개수 1개 남을때 까지 반복
-    while len(bake) > 1:
-        p = bake.pop(0)  # 피자 꺼내서
-        c = p[1] // 2  # 피자 치즈 양 확인
+* 항체가 퍼졌던 칸들의 데이터 값은 모두 "어떤 동일한 값"으로 업데이트 된다.
+** 원래의 데이터 값과 업데이트 된 데이터 값이 동일할 수도 있다.
+"""
+from collections import deque
 
-        if c != 0:  # 0이 아니면
-            p[1] = c
-            bake.append(p)  # 다시 넣기
+def bfs(x,y):
+    global diff_cnt
+    is_different = False
+    queue = deque()
+    queue.append((x,y))
+    v_a[x][y] = True    ## after 사진의 visited 배열
+    if after[x][y] != before[x][y]:
+        is_different = True
+    while queue:
+        x,y = queue.popleft()
+        for f in range(4):
+            nx = x + dx[f]
+            ny = y + dy[f]
+            if 0<=nx<N and 0<=ny<M and not v_a[nx][ny] and after[x][y] == after[nx][ny]:
+                queue.append((nx,ny))
+                v_a[nx][ny] = True
+                if after[nx][ny] != before[nx][ny]:
+                    is_different = True             ## 이제부터 해당 bfs는 달라진 영역을 탐색하는 bfs가 된다.
+    
+    if is_different:                                ## 다른 영역을 탐색한 bfs 였다면, 다른 영역 수를 1 늘린다.
+        diff_cnt += 1
 
-        else:  # 0이고
-            if pizza:  # 굽히지 않은 피자가 있다면
-                new_p = pizza.pop(0)  # 새로운 피자 넣기
-                bake.append(new_p)
+def my_print(a):
+    for x in a:
+        print(x)
+    print()
 
-    print(f'#{tc} {bake[0][0]}')
+N, M = map(int,input().split())
+before = [list(map(int,input().split())) for _ in range(N)]
+after = [list(map(int,input().split())) for _ in range(N)]
+v_a = [[False for _ in range(M)] for _ in range(N)]
+global diff_cnt
+diff_cnt = 0
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+for x in range(N):
+    for y in range(M):
+        if not v_a[x][y]:
+            bfs(x,y)
+        if diff_cnt >= 2:
+            break
+    if diff_cnt >= 2:
+        break
+
+if diff_cnt >= 2:           ## 접종 전/후의 값이 다른 영역의 수가 2 이상이라면
+    print("NO")             
+else:                       ## 접종 전/후의 값이 다른 영역의 수가 1 이하라면
+    print("YES")
+
