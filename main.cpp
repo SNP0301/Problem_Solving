0b668353c5b80458791fad050ec5d6a2c6b13cc1
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-09-15 20:53:32 +0900

    tmp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <array>
-#include <string>
-using namespace std;
+/*
+    긍정
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    DP
+        - Identifying and solving subproblems
+        - Using subproblems together to solve larger problem
 
-    string s; 
-    cin >> s;
+        LIS
+            focus on the length of the LIS
+            find an appropriate subproblem
+                ex) all IS have a start & end
+                    LIS[4] = 1 + max {LIS[0], LIS[1], LIS[3]}
+            find relationships among subproblems
+            generalize the relationship
+            implement by solving subproblems in **appropriate** order
+                def lis(A):
+                    L = [1] * len(A)
+                    for i in range(1,len(L)):
+                        subproblems = 
+                        L[i] = 1 + max(subproblems,0)
 
-    cout << s;
-    return 0;
-}
-
+ */
