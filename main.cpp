b0be8c9328b303ca653eff44f953e41914292b15
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-09-24 22:12:33 +0900

    boj 1018

--- main.cpp
+++ main.cpp
@@ -1,106 +1,67 @@
+/*
+    긍정
+
+    사람 안 변한다
+*/
+
 #include <iostream>
 #include <vector>
-#include <queue>
+
 
 using namespace std;
 
-const int INF = 21*21 + 1;
+// int myPrint(vector<vector<char>> vc){
+//     int N = size(vc); // sizeof는 멤버변수 크기만 돌려줌 ㅇㅇㅇ 객체 자체 크기를 구하는게 아니니까 size 쓸 것
+//     int M = size(vc[0]);
+//     cout << "N: " << N;
+//     cout << "M: " << M;
 
-int N;
-vector<vector<int>> arr; // main에서 선언하면 bfs에서 못 먹는다
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
+//     for (int i = 0; i<N; i++){
+//         for (int j = 0; j<M; j++){
+//             cout << vc[i][j];
+//         }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
 
-// 먹을래 말래
-bool bfs(int sx, int sy, int sz, int &d, int &ox, int &oy) {
-    vector<vector<int>> dist(N, vector<int>(N, -1));
-    queue<pair<int,int>> q;
-    q.push({sx, sy});
-    dist[sx][sy] = 0;
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
 
-    int bestDist = INF;
-    int bx = INF;
-    int by = INF;
+    int N, M;
+    cin >> N >> M;
 
-    while (!q.empty()) {
-        auto [x, y] = q.front(); 
-        q.pop(); 
-        int d = dist[x][y];
+    vector<vector<char>> vc(N);
+    for (int i = 0; i<N; i++){
+        for (int j = 0;j<M;j++){
+            char c;
+            cin >> c;
+            vc[i].push_back(c);
+        }
+    }
 
-        if (d > bestDist) continue;
+    int answer = 8*8+1;
 
-        for (int f = 0; f < 4; ++f) { // f of four
-            int nx = x + dx[f];
-            int ny = y + dy[f];
-
-            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-            if (dist[nx][ny] != -1) continue;
-            if (arr[nx][ny] > sz) continue;
-
-            dist[nx][ny] = d + 1;
-
-            if (arr[nx][ny] != 0 && arr[nx][ny] < sz) {
-                int nd = d + 1;
-                if (nd < bestDist ||
-                   (nd == bestDist && (nx < bx || (nx == bx && ny < by)))) { // 하
-                    bestDist = nd;
-                    bx = nx;
-                    by = ny;
+    for (int i =0; i<=N-8;i++){
+        for (int j =0; j<=M-8;j++){
+            int w_answer = 0;
+            int b_answer = 0;   
+            for (int ni=0; ni<8; ni++){
+                for (int nj=0; nj<8; nj++){
+                    if((ni+nj)%2==0 && vc[i+ni][j+nj] == 'B') w_answer ++;
+                    else if((ni+nj)%2==1 && vc[i+ni][j+nj] == 'W') w_answer ++;
+                    else if((ni+nj)%2==1 && vc[i+ni][j+nj] == 'B') b_answer ++;
+                    else if((ni+nj)%2==0 && vc[i+ni][j+nj] == 'W') b_answer ++;
                 }
             }
-            q.push({nx, ny});
+            int cur_answer = min(w_answer,b_answer);
+            answer = min(answer,cur_answer);
         }
     }
 
-    if (bestDist == INF) return false; // 해당사항 없으면
 
-    d = bestDist;
-    ox = bx;
-    oy = by;
-    return true;
-}
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
-    cin >> N;
-
-    arr.assign(N, vector<int>(N));
-    int sx = -1, sy = -1;
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            cin >> arr[i][j];
-            if (arr[i][j] == 9){
-                sx = i;
-                sy = j;
-            }
-        }
-    }
-    if (sx != -1) arr[sx][sy] = 0;
-
-    int sz = 2;
-    int eaten = 0;
-    int answer = 0;
-    int x = sx;
-    int y = sy;
-
-    while (true) {
-        int d, tx, ty;
-        if (!bfs(x, y, sz, d, tx, ty)) break; // 못 먹으면 break
-
-        answer += d;
-        arr[tx][ty] = 0;
-        x = tx;
-        y = ty;
-
-        if (++eaten == sz){
-            eaten = 0;
-            ++sz;
-        }
-    }
-
-    cout << answer << '\n';
+    cout << answer;
     return 0;
 }
