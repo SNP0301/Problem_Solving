90d9ba2bdf041736930ed2407c3065b5f16b4481
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-10 21:00:49 +0900

    10807 new

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
+/*
+    긍정, 책임
+
+    ---
+    1
+    1
+    100
+    ---
+    1
+    1
+    -100
+    ---
+    11
+    1 4 1 2 4 2 4 2 3 4 4
+    -100
+    ---
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main (){
-    cout << "Hello Wolrd!";
+    int n,v,tmp;
+    cin >> n;
+    int arr[201+1]={0,};
 
+    for (int i = 0; i<n; i++){
+        cin >> tmp;
+        arr[tmp+100]++;
+    }
+
+    cin >> v;
+    cout << arr[v+100];
     return 0;
 }
