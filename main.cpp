dfe25dbcbb33f7a4c5acbfb2b43cbe47e2a2bea3
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-10 21:55:26 +0900

    28278

--- main.cpp
+++ main.cpp
@@ -1,36 +1,46 @@
 /*
     긍정, 책임
 
-    ---
-    1
-    1
-    100
-    ---
-    1
-    1
-    -100
-    ---
-    11
-    1 4 1 2 4 2 4 2 3 4 4
-    -100
-    ---
+    컴파일 에러 1회: #include <vector>를 포함하지 않았다.
 */
-
 #include <iostream>
 
 using namespace std;
 
-int main (){
-    int n,v,tmp;
-    cin >> n;
-    int arr[201+1]={0,};
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    for (int i = 0; i<n; i++){
-        cin >> tmp;
-        arr[tmp+100]++;
+    int N; 
+    if(!(cin >> N)) return 0;
+
+    vector<int> st;
+    st.reserve(N);
+    int top = 0;
+
+    for(int i=0;i<N;i++){
+        int cmd; cin >> cmd;
+
+        if(cmd == 1){
+            int X; cin >> X;
+            if(top < (int)st.size()) st[top] = X;
+            else st.push_back(X);
+            ++top;
+        }
+        else if(cmd == 2){
+            if(top == 0) cout << -1 << '\n';
+            else cout << st[--top] << '\n';
+        }
+        else if(cmd == 3){
+            cout << top << '\n';
+        }
+        else if(cmd == 4){
+            cout << (top == 0) << '\n';
+        }
+        else if(cmd == 5){
+            if(top == 0) cout << -1 << '\n';
+            else cout << st[top-1] << '\n';
+        }
     }
-
-    cin >> v;
-    cout << arr[v+100];
     return 0;
 }
