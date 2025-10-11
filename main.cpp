4e34f1ba2ae9f1dfae99714ca6d51c674f3048f7
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-11 22:42:22 +0900

    1008

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-static long first = 0;
-
-long fib(int n){
-    if (n == 1 || n == 2){
-        ++first;
-        return 1;
-    }
-    else return (fib(n-1)+fib(n-2));
-}
-
-int main(){
-    int n;
-    cin >> n;
-    fib(n);
-    cout << first << " " << n-2;
-
-
+int main() {
+    double A, B;
+    cin >> A >> B;
+    cout.precision(10);
+    cout << A/B << '\n';
     return 0;
 }
