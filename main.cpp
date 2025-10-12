e7e7e06c4e430f63139ef3f1f475982931522844
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-12 10:02:21 +0900

    1330

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
-using namespace std;
+int main(){
+    int A,B;
+    std::cin >> A >> B;
+    
+    if (A > B) std::cout << ">";
+    else if (A < B) std::cout << "<";
+    else std::cout << "==";
 
-int main() {
-    double A, B;
-    cin >> A >> B;
-    cout.precision(10);
-    cout << A/B << '\n';
+
+
     return 0;
 }
