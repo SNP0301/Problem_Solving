f2705970056e76482e412820f17d337bfb923e59
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-12 10:38:13 +0900

    25083

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 
 
 int main(){
-    char c;
-    std::cin >> c;
-    std::cout << (int)c;
 
+    std::cout << "         ,r\'\"7\n";
+    std::cout << "r`-_   ,\'  ,/\n";
+    std::cout << " \\. \". L_r\'\n";
+    std::cout << "   `~\\/\n";
+    std::cout << "      |\n";
+    std::cout << "      |\n";
     return 0;
 }
