0d8284a8621e76b6a69b6adbbdb812ff9ab35032
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-12 14:36:54 +0900

    1546

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main(){
-    std::string num;
-    while (true){
-        std::cin >> num;
-        bool isPalindrome = true;
-        if (num == "0") break;
-        for (int i=0; i<(int)num.size(); ++i){
-            if (num[i] != num[(int)num.size()-i-1]) isPalindrome = false;
-        }
-        std::string answer = (isPalindrome) ? "yes" : "no";
-        std::cout << answer << "\n";
+    float N,num;
+    float mx = -1;
+    float newSum = 0;
+    vector <int> vc;
+
+    cin >> N;
+
+    for (int i=0; i<N; ++i){
+        cin >> num;
+        vc.push_back(num);
+        if (num >= mx) mx = num;
     }
-    
+
+    for (int i=0; i<N; ++i){
+        newSum += (vc[i]/mx)*100;
+
+    }
+    cout.precision(10);
+    cout << float(newSum/N);
+
 
     return 0;
 }
