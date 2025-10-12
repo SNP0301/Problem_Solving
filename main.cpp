c4607967276f97cdd03494529c9834ca2edb264b
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-12 14:22:27 +0900

    1259

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
-#include <vector>
 
 int main(){
-
-    int N, M, curNum;
-    int mx = -1;
-    std::vector <int> vc;
-    std::cin >> N >> M;
-    for(int i=0; i<N; ++i){
-        std::cin >> curNum;
-        vc.push_back(curNum);
-    }
-
-    for(int i=0; i<N-2; ++i){
-        for(int j=i+1; j<N-1; ++j){
-            for(int k=j+1; k<N; ++k){
-                if (vc[i]+vc[j]+vc[k] > mx && vc[i]+vc[j]+vc[k] <= M) mx = vc[i]+vc[j]+vc[k];
-            }
+    std::string num;
+    while (true){
+        std::cin >> num;
+        bool isPalindrome = true;
+        if (num == "0") break;
+        for (int i=0; i<(int)num.size(); ++i){
+            if (num[i] != num[(int)num.size()-i-1]) isPalindrome = false;
         }
+        std::string answer = (isPalindrome) ? "yes" : "no";
+        std::cout << answer << "\n";
     }
+    
 
-    std::cout << mx;
     return 0;
 }
