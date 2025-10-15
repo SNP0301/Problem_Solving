b1503a9b0d4b3f5d1a540f63bcade231ee71e391
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-15 10:45:36 +0900

    2609

--- main.cpp
+++ main.cpp
@@ -1,47 +1,26 @@
 /*
     긍정, 책임
-
-    array로 도전
+    유클리드 호제법
 */
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(false); cin.tie(NULL);
-    int arr[10'000] = {-1,};
-    int N,num;
-    int startIdx = 0;
-    int endIdx = -1;
-    cin >> N;
+int gcd(int A, int B) {
+	int res = A % B;
+	while (res != 0) {
+		A = B;
+		B = res;
+		res = A % B;
+	}
+	return B;
+}
 
-    string cmd;
+int lcm(int A, int B) {
+	return ((A * B) / gcd(A, B));
+}
 
-    for(int i=0; i<N; ++i){
-        cin >> cmd;
-        if (cmd == "push"){
-            cin >> num;
-            arr[++endIdx] = num;
-        }
-        else if(cmd == "pop"){
-            if (endIdx < startIdx) cout << -1 << "\n";
-            else cout << arr[startIdx++] << "\n";
-        }
-        else if(cmd == "size") cout << endIdx-startIdx+1 << "\n";
-        else if(cmd == "empty"){
-            if (endIdx < startIdx) cout << 1 << "\n";
-            else cout << 0 << "\n";
-        }
-        else if(cmd == "front"){
-            if(endIdx < startIdx) cout << -1 << "\n";
-            else cout << arr[startIdx] << "\n";
-        }
-        else if(cmd == "back"){
-            if(endIdx < startIdx) cout << -1 << "\n";
-            else cout << arr[endIdx] << "\n";
-        }
-
-    }
-
-    return 0;
+int main() {
+	int A, B;
+	cin >> A >> B;
+	cout << gcd(A, B) << "\n" << lcm(A, B);
 }
