c16b0f04e1d730ca5a24486e2c9f650ae377b9da
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-15 09:51:31 +0900

    10845

--- main.cpp
+++ main.cpp
@@ -1,39 +1,44 @@
 /*
     긍정, 책임
+
+    array로 도전
 */
 #include <iostream>
-#include <vector>
 #include <string>
-
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL);
+    int arr[10'000] = {-1,};
     int N,num;
+    int startIdx = 0;
+    int endIdx = -1;
     cin >> N;
-    vector<int> vc;
+
+    string cmd;
+
     for(int i=0; i<N; ++i){
-        string cmd;
         cin >> cmd;
         if (cmd == "push"){
             cin >> num;
-            vc.push_back(num);
+            arr[++endIdx] = num;
         }
-        else if (cmd == "pop"){
-            if (vc.empty()) cout << -1 << "\n";
-            else{
-                cout << vc.back() << "\n";
-                vc.pop_back();
-            }
+        else if(cmd == "pop"){
+            if (endIdx < startIdx) cout << -1 << "\n";
+            else cout << arr[startIdx++] << "\n";
         }
-        else if (cmd == "size") cout << vc.size() << "\n";
-        else if (cmd == "empty"){
-            if (vc.empty()) cout << 1 << "\n";
+        else if(cmd == "size") cout << endIdx-startIdx+1 << "\n";
+        else if(cmd == "empty"){
+            if (endIdx < startIdx) cout << 1 << "\n";
             else cout << 0 << "\n";
         }
-        else if (cmd == "top"){
-            if (vc.empty()) cout << -1 << "\n";
-            else cout << vc.back() << "\n";
+        else if(cmd == "front"){
+            if(endIdx < startIdx) cout << -1 << "\n";
+            else cout << arr[startIdx] << "\n";
+        }
+        else if(cmd == "back"){
+            if(endIdx < startIdx) cout << -1 << "\n";
+            else cout << arr[endIdx] << "\n";
         }
 
     }
