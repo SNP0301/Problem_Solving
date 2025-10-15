1dd3c1eac9b62174efd4abc1ec5018a21b91fd6b
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-15 16:02:26 +0900

    10451

--- main.cpp
+++ main.cpp
@@ -1,63 +1,44 @@
-/*
-    긍정
-    나는 기본기에 충실한 글로벌 개발자가 된다
-*/
 #include <iostream>
 using namespace std;
 
-const int MAXN = 101;
-int answer = 0;
-int V,E;
-int front=-1, rear=-1;
-bool v[101] = {false};
-int adj[101][101] = {{0}};
-int q[MAXN] = {};
+const int MAXN = 1001;
+int P[MAXN];        // 순열
+bool visited[MAXN]; // 방문 여부
+int N;
 
-// for BFS
-void enqueue(int num){
-    q[++rear] = num;
+void dfs(int cur) {
+    visited[cur] = true;
+    int next = P[cur];
+    
+    if (!visited[next]) {
+        dfs(next);
+    }
 }
 
-int dequeue(){
-    int num = q[++front];
-    return num;
-}
+int main() {
+    int T;
+    cin >> T;
+    for (int j=0; j<T; ++j){
+        for(int i=0; i<MAXN; ++i){
+            P[i] = 0;
+            visited[i] = false;
+        }
+        cin >> N;
+        for (int i = 1; i <= N; ++i) {
+            cin >> P[i];
+        }
 
-bool isEmpty(){
-    return (rear <= front );
-}
+        int cycleCount = 0;
 
-
-void BFS(){
-    enqueue(1);
-    v[1] = true;
-
-    while(!isEmpty()){
-        int cur = dequeue();
-        for(int j=1; j<=V; ++j){
-            if(adj[cur][j]==1 && !v[j]){
-                enqueue(j);
-                v[j] = true;
-                ++answer;
+        for (int i = 1; i <= N; ++i) {
+            if (!visited[i]) {
+                dfs(i);
+                ++cycleCount;
             }
         }
+
+        cout << cycleCount << '\n';
     }
-    
-}
-
-int main(){
-
-    int s,e;
-    cin >> V >> E;
-    for(int i=0; i<E; ++i){
-        cin >> s >> e;
-        adj[s][e] = 1;
-        adj[e][s] = 1;
-    }
-
-    BFS();
-
-    cout << answer;
 
     return 0;
 }
