e82bcd3a7c40f2469185bbbd4b13bfcdcf8faa71
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-15 09:29:10 +0900

    10828

--- main.cpp
+++ main.cpp
@@ -2,30 +2,40 @@
     긍정, 책임
 */
 #include <iostream>
-#include <map>
+#include <vector>
+#include <string>
+
 using namespace std;
-
-static map<int,int> mp;
-
-bool doesExist(int n){
-    return mp.find(n) != mp.end();
-}
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL);
-
-    int N,M,num;
+    int N,num;
     cin >> N;
+    vector<int> vc;
     for(int i=0; i<N; ++i){
-        cin >> num;
-        if (doesExist(num)) mp[num] += 1;
-        else mp[num] = 1;
-    }
+        string cmd;
+        cin >> cmd;
+        if (cmd == "push"){
+            cin >> num;
+            vc.push_back(num);
+        }
+        else if (cmd == "pop"){
+            if (vc.empty()) cout << -1 << "\n";
+            else{
+                cout << vc.back() << "\n";
+                vc.pop_back();
+            }
+        }
+        else if (cmd == "size") cout << vc.size() << "\n";
+        else if (cmd == "empty"){
+            if (vc.empty()) cout << 1 << "\n";
+            else cout << 0 << "\n";
+        }
+        else if (cmd == "top"){
+            if (vc.empty()) cout << -1 << "\n";
+            else cout << vc.back() << "\n";
+        }
 
-    cin >> M;
-    for(int i=0; i<M; ++i){
-        cin >> num;
-        cout << mp[num] << " ";
     }
 
     return 0;
