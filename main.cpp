0c3e7fcb33f3cd22e21799f88a46843a6c9573f4
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-15 15:52:42 +0900

    2606

--- main.cpp
+++ main.cpp
@@ -5,17 +5,59 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int T,a,b,c,d;
-    cin >> T;
-    for(int i=0; i<T; ++i){
-        cin >> a >> b;
-        cout << a-1 << "\n";
-        for(int j=0; j<b; ++j){
-            cin >> c >> d;
+const int MAXN = 101;
+int answer = 0;
+int V,E;
+int front=-1, rear=-1;
+bool v[101] = {false};
+int adj[101][101] = {{0}};
+int q[MAXN] = {};
+
+// for BFS
+void enqueue(int num){
+    q[++rear] = num;
+}
+
+int dequeue(){
+    int num = q[++front];
+    return num;
+}
+
+bool isEmpty(){
+    return (rear <= front );
+}
+
+
+void BFS(){
+    enqueue(1);
+    v[1] = true;
+
+    while(!isEmpty()){
+        int cur = dequeue();
+        for(int j=1; j<=V; ++j){
+            if(adj[cur][j]==1 && !v[j]){
+                enqueue(j);
+                v[j] = true;
+                ++answer;
+            }
         }
     }
+    
+}
 
+int main(){
+
+    int s,e;
+    cin >> V >> E;
+    for(int i=0; i<E; ++i){
+        cin >> s >> e;
+        adj[s][e] = 1;
+        adj[e][s] = 1;
+    }
+
+    BFS();
+
+    cout << answer;
 
     return 0;
 }
