d9ee24b665682493402fde8c08db61c53d968cf4
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-17 08:51:30 +0900

    1003

--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
 /*
     긍정, 책임
-    추억이 새록새록
 */
 #include <iostream>
-#include <queue>
 using namespace std;
+
+int zeroArr[41] = {1};
+int oneArr[41] = {0};
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int X,cmd;
-    cin >> X;
-    priority_queue<int> pq;
+    int T,N;
+    cin >> T;
 
-    for(int x=0; x<X; ++x){
-        cin >> cmd;
-        cmd = cmd*-1;
-        if (cmd == 0){
-            if (pq.empty()) cout << 0 << "\n";
-            else{
-                int res = pq.top(); pq.pop();
-                cout << res*-1 << "\n";
-            }
-        }
-        else pq.push(cmd);
+    zeroArr[1] = 0;
+    oneArr[1] = 1;
+
+    for(int n=2; n<=40; ++n){
+        zeroArr[n] = zeroArr[n-1] + zeroArr[n-2];
+        oneArr[n] = oneArr[n-1] + oneArr[n-2];
     }
 
+    for (int t=0; t<T; ++t){
+        cin >> N;
+
+        cout << zeroArr[N] << " " << oneArr[N] << "\n";
+    }
+
+    
     return 0;
 }
