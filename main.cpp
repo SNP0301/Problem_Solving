aab5cee7280e43eca0bd0067f40a53e8d261dbf3
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-16 19:00:26 +0900

    4963

--- main.cpp
+++ main.cpp
@@ -1,92 +1,90 @@
 /*
     긍정, 책임
+    8방인접 bfs
 
-    MST를 만들어라. 근데 2개.
-    MST를 2개 만들 수 있는 경우의 수들 중에서 가장 비용이 가장 케이스? 말도 안됨
+    -1-1  -10  -11
+     0-1   00  >01<
+     1-1   10   11
 */
 #include <iostream>
-#include <vector>
 #include <queue>
 using namespace std;
 
-struct Edge {
-    int u, v, weight;
-    Edge(int u, int v, int weight) : u(u), v(v), weight(weight) {}
+const int ex[8] = {0,1,1,1,0,-1,-1,-1};
+const int ey[8] = {1,1,0,-1,-1,-1,0,1};
+const int MAXSIZE = 50+1;
+static int w,h;
+static int arr[MAXSIZE][MAXSIZE] = {{0}};
+static bool v[MAXSIZE][MAXSIZE] = {{false}};
 
-    bool operator<(const Edge& other) const { // 해당 struct에 한정되는 오버라이딩이므로 걱정 ㄴㄴ
-        return weight > other.weight;
+void clean(){
+    for(int x=0; x<MAXSIZE; ++x){
+        for(int y=0; y<MAXSIZE; ++y){
+            arr[x][y] = 0;
+            v[x][y] = false;
+        }
     }
-};
+}
 
-class UnionFind {
-private:
-    vector<int> parent, rank;
+bool outofBound(int x, int y){
+    return (x<0 || x >= h || y<0 || y >= w);
+}
 
-public:
-    UnionFind(int n) {
-        parent.resize(n+1);
-        rank.resize(n+1, 0);
-        for (int i = 1; i <= n; ++i) parent[i] = i; // 일단 나 자신으로 초기화
-    }
+void BFS(int x, int y){
+    queue<pair<int,int>> q;
+    q.push({x,y});
+    v[x][y] = true;
 
-    int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    bool unite(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX != rootY) {
-            if (rank[rootX] > rank[rootY]) parent[rootY] = rootX;
-            else if (rank[rootX] < rank[rootY]) parent[rootX] = rootY;
-            else {
-                parent[rootY] = rootX;
-                rank[rootX]++;
+    while(!q.empty()){
+        pair<int,int> cur = q.front(); q.pop();
+        int ix = cur.first;
+        int iy = cur.second;
+        for(int e=0; e<8; ++e){ // e for eight
+            int nx = ix + ex[e];
+            int ny = iy + ey[e];
+            if(!outofBound(nx,ny) && !v[nx][ny] && arr[nx][ny]==1){ // 범위 내, 미방문, 조건 만족하면
+                q.push({nx,ny});
+                v[nx][ny] = true;
             }
-            return true;
         }
-        else return false;  // 이미 같은 집합에 있는 경우
-    }
-};
 
-int main() {
-    int n, m;
-    int mx = -1;
-    cin >> n >> m;
-    
-    if (n == 2){
-        cout << 0;
-        return 0;
-    }
-    
-    
-    priority_queue<Edge> pq;
-
-    for (int i = 0; i < m; ++i) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        pq.push(Edge(u, v, w));
     }
 
-    UnionFind uf(n);
+}
 
-    int mstWeight = 0;
-    int edgesUsed = 0;
+int main(){
+    int num;
+    while (true){
+        clean();
+        cin >> w >> h;
+        if(w==0 && h==0) break; // 입력의 마지막에는 0이 2개 주어진다
 
-    while (!pq.empty() && edgesUsed < n - 1) {
-        Edge e = pq.top(); pq.pop();
+        // 정답, 지도, 방문 배열 초기화
+        int answer = 0;
 
-        if (uf.unite(e.u, e.v)) {
-            mstWeight += e.weight;
-            edgesUsed++;
-            mx = max(mx,e.weight);
+
+        for(int x=0; x<h; ++x){
+            for(int y=0; y<w; ++y){
+                cin >> num;
+                arr[x][y] = num;
+            }
         }
-    }
 
-    // 최소 신장 트리의 가중치 출력
-    cout << mstWeight-mx;  // 최소 신장 트리의 총 가중치
+        for(int x=0; x<h; ++x){
+            for(int y=0; y<w; ++y){
+                if(arr[x][y]==1 && !v[x][y]){
+                    BFS(x,y);
+                    answer += 1;
+                }
+            }
+        }
+
+        cout << answer << "\n";
+
+
+
+
+    }
 
     return 0;
 }
