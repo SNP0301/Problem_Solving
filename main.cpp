a913f1acd632ee02aad23aee46c64b343bd0d7bc
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-16 19:45:59 +0900

    1927

--- main.cpp
+++ main.cpp
@@ -1,55 +1,28 @@
 /*
     긍정, 책임
-
-    X가 3으로 나누어 떨어지면, 3으로 나눈다.
-    X가 2로 나누어 떨어지면, 2로 나눈다.
-    1을 뺀다.
-
+    추억이 새록새록
 */
 #include <iostream>
 #include <queue>
 using namespace std;
 
-const static int MAXN = 1'000'000+1;
-int arr[MAXN] = {MAXN};
-bool v[MAXN] = {false};
-
 int main(){
-    int N;
-    cin >> N;
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int X,cmd;
+    cin >> X;
+    priority_queue<int> pq;
 
-    queue<int> q;
-    q.push(N);
-
-    arr[N] = 0;
-    v[N] = true;
-
-    while (!q.empty()){
-        int cur = q.front(); q.pop();
-
-        int curScore = arr[cur];
-        
-        if (cur == 1){ // 종료 조건
-            cout << arr[cur];
-            return 0;
+    for(int x=0; x<X; ++x){
+        cin >> cmd;
+        cmd = cmd*-1;
+        if (cmd == 0){
+            if (pq.empty()) cout << 0 << "\n";
+            else{
+                int res = pq.top(); pq.pop();
+                cout << res*-1 << "\n";
+            }
         }
-
-        if(cur%3 == 0 && !v[cur/3]){
-            q.push(cur/3);
-            v[cur/3] = true;
-            arr[cur/3] = curScore + 1;
-        }
-        if (cur%2 == 0 && !v[cur/2]){
-            q.push(cur/2);
-            v[cur/2] = true;
-            arr[cur/2] = curScore + 1;
-        }
-        if (cur-1 >= 0 && !v[cur-1]){
-            q.push(cur-1);
-            v[cur-1] = true;
-            arr[cur-1] = curScore +1;
-        }
-        
+        else pq.push(cmd);
     }
 
     return 0;
