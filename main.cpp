104e2f83785c407aad209c3804841c35d09c7221
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-17 11:06:19 +0900

    18352

--- main.cpp
+++ main.cpp
@@ -2,42 +2,60 @@
     긍정, 책임
 */
 #include <iostream>
-#include <unordered_map>
-#include <unordered_set>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
-// const static int MAX = 1'000'000'000;
+const int MAXN = 300'001;
+bool v[MAXN+1];
+int dist[MAXN+1];
+static int X,K;
+vector<int> answerVc;
+vector<vector<int>> adj;
 
-int main(){
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    long A,B;
-    cin >> A >> B;
-
-    unordered_map<long,long> mp;
-    unordered_set<long> v;
-    queue<long> q;
-
-    mp[A] = 0;
-    q.push(A);
-    v.insert(A);
+void BFS(){
+    queue<int> q;
+    q.push(X);
+    v[X] = true;
 
     while(!q.empty()){
-        long cur = q.front(); q.pop();
-        if (cur == B) break;
-        long nxtArr[2] = {cur*2, cur*10+1};
-
-        for(long nxt: nxtArr){
-            if(v.find(nxt) == v.end() && nxt <= B){ // 못 찾았으면 = 간 적 없으면
+        int cur = q.front(); q.pop();
+        for(int nxt: adj[cur]){
+            if(!v[nxt]){
                 q.push(nxt);
-                v.insert(nxt);
-                mp[nxt] = mp[cur]+1;
+                dist[nxt] = dist[cur] + 1;
+                if (dist[nxt] == K) answerVc.push_back(nxt);
+                v[nxt] = true;
             }
         }
     }
-    
-    if(v.find(B) == v.end()) cout << -1;
-    else cout << mp[B]+1;
+}
+
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N,M,start,end;
+
+    cin >> N >> M >> K >> X;
+    adj.resize(N+1);
+
+    for(int m=0; m<M; ++m){
+        cin >> start >> end;
+        adj[start].push_back(end);
+    }
+    fill(dist,dist+MAXN+1,0);
+    fill(v,v+MAXN+1,false);
+    dist[X] = 0;
+
+    BFS();
+
+    if ((int)answerVc.size() == 0) cout << -1;
+    else{
+        sort(answerVc.begin(), answerVc.end());
+        for(int city: answerVc){
+            cout << city << "\n";
+        }
+    }
 
     return 0;
 }
