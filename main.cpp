4b087ec56527a4a21684feca854f1e74642508c5
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-17 09:50:32 +0900

    2644

--- main.cpp
+++ main.cpp
@@ -1,34 +1,50 @@
 /*
     긍정, 책임
-
-    [1] = 1;
-    [2] = 2;
-    [3] = 4;
-    [4] = 7;
-    [5] = 13;
-    [6] = 24;
+    단방향, cost=1
 */
 #include <iostream>
+#include <queue>
 using namespace std;
 
+bool adj[101][101] = {{false}};
+bool v[101] = {false};
+int cost[101] = {0};
+static int N;
+
+void BFS(int node){
+    queue<int> q;
+    q.push(node);
+    v[node] = true;
+
+    while(!q.empty()){
+        int cur = q.front(); q.pop();
+        for(int nxt=1; nxt<=N; ++nxt){
+            if(adj[cur][nxt] && !v[nxt]){
+                q.push(nxt);
+                v[nxt] = true;
+                cost[nxt] = cost[cur] + 1;
+            }
+        }
+    }
+}
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int T, N;
-    cin >> T;
-    int answerArr[11+1];
-    answerArr[1] = 1;
-    answerArr[2] = 2;
-    answerArr[3] = 4;
+    int M,m,parent,child,start,end;
+    cin >> N >> start >> end >> M;
 
-    for(int i=4; i<=11; ++i){
-        answerArr[i] = answerArr[i-1] + answerArr[i-2] + answerArr[i-3];
+    for(m=0; m<M; ++m){
+        cin >> parent >> child;
+        adj[child][parent] = true;
+        adj[parent][child] = true;
     }
 
-    for(int t=0; t<T; ++t){
-        cin >> N;
-        cout << answerArr[N] << "\n";
-    }
+    cost[start] = 0;
+
+    BFS(start);
+
+    if(cost[end]==0) cout << -1;
+    else cout << cost[end];
 
     return 0;
 }
