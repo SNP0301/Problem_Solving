06077c18d769c06b056d890d4dd1d7d46ab8b57d
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-17 09:07:34 +0900

    11659

--- main.cpp
+++ main.cpp
@@ -1,31 +1,38 @@
 /*
     긍정, 책임
+
+    5 4 3 2 1
+    5 9 12 14 15
 */
 #include <iostream>
 using namespace std;
 
-int zeroArr[41] = {1};
-int oneArr[41] = {0};
+const int MAXN = 100'001;
+int arr[MAXN+1] = {0};
+int acc[MAXN+1] = {0};
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int T,N;
-    cin >> T;
-
-    zeroArr[1] = 0;
-    oneArr[1] = 1;
-
-    for(int n=2; n<=40; ++n){
-        zeroArr[n] = zeroArr[n-1] + zeroArr[n-2];
-        oneArr[n] = oneArr[n-1] + oneArr[n-2];
-    }
-
-    for (int t=0; t<T; ++t){
-        cin >> N;
-
-        cout << zeroArr[N] << " " << oneArr[N] << "\n";
-    }
-
+    int N,M,num,start,end;
+    cin >> N >> M;
     
+    for(int i=1; i<=N; ++i){
+        cin >> num;
+        arr[i] = num;
+    }
+
+    acc[1] = arr[1];
+
+    for(int i=2; i<=N; ++i){
+        acc[i] = acc[i-1] + arr[i];
+    }
+
+    for(int m=0; m<M; ++m){
+        cin >> start >> end;
+        cout << acc[end]-acc[start-1] << "\n";
+    }
+
+
+
     return 0;
 }
