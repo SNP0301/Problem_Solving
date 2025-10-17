19a5d547c704e486cb36421305afd9ba194113d5
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-17 23:29:10 +0900

    4386

--- main.cpp
+++ main.cpp
@@ -1,61 +1,109 @@
 /*
     긍정, 책임
+
+    {start, end, 거리} 저장해두고 MST 완성하기
 */
 #include <iostream>
 #include <queue>
-#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+
 using namespace std;
 
-const int MAXN = 300'001;
-bool v[MAXN+1];
-int dist[MAXN+1];
-static int X,K;
-vector<int> answerVc;
-vector<vector<int>> adj;
 
-void BFS(){
-    queue<int> q;
-    q.push(X);
-    v[X] = true;
+const static int MAXN = 100;
 
-    while(!q.empty()){
-        int cur = q.front(); q.pop();
-        for(int nxt: adj[cur]){
-            if(!v[nxt]){
-                q.push(nxt);
-                dist[nxt] = dist[cur] + 1;
-                if (dist[nxt] == K) answerVc.push_back(nxt);
-                v[nxt] = true;
-            }
+struct DSU {
+    vector <int> parent, rankv; // 매크로 충돌 에반데;; 이름 바꿔야하나
+    DSU(int n=0) {if (n) init(n);}
+
+    void init(int n){
+        parent.resize(n);
+        rankv.assign(n,0);
+        for(int i=0; i<n; ++i){
+            parent[i] = i;
         }
     }
+
+    int find(int x){
+        if(parent[x] == x) return x;
+        else{
+            return parent[x] = find(parent[x]);
+        }
+    }
+
+    bool unite(int a, int b){
+        int ra = find(a);
+        int rb = find(b);
+
+        if(ra == rb) return false;
+        if(rankv[ra] < rankv[rb]) swap(ra, rb);
+        parent[rb] = ra;
+        if(rankv[ra] == rankv[rb]) ++rankv[ra];
+        return true;
+    }
+};
+
+struct star {
+    float x;
+    float y;
+    star() : x(0), y(0) {}
+    star(float x_, float y_) : x(x_), y(y_) {}
+};
+
+star starArr[MAXN];
+
+using Edge = tuple<int,int,float>;
+priority_queue<Edge,vector<Edge>,greater<Edge>> pq;
+
+float getDistance(star A, star B){
+    return (float)(A.x-B.x)*(A.x-B.x)+(A.y-B.y)*(A.y-B.y);
 }
 
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int N,M,start,end;
+    float N,x,y;
+    DSU dsu;
 
-    cin >> N >> M >> K >> X;
-    adj.resize(N+1);
-
-    for(int m=0; m<M; ++m){
-        cin >> start >> end;
-        adj[start].push_back(end);
-    }
-    fill(dist,dist+MAXN+1,0);
-    fill(v,v+MAXN+1,false);
-    dist[X] = 0;
-
-    BFS();
-
-    if ((int)answerVc.size() == 0) cout << -1;
-    else{
-        sort(answerVc.begin(), answerVc.end());
-        for(int city: answerVc){
-            cout << city << "\n";
+    cin >> N;
+    for(int i=0; i<N; ++i){
+        cin >> x >> y;
+        starArr[i] = star(x,y);
+        for(int j=0; j<i; ++j){
+            float curDistance = getDistance(starArr[i],starArr[j]);
+            pq.push({curDistance,i,j});
         }
     }
+
+    dsu.init(N);
+    int picked = 0;
+    double answer = 0.0;
+
+    while(!pq.empty() && picked < N-1){
+        Edge cur = pq.top(); pq.pop();
+        float w = get<0>(cur);
+        int u = get<1>(cur);
+        int v = get<2>(cur);
+        if(dsu.unite(u,v)){
+            answer += sqrt(w);
+            ++picked;
+        }
+    }
+    
+    // for(const star& s: stars){
+    //     cout << fixed << setprecision(2) << s.x << "\n";
+    // }
+
+    /*
+    while(!pq.empty()){
+        tuple<int,int,float> cur = pq.top(); pq.pop();
+        cout << fixed << setprecision(2) << get<0>(cur) << ", " << get<1>(cur) << ", " << get<2>(cur) << "\n";
+    }
+    */
+   cout << fixed << setprecision(2) << answer;
+
 
     return 0;
 }
