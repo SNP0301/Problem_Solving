e4fb59b0d354ad1bf4c1310a03e86c44a5e6bec8
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-17 09:19:33 +0900

    9095

--- main.cpp
+++ main.cpp
@@ -1,38 +1,34 @@
 /*
     긍정, 책임
 
-    5 4 3 2 1
-    5 9 12 14 15
+    [1] = 1;
+    [2] = 2;
+    [3] = 4;
+    [4] = 7;
+    [5] = 13;
+    [6] = 24;
 */
 #include <iostream>
 using namespace std;
 
-const int MAXN = 100'001;
-int arr[MAXN+1] = {0};
-int acc[MAXN+1] = {0};
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int N,M,num,start,end;
-    cin >> N >> M;
-    
-    for(int i=1; i<=N; ++i){
-        cin >> num;
-        arr[i] = num;
+    int T, N;
+    cin >> T;
+    int answerArr[11+1];
+    answerArr[1] = 1;
+    answerArr[2] = 2;
+    answerArr[3] = 4;
+
+    for(int i=4; i<=11; ++i){
+        answerArr[i] = answerArr[i-1] + answerArr[i-2] + answerArr[i-3];
     }
 
-    acc[1] = arr[1];
-
-    for(int i=2; i<=N; ++i){
-        acc[i] = acc[i-1] + arr[i];
+    for(int t=0; t<T; ++t){
+        cin >> N;
+        cout << answerArr[N] << "\n";
     }
-
-    for(int m=0; m<M; ++m){
-        cin >> start >> end;
-        cout << acc[end]-acc[start-1] << "\n";
-    }
-
-
 
     return 0;
 }
