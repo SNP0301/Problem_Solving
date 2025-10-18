4d47c1a797b9b71660ab930cb49b3a88c04a9eb9
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-18 10:39:07 +0900

    7576

--- main.cpp
+++ main.cpp
@@ -1,85 +1,79 @@
 /*
     긍정, 책임
-
-    4 6
-    101111
-    101010
-    101011
-    111011
 */
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <string>
-#include <algorithm>
 
 using namespace std;
-using node = pair<int,int>;
+using box = pair<int,int>;
 
-static int N,cnt;
+static int N,M,timeCnt;
+int fx[4] = {0,0,-1,1};
+int fy[4] = {-1,1,0,0};
+
 vector<vector<int>> vc;
 vector<vector<bool>> v;
+queue<box> q;
 
-int fx[4] = {0,0,-1,1};
-int fy[4] = {1,-1,0,0};
+bool oufofBound(int x, int y) {return (x<0 || x>=N || y<0 || y>=M);}
 
-bool outofBound(int x, int y){ return (x<0 || x>=N || y<0 || y>=N);}
-
-int BFS(int ix, int iy){
-    queue<node> q;
-    q.push({ix,iy});
-    v[ix][iy] = true;
-    int building_cnt = 1;
-
-    while(!q.empty()){
-        node cur = q.front(); q.pop();
+void BFS(){
+    while (!q.empty()){
+        box cur = q.front(); q.pop();
         int x = cur.first;
         int y = cur.second;
         for(int f=0; f<4; ++f){
-            int nx = x+fx[f];
-            int ny = y+fy[f];
-            if(!outofBound(nx,ny) && vc[nx][ny]==1 && !v[nx][ny]){
+            int nx = x + fx[f];
+            int ny = y + fy[f];
+            if(!oufofBound(nx,ny) && !v[nx][ny] && vc[nx][ny]!=-1){
                 q.push({nx,ny});
                 v[nx][ny] = true;
-                ++building_cnt;
+                vc[nx][ny] = vc[x][y]+1;
+                timeCnt = max(timeCnt,vc[nx][ny]);
             }
         }
     }
-
-    return building_cnt;
 }
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    string numString;
-    vector<int> buildingVc;
-    int res;
-    cin >> N;
-    vc.resize(N);
+
+    int num;
+    bool allDone = true;
+    timeCnt = 0;
+    cin >> M >> N;
     v.resize(N);
+    vc.resize(N);
 
     for(int i=0; i<N; ++i){
-        cin >> numString;
-        for(int j=0; j<N; ++j){
-            vc[i].push_back(numString[j]-48);
+        for(int j=0; j<M; ++j){
+            cin >> num;
+            vc[i].push_back(num);
             v[i].push_back(false);
-        }
-    }
-
-    for(int i=0; i<N; ++i){
-        for(int j=0; j<N; ++j){
-            if(vc[i][j]==1 && !v[i][j]){
-                res = BFS(i,j);
-                buildingVc.push_back(res);
-                ++cnt;
+            if(num==1){
+                q.push({i,j});
+                v[i][j] = true;
             }
         }
     }
-    sort(buildingVc.begin(), buildingVc.end());
-    cout << cnt << "\n";
 
-    for(const int &building: buildingVc){
-        cout << building << "\n";
+    BFS();
+
+    for(int i=0; i<N; ++i){
+        for(int j=0; j<M; ++j){
+            if (vc[i][j]==0){
+                allDone = false;
+                break;
+            }
+        }
+        if(!allDone) break;
+    }
+
+    if(!allDone) cout << -1;
+    else{
+        if(timeCnt == 0) cout << 0;
+        else cout << timeCnt-1;
     }
 
     return 0;
