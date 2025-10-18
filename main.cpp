0262e6a5d1433cc22692ee55ca408422e72f6b03
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-18 10:12:25 +0900

    2667

--- main.cpp
+++ main.cpp
@@ -11,66 +11,76 @@
 #include <queue>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 using node = pair<int,int>;
 
-static int N,M,answer;
+static int N,cnt;
 vector<vector<int>> vc;
-vector<vector<int>> cost;
 vector<vector<bool>> v;
 
 int fx[4] = {0,0,-1,1};
 int fy[4] = {1,-1,0,0};
 
-bool outofBound(int x, int y){ return (x<0 || x>=N || y<0 || y>=M);}
+bool outofBound(int x, int y){ return (x<0 || x>=N || y<0 || y>=N);}
 
-void BFS(){
+int BFS(int ix, int iy){
     queue<node> q;
-    q.push({0,0});
-    v[0][0] = true;
-    cost[0][0] = 1;
+    q.push({ix,iy});
+    v[ix][iy] = true;
+    int building_cnt = 1;
 
     while(!q.empty()){
         node cur = q.front(); q.pop();
         int x = cur.first;
         int y = cur.second;
-        if(x==N-1 && y==M-1) return;
         for(int f=0; f<4; ++f){
             int nx = x+fx[f];
             int ny = y+fy[f];
             if(!outofBound(nx,ny) && vc[nx][ny]==1 && !v[nx][ny]){
                 q.push({nx,ny});
                 v[nx][ny] = true;
-                cost[nx][ny] = cost[x][y] + 1;
+                ++building_cnt;
             }
         }
     }
 
-
-
+    return building_cnt;
 }
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     string numString;
-    answer = 0;
-    cin >> N >> M;
+    vector<int> buildingVc;
+    int res;
+    cin >> N;
     vc.resize(N);
-    cost.resize(N);
     v.resize(N);
 
     for(int i=0; i<N; ++i){
         cin >> numString;
-        for(int j=0; j<M; ++j){
+        for(int j=0; j<N; ++j){
             vc[i].push_back(numString[j]-48);
-            cost[i].push_back(0);
             v[i].push_back(false);
         }
     }
 
-    BFS();
+    for(int i=0; i<N; ++i){
+        for(int j=0; j<N; ++j){
+            if(vc[i][j]==1 && !v[i][j]){
+                res = BFS(i,j);
+                buildingVc.push_back(res);
+                ++cnt;
+            }
+        }
+    }
+    sort(buildingVc.begin(), buildingVc.end());
+    cout << cnt << "\n";
 
-    cout << cost[N-1][M-1];
+    for(const int &building: buildingVc){
+        cout << building << "\n";
+    }
+
     return 0;
 }
