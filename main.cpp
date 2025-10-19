9c29ebcebda7df3065e803e02cacb9300d3745d0
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-19 21:35:52 +0900

    24444

--- main.cpp
+++ main.cpp
@@ -2,44 +2,56 @@
     긍정, 책임
 */
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <queue>
 using namespace std;
 
-static int N;
-static int cnt = 1;
-const static int MAXN = 100'001;
 vector<vector<int>> adj;
-bool v[MAXN]= {false};
-int answer[MAXN] = {0};
+vector<bool> v;
+int answer[100'001] = {0,};
 
-void DFS(int cur){
-    for(int i=(int)adj[cur].size()-1; i>=0; --i){
-        if(!v[adj[cur][i]]){
-            v[adj[cur][i]] = true;
-            answer[adj[cur][i]] = ++cnt;
-            DFS(adj[cur][i]);
-        }
+static int cnt = 1;
+static int N;
+
+void BFS(int n){
+    queue<int> q;
+    q.push(n);
+
+    while(!q.empty()){
+        int cur = q.front(); q.pop();
+            for(const int& nxt: adj[cur]){
+                if(!v[nxt]){
+                    q.push(nxt);
+                    v[nxt] = true;
+                    answer[nxt] = ++cnt;
+                }
+            }
     }
+
 }
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
     int M,R,s,e;
+
     cin >> N >> M >> R;
+
     adj.resize(N+1);
+    v.resize(N+1);
+
     for(int i=0; i<M; ++i){
         cin >> s >> e;
         adj[s].push_back(e);
         adj[e].push_back(s);
     }
 
-    for (int i=1;i<=N;i++) sort(adj[i].begin(), adj[i].end());
-
-    answer[R] = 1;
+    answer[R] = cnt;
     v[R] = true;
-    DFS(R);
+
+    for(int i=1; i<=N; ++i) sort(adj[i].begin(), adj[i].end());
+
+    BFS(R);
 
     for(int i=1; i<=N; ++i){
         cout << answer[i] << "\n";
