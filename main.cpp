de2ced8c89e0352397873bb0ccd06a7427db5316
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-20 20:45:20 +0900

    18870

--- main.cpp
+++ main.cpp
@@ -1,80 +1,41 @@
 /*
     긍정, 책임
+
+    Map에 넣어두고, Key값을 NlgN으로 정렬 N 최대값은 1'000'000
+        -> 1'000'000 * lg 1'000'000 = 6'000'000
 */
 #include <iostream>
+#include <unordered_map>
+#include <algorithm>
 #include <vector>
-#include <queue>
+#include <set>
 using namespace std;
-using sheep = pair<int,int>;
-
-static int H,W;
-vector<vector<char>> vc;
-vector<vector<bool>> v;
-
-int fx[4] = {0,0,-1,1};
-int fy[4] = {-1,1,0,0};
-
-bool outofBound(int ox, int oy){ return (ox<0 || ox>=H || oy<0 || oy>=W); }
-
-void BFS(int bx, int by){
-    queue<sheep> q;
-    q.push({bx,by});
-    v[bx][by] = true;
-
-    while(!q.empty()){
-        sheep sh = q.front(); q.pop();
-        int x = sh.first;
-        int y = sh.second;
-        for(int f=0; f<4; ++f){
-            int nx = x + fx[f];
-            int ny = y + fy[f];
-            if(!outofBound(nx,ny) && !v[nx][ny] && vc[nx][ny]=='#'){
-                q.push({nx,ny});
-                v[nx][ny] = true;
-            }
-        }
-    }
-
-}
 
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N,num;
+    set<int> st;
+    vector<int> vc;
+    unordered_map<int, int> map;
 
-    int T,answer;
-    char c;
-    cin >> T;
-    for(int t=0; t<T; ++t){
-        cin >> H >> W;
-
-        vc.resize(0);
-        v.resize(0);
-
-        vc.resize(H);
-        v.resize(H);
-        answer = 0;
-
-        for(int h=0; h<H; ++h){
-            for(int w=0; w<W; ++w){
-                cin >> c;
-                vc[h].push_back(c);
-                v[h].push_back(false);
-            }
-        }
-
-        for(int x=0; x<H; ++x){
-            for(int y=0; y<W; ++y){
-                if(vc[x][y]=='#' && !v[x][y]){
-                    // cout << x << "," << y << "\n";
-                    BFS(x,y);
-                    ++answer;
-                }
-            }
-        }
-
-        cout << answer << "\n";
-        
+    cin >> N;
+    int idx = 0;
+    for(int i=0; i<N; ++i){
+        cin >> num;
+        st.insert(num);
+        vc.push_back(num);
     }
+
+    for(const int &e : st){
+        map[e] = idx++;
+    }
+
+    for(int i=0; i<N; ++i){
+        cout << map[vc[i]] << " ";
+    }
+
+
 
     return 0;
 }
