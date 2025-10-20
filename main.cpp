c67e88f20cccf519bdad1a6a51b3389b62d7126b
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-20 21:47:51 +0900

    14940

--- main.cpp
+++ main.cpp
@@ -2,43 +2,71 @@
     긍정, 책임
 */
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 using namespace std;
+using node = pair<int,int>;
 
-const static int MAXN = 100'000;
-int arr[MAXN+1];
-int cost[MAXN+1];
-bool v[MAXN+1];
+static int N,M;
+const static int MAXN = 1'000;
 
-vector<int> getNext(int n){ return {n-1,n+1,n*2}; }
-bool outofBound(int n) { return (n<0 || n > 100'000); }
+int arr[MAXN][MAXN];
+int answer[MAXN][MAXN];
+bool v[MAXN][MAXN];
+int fx[4] = {0,0,-1,1};
+int fy[4] = {1,-1,0,0};
+
+bool outofBound(int ox, int oy){ return (ox<0 || ox>=N || oy<0 || oy>=M);}
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int N, K;
-    queue<int> q;
-    cin >> N >> K;
     
-    for(int i=0; i<MAXN+1; ++i){ arr[i] = MAXN*2; }
+    int cnt,sx,sy;
+    cin >> N >> M;
 
-    q.push(N);
-    v[N] = true;
-
-    while(!q.empty()){
-        int cur = q.front(); q.pop();
-        if (cur == K){
-            cout << cost[cur];
-            break;
-        }
-        for(const int &nxt: getNext(cur)){
-            if(!outofBound(nxt) && !v[nxt]){
-                q.push(nxt);
-                v[nxt] = true;
-                cost[nxt] = cost[cur] + 1;
+    //  입력 받기
+    for(int x=0; x<N; ++x){
+        for(int y=0; y<M; ++y){
+            cin >> cnt;
+            arr[x][y] = cnt;
+            if(arr[x][y]==2){
+                sx = x;
+                sy = y;
             }
         }
     }
+
+
+    // BFS
+    queue<node> q;
+    q.push({sx,sy});
+    v[sx][sy] = true;
+    answer[sx][sy] = 0;
+
+    while (!q.empty()){
+        node cur = q.front(); q.pop();
+        int x = cur.first;
+        int y = cur.second;
+        for(int f=0; f<4; ++f){
+            int nx = x + fx[f];
+            int ny = y + fy[f];
+            if(!v[nx][ny] && arr[nx][ny]==1 && !outofBound(nx,ny)){
+                q.push({nx,ny});
+                answer[nx][ny] = answer[x][y] + 1;
+                v[nx][ny] = true;
+            }
+        }
+    }
+
+    for(int x=0; x<N; ++x){
+        for(int y=0; y<M; ++y){
+            if(arr[x][y] == 1 && answer[x][y] == 0) cout << -1 << " ";
+            else cout << answer[x][y] << " ";
+        }
+        cout << "\n";
+    }
+
+
 
     return 0;
 }
