ae7fff365e6bef987a49d40eb669291545516f70
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-22 22:14:20 +0900

    21736

--- main.cpp
+++ main.cpp
@@ -2,61 +2,55 @@
     긍정, 책임
 */
 #include <iostream>
-#include <vector>
-
+#include <queue>
 using namespace std;
 
-int N, M, K;
-vector<long long> A, seg;
+int N, M;
+const int MAXNM = 600;
 
-long long build(int idx, int l, int r){
-    if(l == r) return seg[idx] = A[l]; // 어차피 l == r이 true임
-    int mid = (l + r) / 2;
-    return seg[idx] = build(idx*2, l, mid) + build(idx*2+1, mid+1, r);
-}
+int arr[MAXNM][MAXNM];
+bool v[MAXNM][MAXNM];
+int fx[4] = {0,0,-1,1};
+int fy[4] = {-1,1,0,0};
 
-long long query(int idx, int l, int r, int ql, int qr){
-    if (qr < l || r < ql) return 0;
-    if (ql <= l && r <= qr) return seg[idx];
-    int mid = (l + r) / 2;
-    return query(idx*2, l, mid, ql, qr) + query(idx*2+1, mid+1, r, ql, qr);
-}
-
-void update(int idx, int l, int r, int pos, long long val){
-    if(l == r){
-        seg[idx] = val;
-        return;
-    }
-    int mid = (l + r) / 2;
-    if(pos <= mid) update (idx*2, l, mid, pos, val);
-    else update(idx*2+1, mid+1, r, pos, val);
-    seg[idx] = seg[idx*2] + seg[idx*2+1];
-}
-
+bool outofBound(int x, int y) { return (x<0 || x>=N || y<0 || y>=M);}
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    long long num;
-    cin >> N >> M >> K;
-    A.assign(N+1,0);
-    for(int n=1; n<=N; ++n){
-        cin >> num;
-        A[n] = num;
-    }
-    seg.assign(4*(N+1),0);
-    build(1,1,N);
+    cin >> N >> M;
+    queue<pair<int,int>> q;
+    char c;
+    int answer = 0;
 
-    for(int i=0; i<M+K; ++i){
-        int a;
-        long long b,c;
-        cin >> a >> b >> c;
-        if(a == 1){
-            update(1,1,N,(int)b,c);
-        }
-        else{
-            cout << query(1,1,N,(int)b, (int)c) << "\n";
+
+    for(int x=0; x<N; ++x){
+        for (int y=0; y<M; ++y){
+            cin >> c;
+            arr[x][y] = c;
+            if (arr[x][y] == 'I'){
+                v[x][y] = true;
+                q.push({x,y});
+            }
         }
     }
+
+    while(!q.empty()){
+        pair<int,int> cur = q.front(); q.pop();
+        int x = cur.first;
+        int y = cur.second;
+        for(int f=0; f<4; ++f){
+            int nx = x + fx[f];
+            int ny = y + fy[f];
+            if(!outofBound(nx,ny) && !v[nx][ny] && arr[nx][ny]!= 'X'){
+                q.push({nx,ny});
+                v[nx][ny] = true;
+                if(arr[nx][ny] == 'P') ++answer;
+            }
+        }
+    }
+
+    if (answer == 0) cout << "TT";
+    else cout << answer;
 
     return 0;
 }
