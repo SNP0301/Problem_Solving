76ff38d80a0c60704e915705b07dd966f3939b50
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-22 22:05:11 +0900

    2042

--- main.cpp
+++ main.cpp
@@ -1,67 +1,62 @@
 /*
     긍정, 책임
-
-    0이 하양, 1이 파랑 출력은 하얀 다음 파랑
-    128*128에 대해 완전탐색 7번
 */
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-const int MAXN = 128;
-static int N;
-int arr[MAXN][MAXN];
-bool v[MAXN][MAXN];
+int N, M, K;
+vector<long long> A, seg;
 
-bool outofBound(int x, int y){ return(x<0 || x>=N || y<0 || y>=N);}
+long long build(int idx, int l, int r){
+    if(l == r) return seg[idx] = A[l]; // 어차피 l == r이 true임
+    int mid = (l + r) / 2;
+    return seg[idx] = build(idx*2, l, mid) + build(idx*2+1, mid+1, r);
+}
+
+long long query(int idx, int l, int r, int ql, int qr){
+    if (qr < l || r < ql) return 0;
+    if (ql <= l && r <= qr) return seg[idx];
+    int mid = (l + r) / 2;
+    return query(idx*2, l, mid, ql, qr) + query(idx*2+1, mid+1, r, ql, qr);
+}
+
+void update(int idx, int l, int r, int pos, long long val){
+    if(l == r){
+        seg[idx] = val;
+        return;
+    }
+    int mid = (l + r) / 2;
+    if(pos <= mid) update (idx*2, l, mid, pos, val);
+    else update(idx*2+1, mid+1, r, pos, val);
+    seg[idx] = seg[idx*2] + seg[idx*2+1];
+}
+
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int num;
-    int white = 0; // 0이 하양
-    int blue = 0; // 1이 파랑
-    cin >> N;
+    long long num;
+    cin >> N >> M >> K;
+    A.assign(N+1,0);
+    for(int n=1; n<=N; ++n){
+        cin >> num;
+        A[n] = num;
+    }
+    seg.assign(4*(N+1),0);
+    build(1,1,N);
 
-    for(int i=0; i<N; ++i){
-        for(int j=0; j<N; ++j){
-            cin >> num;
-            arr[i][j] = num;
+    for(int i=0; i<M+K; ++i){
+        int a;
+        long long b,c;
+        cin >> a >> b >> c;
+        if(a == 1){
+            update(1,1,N,(int)b,c);
+        }
+        else{
+            cout << query(1,1,N,(int)b, (int)c) << "\n";
         }
     }
-
-    for(int sz=N; sz>0; sz/=2){ // 8 4 2 1
-        for(int x=0; x<N; x += sz){
-            for(int y=0; y<N; y += sz){
-                if(!v[x][y]){ // 아직 세지 않은 곳이면
-                    int cur = arr[x][y]; // 지금 색
-                    bool possible = true;
-
-                    // 한번이라도 다른 색이거나, 범위를 벗어나거나, 
-                    for(int lx=0; lx<sz; ++lx){
-                        for(int ly=0; ly<sz; ++ly){
-                            if(arr[x+lx][y+ly]!=cur || v[x+lx][y+ly] || outofBound(x+lx,y+ly)){
-                                possible = false;
-                                break;
-                            }
-                        }
-                        if(!possible) break;
-                    }
-
-                    if (possible){
-
-                    for(int lx=0; lx<sz; ++lx){
-                        for(int ly=0; ly<sz; ++ly){
-                            v[x+lx][y+ly] = true;
-                        }
-                    }
-                    if (arr[x][y] == 0) white += 1;
-                    else blue += 1;
-                }
-            }
-        }
-    }
-    }
-
-    cout << white << "\n" << blue;
 
     return 0;
 }
