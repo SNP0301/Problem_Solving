e34ae412ca6584c7b6fe86bf7f4e4690efa299a2
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-23 21:04:33 +0900

    5567

--- main.cpp
+++ main.cpp
@@ -2,55 +2,54 @@
     긍정, 책임
 */
 #include <iostream>
+#include <vector>
 #include <queue>
 using namespace std;
 
-int N, M;
-const int MAXNM = 600;
+const static int MAXN = 501;
 
-int arr[MAXNM][MAXNM];
-bool v[MAXNM][MAXNM];
-int fx[4] = {0,0,-1,1};
-int fy[4] = {-1,1,0,0};
-
-bool outofBound(int x, int y) { return (x<0 || x>=N || y<0 || y>=M);}
+bool arr[MAXN][MAXN];
+bool v[MAXN];
+int answerArr[MAXN];
+int dist[MAXN];
+queue<int> q;
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N,M,s,e,answer;
+    answer = 0;
     cin >> N >> M;
-    queue<pair<int,int>> q;
-    char c;
-    int answer = 0;
 
-
-    for(int x=0; x<N; ++x){
-        for (int y=0; y<M; ++y){
-            cin >> c;
-            arr[x][y] = c;
-            if (arr[x][y] == 'I'){
-                v[x][y] = true;
-                q.push({x,y});
-            }
-        }
+    for(int i=1; i<=N; ++i) dist[i] = 10'001;
+    for(int i=0; i<M; ++i){
+        cin >> s >> e;
+        arr[s][e] = true;
+        arr[e][s] = true;
     }
+
+
+    v[1] = true;
+    q.push(1);
+    dist[1] = 0;
 
     while(!q.empty()){
-        pair<int,int> cur = q.front(); q.pop();
-        int x = cur.first;
-        int y = cur.second;
-        for(int f=0; f<4; ++f){
-            int nx = x + fx[f];
-            int ny = y + fy[f];
-            if(!outofBound(nx,ny) && !v[nx][ny] && arr[nx][ny]!= 'X'){
-                q.push({nx,ny});
-                v[nx][ny] = true;
-                if(arr[nx][ny] == 'P') ++answer;
+        int cur = q.front(); q.pop();
+        for(int nxt=1; nxt<=N; ++nxt){
+            if(!v[nxt] && arr[cur][nxt] && dist[cur]<=1){
+                v[nxt] = true;
+                answerArr[nxt] = 1;
+                dist[nxt] = dist[cur]+1;
+                if (dist[cur] <= 1){
+                    q.push(nxt);
+                }
             }
         }
+
+
     }
 
-    if (answer == 0) cout << "TT";
-    else cout << answer;
+    for(int i=1; i<=N; ++i) answer += answerArr[i];
+    cout << answer;
 
     return 0;
 }
