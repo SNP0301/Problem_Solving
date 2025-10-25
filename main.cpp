cb3abbc0b21d2f846ec23d4339c22394b78173b2
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-25 14:48:36 +0900

    16173

--- main.cpp
+++ main.cpp
@@ -6,80 +6,53 @@
 #include <queue>
 #include <vector>
 using namespace std;
+using area = pair<int,int>;
 
-const static int MAXN = 100;
-char arr[MAXN][MAXN];
-bool v[MAXN][MAXN];
+int N;
+int arr[3][3];
+bool v[3][3];
 int fx[4] = {0,0,-1,1};
 int fy[4] = {-1,1,0,0};
-int N;
 
-bool outofBound(int x, int y){ return(x<0 || x>=N || y<0 || y>=N);}
+bool outofBound(int x, int y){return(x<0 || x>=N || y<0 || y>=N);}
 
-void bfs(int x, int y){
-    queue<pair<int,int>> q;
-    q.push({x,y});
-    v[x][y] = true;
-    char color = arr[x][y];
+void bfs(){
+    queue<area> q;
+    q.push({0,0});
+    v[0][0] = true;
 
     while(!q.empty()){
         auto [x,y] = q.front(); q.pop();
+        int dist = arr[x][y];
         for(int f=0; f<4; ++f){
-            int nx = x + fx[f];
-            int ny = y + fy[f];
-            if(!outofBound(nx,ny) && arr[nx][ny]==color && !v[nx][ny]){
-                q.push({nx,ny});
+            int nx = x + fx[f]*dist;
+            int ny = y + fy[f]*dist;
+            if(!outofBound(nx,ny) && !v[nx][ny]){
                 v[nx][ny] = true;
+                q.push({nx,ny});
             }
         }
     }
-    
+
 
 }
 
+
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int first = 0;
-    int second = 0;
+    
     cin >> N;
 
-    
-    for(int i=0; i<N; ++i){
-        for(int j=0; j<N; ++j){
-            cin >> arr[i][j];
+    for(int x=0; x<N; ++x){
+        for(int y=0; y<N; ++y){
+            cin >> arr[x][y];
         }
     }
 
-    for(int i=0; i<N; ++i){
-        for(int j=0; j<N; ++j){
-            if(!v[i][j]){
-                ++first;
-                bfs(i,j);
-            }
-        }
-    }
+    bfs();
 
-    cout << first << " ";
-
-
-    for(int i=0; i<N; ++i){
-        for(int j=0; j<N; ++j){
-            v[i][j] = false;
-            if (arr[i][j]=='R') arr[i][j] = 'G';
-        }
-    }
-    for(int i=0; i<N; ++i){
-        for(int j=0; j<N; ++j){
-
-            if(!v[i][j]){
-                ++second;
-                bfs(i,j);
-            }
-        }
-    }
-    
-    cout << second;
-
+    if(v[N-1][N-1]) cout << "HaruHaru";
+    else cout << "Hing";
 
     return 0;
 }
