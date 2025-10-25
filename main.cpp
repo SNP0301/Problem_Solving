39ffd783302ff769b459cedb94161baab0c7941b
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-25 15:07:31 +0900

    1388

--- main.cpp
+++ main.cpp
@@ -6,53 +6,52 @@
 #include <queue>
 #include <vector>
 using namespace std;
-using area = pair<int,int>;
 
-int N;
-int arr[3][3];
-bool v[3][3];
-int fx[4] = {0,0,-1,1};
-int fy[4] = {-1,1,0,0};
+const int MAXNM = 50;
+static int answer = 0;
+bool v[MAXNM][MAXNM];
+char arr[MAXNM][MAXNM];
 
-bool outofBound(int x, int y){return(x<0 || x>=N || y<0 || y>=N);}
+int N,M;
 
-void bfs(){
-    queue<area> q;
-    q.push({0,0});
-    v[0][0] = true;
+bool outofBound(int x, int y) {return (x<0 || x>=N || y<0 || y>=M);}
 
-    while(!q.empty()){
-        auto [x,y] = q.front(); q.pop();
-        int dist = arr[x][y];
-        for(int f=0; f<4; ++f){
-            int nx = x + fx[f]*dist;
-            int ny = y + fy[f]*dist;
-            if(!outofBound(nx,ny) && !v[nx][ny]){
-                v[nx][ny] = true;
-                q.push({nx,ny});
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M;
+
+    for(int x=0; x<N; ++x){
+        for(int y=0; y<M; ++y){
+            cin >> arr[x][y];
+        }
+    }
+
+    for(int x=0; x<N; ++x){
+        for(int y=0; y<M; ++y){
+            if(!v[x][y]){
+                ++answer;
+                if(arr[x][y] == '-'){
+                    int sz = 1;
+                    while(!outofBound(x,y+sz) && arr[x][y+sz]=='-'){
+                        v[x][y+sz] = true;
+                        ++sz;
+                    }
+                }
+                else{
+                    int sz = 1;
+                    while(!outofBound(x+sz,y) && arr[x+sz][y]=='|'){
+                        v[x+sz][y] = true;
+                        ++sz;
+                    }
+                }
             }
         }
     }
 
 
-}
 
+    cout << answer;
 
-int main(){
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    
-    cin >> N;
-
-    for(int x=0; x<N; ++x){
-        for(int y=0; y<N; ++y){
-            cin >> arr[x][y];
-        }
-    }
-
-    bfs();
-
-    if(v[N-1][N-1]) cout << "HaruHaru";
-    else cout << "Hing";
 
     return 0;
 }
