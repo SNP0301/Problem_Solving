dbee30e550767abfd95cfac607999b707426f1a4
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-25 23:01:20 +0900

    11055

--- main.cpp
+++ main.cpp
@@ -1,67 +1,39 @@
 /*
     긍정, 책임
-    
-    lo, hi를 두고 isPossible 해보고 만족하면 크기 늘려보고, 만족 못하면 크기 줄이기
+    22:35 ~ 
+    순서대로 정렬했을 때, 내가 봤을 때, 나보다 바로 한 계층 작은 애 + 나?
 */
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <algorithm>
+#include <set>
 using namespace std;
-using ll = long long;
-int N, C;
-ll mnLoc, mxLoc;
-vector<ll> vc;
 
-bool isPossible(ll x){
-    int cCnt = 1;
+const int MAXN = 1'000 + 1; // 1based
+int dp[MAXN];
 
-    ll prev = vc[0];
-    for(int idx=1; idx<N; ++idx){
-        if(vc[idx]-prev >= x){ // 1 2 4 8 9 
-            prev = vc[idx];
-            ++cCnt;
-        }
-    }
-    //cout << x << ": "<< cCnt << "\n";
-    return (cCnt >= C);
-}
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    ll num;
-    ll answer = 0;
-    mnLoc = 1'000'000'000 + 1;
-    mxLoc = -1;
+    int N,num;
+    int answer = -1;
+    cin >> N;
+    vector<int> vc;
+    vector<int> dp;
 
-    cin >> N >> C;
     for(int i=0; i<N; ++i){
         cin >> num;
         vc.push_back(num);
-        if (num <= mnLoc) mnLoc = num;
-        if (num >= mxLoc) mxLoc = num;
+        dp.push_back(num);
     }
-    
-    sort(vc.begin(),vc.end());
 
-    ll lo = 0;
-    ll hi = 1'000'000'000 + 1;
-    
-    while(lo <= hi){
-        ll mid = (lo + hi) / 2;
-
-        if(isPossible(mid)){
-            answer = mid;
-            lo = mid + 1;
+    for(int i=0; i<N; ++i){
+        for(int j=0; j<i; ++j){
+            if(vc[i] > vc[j] && dp[i] < dp[j] + vc[i]) dp[i] = dp[j] + vc[i];
         }
-        else{
-            hi = mid-1;
-        }
-
-
+        answer = max(answer, dp[i]);
     }
 
     cout << answer;
-
     return 0;
 }
