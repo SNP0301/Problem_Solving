88254e7e612004d69d8042ba63d77780ccc9cb0d
Seung-Gi0301 <studiosnp0301@gmail.com>
2025-10-27 22:46:50 +0900

    16934

--- main.cpp
+++ main.cpp
@@ -2,64 +2,51 @@
     긍정, 책임
 */
 #include <iostream>
-#include <queue>
-#include <vector>
+#include <string>
 using namespace std;
-
-char arr[10'000][10+1];
+string s;
 
 struct Trie{
     bool endofWord;
-    Trie* children[10]; // 0부터 9까지 10자리
+    int sameCnt;
+    Trie* children[26]; // 알파벳 소문자 26개
 
-    Trie(): endofWord(false), children{}{
+    Trie(): endofWord(false), sameCnt(0), children{}{
     }
 
-    void insert(char* key){
-        if(*key == '\0') endofWord = true;
-        else{
-            int idx = *key - '0';
-            if(children[idx] == 0) children[idx] = new Trie();
-            children[idx]->insert(key+1);
+    void insert(string& s, int idx, bool possible){
+
+        if(idx >= (int)s.length()){
+            sameCnt++;
+            if(sameCnt > 1) cout << sameCnt;
+            cout << "\n";
+            return;
         }
-    }
 
+        int ctoi = s[idx] - 'a';
+        if(!possible) cout << s[idx];
 
-    bool find(char* key){
-        if(*key == '\0') return true;
-        if(endofWord) return false;
-        int idx = *key - '0';
-        return children[idx]->find(key+1);
+        if(!children[ctoi]){
+            children[ctoi] = new Trie();
+            possible = true; // 없네
+        }
+
+        children[ctoi]->insert(s,idx+1,possible);
+    
     }
 };
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int T, N;
-    cin >> T;
+    int N;
+    cin >> N;
+    Trie* root = new Trie();
 
-    for(int i=0; i<T; ++i){
-        Trie* root = new Trie();
-        cin >> N;
-
-        for(int j=0; j<N; ++j){
-            cin >> arr[j];
-            root -> insert(arr[j]);
-        }
-
-        bool isPossible = true;
-        for(int j=0; j<N; ++j){
-            if(root -> find(arr[j]) == false){
-                isPossible = false;
-                break;
-            }
-        }
-
-        if(isPossible) cout << "YES\n";
-        else cout << "NO\n";
-        delete root; // 에반데
+    for(int i=0; i<N; ++i){
+        cin >> s;
+        root -> insert(s,0,false); // 모르니까
     }
 
-
+    delete root;
     return 0;
 }
